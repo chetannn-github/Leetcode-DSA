415d0973ba05de8b157f17358c565c28b6f7b0d0
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2024-12-24 17:52:40 +0530

    Time: 57 ms (7.55%) | Memory: 63.1 MB (87.10%) - LeetSync

---
+++ 2392-successful-pairs-of-spells-and-potions/successful-pairs-of-spells-and-potions.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+        long[] req = new long[potions.length];
+        for(int i=0; i<potions.length; i++){
+            if(success%potions[i]==0){
+                req[i] =(success/potions[i]) ;
+            }else{
+                req[i] = (success/potions[i])+1;
+            }
+        }
+        Arrays.sort(req);
+
+        System.out.print(Arrays.toString(potions));
+
+        for(int i=0; i<spells.length; i++){
+           spells[i] = findUpperBound(req,spells[i]);
+        }
+
+        return spells;
+    }
+
+    public int findUpperBound(long[] nums , int q){
+        int start = 0; 
+        int end = nums.length-1;
+        int upper = nums.length;
+        while(start<=end){
+            int mid = start + ((end-start)>>1);
+            if(nums[mid]>q){
+                upper = mid;
+                end = mid -1;
+            }else{
+                start = mid+1;
+            }
+        }
+
+        return upper;
+    }
+}
