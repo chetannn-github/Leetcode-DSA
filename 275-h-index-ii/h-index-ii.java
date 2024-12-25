7fda3f6aa3db8c656ebe9185f8447e3242093845
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2024-12-25 10:02:23 +0530

    Time: 4 ms (5.38%) | Memory: 48.3 MB (8.50%) - LeetSync

---
+++ 275-h-index-ii/h-index-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int hIndex(int[] nums) {
+        int start = 0;
+        int end = nums.length;
+         
+
+        for(int i=nums.length; i>=0; i--){
+            int biggerCount = nums.length - findUpperBound(nums,i);
+
+            if(biggerCount >=i){
+                return i;
+            }
+        }
+        return 0;
+    }
+
+
+    public int findUpperBound(int[] nums , int q){
+        int start = 0; 
+        int end = nums.length-1;
+        int upper = nums.length;
+        while(start<=end){
+            int mid = start + ((end-start)>>1);
+            if(nums[mid]>=q){
+                upper = mid;
+                end = mid -1;
+            }else{
+                start = mid+1;
+            }
+        }
+
+        return upper;
+    }
+}
