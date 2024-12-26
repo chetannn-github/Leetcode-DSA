b3249156dad727d858bf95f02c73b574451b4a98
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2024-12-26 20:21:59 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (55.49%) - LeetSync

--- 1307-ugly-number-iii/ugly-number-iii.java
+++ 1307-ugly-number-iii/ugly-number-iii.java
@@ -1,7 +1,8 @@
-public class Solution {
-    int MAX_ANS = (int) 2e9; // 2*10^9
+class Solution {
+    
     public int nthUglyNumber(int n, int a, int b, int c) {
-        int left = 0, right = MAX_ANS, result = 0;
+        int left = 0, right = 2_000_000_000, result = 0;
+
         while (left <= right) {
             int mid = left + (right - left) / 2;
             if (count(mid, a, b, c) >= n) {
@@ -13,18 +14,27 @@ public class Solution {
         }
         return result;
     }
+
     int count(long num, long a, long b, long c) {
-        return (int) (num / a + num / b + num / c
-                - num / lcm(a, b)
-                - num / lcm(b, c)
-                - num / lcm(a, c)
-                + num / (lcm(a, lcm(b, c))));
+        long mulA = num/a ;
+        long mulB = num/b ;
+        long mulC = num/c ;
+
+        long mulAB = (num / lcm(a,b));
+        long mulAC = (num / lcm(a,c));
+        long mulCB = (num / lcm(c,b));
+
+        long mulABC = (num / lcm(a, lcm(b, c)));
+
+        long count =(mulA + mulB + mulC - mulAB - mulAC - mulCB  + mulABC);
+        return (int) count;
     }
+
     long gcd(long a, long b) {
         if (a == 0) return b;
         return gcd(b % a, a);
     }
     long lcm(long a, long b) {
-        return a * b / gcd(a, b);
+        return a / gcd(a, b)* b ;
     }
 }
