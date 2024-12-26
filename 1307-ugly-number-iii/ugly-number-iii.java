5629b511de3a7d97cbfdc576e74807db886b557a
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2024-12-26 20:28:03 +0530

    Time: 1 ms (8.67%) | Memory: 41 MB (7.51%) - LeetSync

--- 1307-ugly-number-iii/ugly-number-iii.java
+++ 1307-ugly-number-iii/ugly-number-iii.java
@@ -31,8 +31,8 @@ class Solution {
     }
 
     long gcd(long a, long b) {
-        if (a == 0) return b;
-        return gcd(b % a, a);
+        if (b == 0) return a;
+        return gcd(b, a% b);
     }
     long lcm(long a, long b) {
         return a / gcd(a, b)* b ;
