4c06c4d3853022114e019f3522ce353379b9c34f
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2024-12-29 08:47:28 +0530

    Time: 13 ms (25.00%) | Memory: 46 MB (5.77%) - LeetSync

--- 1647-can-convert-string-in-k-moves/can-convert-string-in-k-moves.java
+++ 1647-can-convert-string-in-k-moves/can-convert-string-in-k-moves.java
@@ -1,9 +1,40 @@
+// class Solution {
+//     public boolean canConvertString(String s, String t, int k) {
+//         if(s.length()!= t.length()){
+//             return false;
+//         }
+//         HashMap<Integer,Integer> hm = new HashMap<>();
+
+//         for(int i=0; i<s.length(); i++){
+//             if(s.charAt(i)!= t.charAt(i)){
+//                 int dif =(int) (t.charAt(i) - s.charAt(i));
+
+//                 if(dif<0){dif = (dif+26)%26;}
+    
+//                 if(dif>k){return false;}
+//                 hm.put(dif, hm.getOrDefault(dif,0)+1);
+//             }
+//         }
+
+//         for(int key : hm.keySet()){
+//             int freq = hm.get(key);
+//             if( (((freq-1)*26)+ key )>k){
+//                 return false;
+//             }
+//         }
+    
+//         return true;
+//     }
+// }
+
+
 class Solution {
     public boolean canConvertString(String s, String t, int k) {
         if(s.length()!= t.length()){
             return false;
         }
-        HashMap<Integer,Integer> hm = new HashMap<>();
+        // HashMap<Integer,Integer> hm = new HashMap<>();
+        int[] map = new int[26];
 
         for(int i=0; i<s.length(); i++){
             if(s.charAt(i)!= t.charAt(i)){
@@ -12,13 +43,13 @@ class Solution {
                 if(dif<0){dif = (dif+26)%26;}
     
                 if(dif>k){return false;}
-                hm.put(dif, hm.getOrDefault(dif,0)+1);
+                // hm.put(dif, hm.getOrDefault(dif,0)+1);
+                map[dif]++;
             }
         }
 
-        for(int key : hm.keySet()){
-            int freq = hm.get(key);
-            if( (((freq-1)*26)+ key )>k){
+        for(int i=1; i<26; i++){
+            if( (((map[i]-1)*26)+ i )>k){
                 return false;
             }
         }
