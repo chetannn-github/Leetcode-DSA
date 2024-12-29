41276c5ca901fa8a61c655cf012d0b36f4df5e8d
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2024-12-29 08:40:18 +0530

    Time: 31 ms (19.23%) | Memory: 45.8 MB (28.85%) - LeetSync

---
+++ 1647-can-convert-string-in-k-moves/can-convert-string-in-k-moves.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean canConvertString(String s, String t, int k) {
+        if(s.length()!= t.length()){
+            return false;
+        }
+        HashMap<Integer,Integer> hm = new HashMap<>();
+
+        for(int i=0; i<s.length(); i++){
+            if(s.charAt(i)!= t.charAt(i)){
+                int dif =(int) (t.charAt(i) - s.charAt(i));
+
+                if(dif<0){dif = (dif+26)%26;}
+    
+                if(dif>k){return false;}
+                hm.put(dif, hm.getOrDefault(dif,0)+1);
+            }
+        }
+
+        for(int key : hm.keySet()){
+            int freq = hm.get(key);
+            if( (((freq-1)*26)+ key )>k){
+                return false;
+            }
+        }
+    
+        return true;
+    }
+}
