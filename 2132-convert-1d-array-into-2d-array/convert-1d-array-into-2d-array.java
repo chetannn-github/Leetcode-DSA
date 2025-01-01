f5e2eda952ac69c213562868d768018f96c54642
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-01 21:45:52 +0530

    Time: 4 ms (58.58%) | Memory: 55.5 MB (56.76%) - LeetSync

--- 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.java
@@ -1,20 +1,33 @@
 class Solution {
     public int[][] construct2DArray(int[] og, int m, int n) {
-        if(m*n != og.length){
-            return new int[][]{};
-        }
-        int [][] ans = new int[m][n];
-        int count = 0;
-        for(int i=0; i<m; i++){
-            int[] row = new int[n];
+        // if(m*n != og.length){
+        //     return new int[][]{};
+        // }
+        // int [][] ans = new int[m][n];
+        // int count = 0;
+        // for(int i=0; i<m; i++){
+        //     int[] row = new int[n];
 
-            for(int j=0; j<n; j++){
-                row[j] = og[count];
-                count++;
-            }
-            ans[i] = row;
+        //     for(int j=0; j<n; j++){
+        //         row[j] = og[count];
+        //         count++;
+        //     }
+        //     ans[i] = row;
+        // }
+        // return ans;
+
+
+         // //OPTIMIZED 
+    // // If dimensions are invalid, return an empty 2D array
+        if (og.length != m * n) return new int[0][0];
+        
+        int[][] res = new int[m][n];
+        for (int i = 0; i < og.length; i++) {
+            res[i / n][i % n] = og[i];
         }
-        return ans;
+        
+        return res;
+
 
     }
 }
