df071491a4972b497a76dfe93684e6fa3b1a15c1
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-06 10:06:35 +0530

    Time: 0 ms (100.00%) | Memory: 59.5 MB (34.42%) - LeetSync

---
+++ 2722-prime-in-diagonal/prime-in-diagonal.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int diagonalPrime(int[][] nums) {
+        int ans = 0;
+        for(int i=0;i<nums.length; i++){
+            if(nums[i][i]>ans && isPrime(nums[i][i])){
+                ans = nums[i][i];
+            }
+            if(nums[i][nums.length-1-i]> ans && isPrime(nums[i][nums.length-1-i])){
+                ans = nums[i][nums.length-1-i];
+            }
+        }
+        return ans;
+    }
+
+    public boolean isPrime(int num){
+        if(num==1){
+            return false;
+        }
+        for(int i= 2; i<= Math.sqrt(num); i++){
+            if(num%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
