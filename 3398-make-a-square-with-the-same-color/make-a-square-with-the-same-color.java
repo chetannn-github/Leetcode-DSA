73c68a773dd3d746acb4ee2e6b4aadc2eea9e84c
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-06 15:44:26 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (53.53%) - LeetSync

--- 3398-make-a-square-with-the-same-color/make-a-square-with-the-same-color.java
+++ 3398-make-a-square-with-the-same-color/make-a-square-with-the-same-color.java
@@ -1,50 +1,70 @@
+// class Solution {
+//     public boolean canMakeSquare(char[][] grid) {
+//         int count = 0;
+//         for(int i=0; i<2; i++){
+//             for(int j=0; j<2; j++){
+//                 if(grid[i][j]=='B'){
+//                     count--;
+//                 }else{
+//                     count++;
+//                 }
+                
+//             }
+//         }if(Math.abs(count)>=2){return true;}
+//         count = 0;
+//         for(int i=1; i<3; i++){
+//             for(int j=0; j<2; j++){
+//                 if(grid[i][j]=='B'){
+//                     count--;
+//                 }else{
+//                     count++;
+//                 }
+                
+//             }
+//         }if(Math.abs(count)>=2){return true;}
+//         count = 0;
+//         for(int i=0; i<2; i++){
+//             for(int j=1; j<3; j++){
+//                 if(grid[i][j]=='B'){
+//                     count--;
+//                 }else{
+//                     count++;
+//                 }
+                
+//             }
+//         }if(Math.abs(count)>=2){return true;}
+//         count = 0;
+//         for(int i=1; i<3; i++){
+//             for(int j=1; j<3; j++){
+//                 if(grid[i][j]=='B'){
+//                     count--;
+//                 }else{
+//                     count++;
+//                 }
+                
+//             }
+//         }if(Math.abs(count)>=2){return true;}
+//         return false;
+//     }
+    
+// }
+
 class Solution {
     public boolean canMakeSquare(char[][] grid) {
-        int count = 0;
-        for(int i=0; i<2; i++){
-            for(int j=0; j<2; j++){
-                if(grid[i][j]=='B'){
-                    count--;
-                }else{
-                    count++;
-                }
-                
+        
+        int n = grid.length-1;
+
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                int count  = 1;
+
+                if (grid[i][j]==grid[i][j+1]) count++;
+                if (grid[i][j]==grid[i+1][j]) count++;
+                if (grid[i][j]==grid[i+1][j+1]) count++;
+
+                if (count==1 || count==3 || count==4) return true;
             }
-        }if(Math.abs(count)>=2){return true;}
-        count = 0;
-        for(int i=1; i<3; i++){
-            for(int j=0; j<2; j++){
-                if(grid[i][j]=='B'){
-                    count--;
-                }else{
-                    count++;
-                }
-                
-            }
-        }if(Math.abs(count)>=2){return true;}
-        count = 0;
-        for(int i=0; i<2; i++){
-            for(int j=1; j<3; j++){
-                if(grid[i][j]=='B'){
-                    count--;
-                }else{
-                    count++;
-                }
-                
-            }
-        }if(Math.abs(count)>=2){return true;}
-        count = 0;
-        for(int i=1; i<3; i++){
-            for(int j=1; j<3; j++){
-                if(grid[i][j]=='B'){
-                    count--;
-                }else{
-                    count++;
-                }
-                
-            }
-        }if(Math.abs(count)>=2){return true;}
+        }
         return false;
     }
-    
 }
