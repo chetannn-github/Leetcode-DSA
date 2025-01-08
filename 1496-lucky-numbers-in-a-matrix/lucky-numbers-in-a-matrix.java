6fc4feddee6eab9549c9e093132cf7ea72c8cfab
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-08 19:05:31 +0530

    Time: 3 ms (47.23%) | Memory: 45.4 MB (34.62%) - LeetSync

---
+++ 1496-lucky-numbers-in-a-matrix/lucky-numbers-in-a-matrix.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> luckyNumbers(int[][] matrix) {
+        int rowMinMax = Integer.MIN_VALUE;
+
+        for(int[] row : matrix){
+            int rowMin = Integer.MAX_VALUE;
+            for(int num : row){
+                rowMin = Math.min(rowMin, num);
+            }
+            rowMinMax = Math.max(rowMinMax,rowMin);
+        }
+        int colMaxMin = Integer.MAX_VALUE;
+
+        for(int j=0; j<matrix[0].length; j++){
+            int colMax = Integer.MIN_VALUE;
+            for(int i=0; i<matrix.length; i++){
+                colMax = Math.max(colMax, matrix[i][j]);
+            }
+            colMaxMin = Math.min(colMaxMin,colMax);
+        }
+        List<Integer> ans = new ArrayList<>();
+        if(colMaxMin == rowMinMax){
+            ans.add(colMaxMin);
+        }
+
+        return ans;
+    }
+}
