efeadfcb3fb2a9a33e8f24b41a755519832ca3fb
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-11 09:52:53 +0530

    Time: 6 ms (76.36%) | Memory: 85.4 MB (96.01%) - LeetSync

---
+++ 2691-count-vowel-strings-in-ranges/count-vowel-strings-in-ranges.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int[] vowelStrings(String[] words, int[][] queries) {
+        Set<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+
+        int[] prefix  = new int[words.length+1];
+        prefix[0] = 1;
+        int idx = 1;
+        for(String word : words){
+            if(checkVowelString(word,vowels)){
+                prefix[idx] = prefix[idx-1] + 1;
+            }else{
+                prefix[idx] = prefix[idx-1];
+            }
+            idx++;
+        }
+        int ans[] = new int[queries.length];
+        for(int i=0; i<queries.length; i++){
+            ans[i] = prefix[queries[i][1]+1] - prefix[queries[i][0]];
+
+        }
+        return ans;
+
+
+    }
+
+    public boolean checkVowelString(String word, Set vowels){
+        return vowels.contains(word.charAt(0)) && vowels.contains(word.charAt(word.length()-1));
+    }
+}
