d2a71f3bcbe05edcc5a79c786e18aa13ca920957
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-11 09:33:14 +0530

    Time: 16 ms (67.01%) | Memory: 69.1 MB (26.09%) - LeetSync

--- 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
+++ 1477-product-of-the-last-k-numbers/product-of-the-last-k-numbers.java
@@ -1,20 +1,29 @@
 class ProductOfNumbers {
-    List<Integer> data;
+    List<Integer> prefix;
     public ProductOfNumbers() {
-       data  = new ArrayList<>();
+       prefix  = new ArrayList<>();
+       prefix.add(1);
     }
     
     public void add(int num) {
-        data.add(num);
+        if(num==0){
+            prefix  = new ArrayList<>();
+            prefix.add(1);
+        }else{ 
+            int last = prefix.size()-1;
+            prefix.add(prefix.get(last)*num);
+        }
     }
     
     public int getProduct(int k) {
-        int size = data.size();
-        int prod = 1;
-        for(int i= size-1; i>size-1-k ; i--){
-            prod *= data.get(i);
+        int size = prefix.size();
+        
+        // matlb khii zero aaya hogaa ussse size choti ho gyiii
+        if(size-1<k){
+            return 0;
         }
-        return prod;
+
+        return prefix.get(size-1) / prefix.get(size - 1 - k);                                          
     }
 }
 
