e142bdc9e559aa94c209fb5e1cde489ecc4a6966
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-14 09:33:54 +0530

    Time: 1 ms (99.85%) | Memory: 58.7 MB (5.07%) - LeetSync

---
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -0,0 +1,69 @@
+// class Solution {
+//     public int minSubArrayLen(int target, int[] nums) {
+//         // answer lies between 1 to length so we can think about binary search
+//         int start = 1; 
+//         int end = nums.length;
+//         int ans = 0;
+//         for(int i=1; i<nums.length; i++){
+//             nums[i] +=nums[i-1];
+//         }
+
+//         while(start<=end){
+//             int mid = start + ((end-start)>>1);
+           
+//             if(isPossible(target, nums, mid)){
+//                 end = mid-1;
+//                 ans = mid;
+//             }else{
+//                 start = mid+1;
+//             }
+//         }
+//         return ans;
+//     }
+
+//     public Boolean isPossible(int target, int[] nums , int size){
+//         // phele prefix sum nikal lete hh easy padegaa 
+//         // desired length ke subarray ka sum nikalne ke liye 
+//         // nhii toh baar traverse krna padegaa
+       
+//         if(target<=nums[size-1]){
+//             return true;
+//         }
+//         for(int j=0, i=size; i<nums.length; i++){
+//             if(target<= nums[i]-nums[j] ){
+//                 return true;
+//             }
+//             j++;
+//         }
+//         return false;
+//     }
+// }
+
+
+
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) {
+        int n = nums.length;
+        int start = 0; 
+        int currentSum = 0;
+        int minLength = Integer.MAX_VALUE; 
+
+        for (int end = 0; end < n; end++) {
+            
+            currentSum += nums[end];
+
+            
+            while (currentSum >= target) {
+                minLength = Math.min(minLength, end - start + 1);
+                currentSum -= nums[start];
+                start++;
+                if(minLength ==1){return 1;}
+            }
+        }
+
+
+        return minLength == Integer.MAX_VALUE ? 0 : minLength;
+    }
+
+    
+}
