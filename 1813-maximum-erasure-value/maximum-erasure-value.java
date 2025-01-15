1e3ee5951283cd89e9d91b7438bbae47f117f9ce
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-15 08:34:55 +0530

    Time: 52 ms (50.24%) | Memory: 61.2 MB (21.95%) - LeetSync

---
+++ 1813-maximum-erasure-value/maximum-erasure-value.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maximumUniqueSubarray(int[] nums) {
+        // esi subrray batani hh jisme saare elements distinct ho aur sum max ho
+        int start = 0;
+        int maxSum = 0;
+        int sum = 0;
+        int n = nums.length;
+        HashSet<Integer> hs = new HashSet<>();
+
+        for(int end =0; end<n; end++){
+
+            while(hs.size()>0 && hs.contains(nums[end])){
+                sum -= nums[start];
+                hs.remove(nums[start]);
+                start++;
+            }
+            hs.add(nums[end]);
+            sum += nums[end];
+            // System.out.println(start + " .... " + end);
+            maxSum = Math.max(sum,maxSum);
+        }
+
+        return maxSum;
+
+    
+    }
+}
