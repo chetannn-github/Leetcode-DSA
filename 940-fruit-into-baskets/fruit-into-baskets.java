a6a26457f4c0a8090a2c4f7799e1edd125c1739a
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-15 08:16:50 +0530

    Time: 44 ms (84.53%) | Memory: 54.9 MB (90.26%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int totalFruit(int[] fruits) {
+        // humee esi max length ki subarray btani hh jisme 2 distinct elements ho
+
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        int n = fruits.length;
+        int max = 0;
+        int start = 0;
+        
+        for(int end =0; end<n; end++){
+
+            hm.put(fruits[end],hm.getOrDefault(fruits[end],0)+1);
+            while(hm.size()>2){
+                if(hm.get(fruits[start])>1){
+                    hm.put(fruits[start],hm.get(fruits[start])-1);
+                }else{
+                    hm.remove(fruits[start]);
+                }
+
+                start++;
+            }
+
+            max = Math.max(end-start+1, max);
+
+        }
+
+        return max;
+    }
+}
