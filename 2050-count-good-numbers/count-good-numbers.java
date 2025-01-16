b75c48c30c211f04301670fb86f1be247c168de6
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-16 16:42:29 +0530

    Time: 3 ms (1.94%) | Memory: 40.5 MB (47.54%) - LeetSync

---
+++ 2050-count-good-numbers/count-good-numbers.java
@@ -0,0 +1,34 @@
+class Solution {
+    public long MOD = 1_000_000_007;
+    public int countGoodNumbers(long n) {
+        long oddPos = n/2;
+        long evenPos = n - oddPos;
+        // 4 prime digits  hain 
+        //5 even digits hain
+
+        // now question is kiii hume yhh btana hhh kiii n digits me se kuch digit select krniii h
+        // aur koi bhi digit kitne baar bhiii select krr skte h aur unke permutations bhi krne h.
+
+        // answer is 4^oddPos * 5^evenPos
+        System.out.println(powerxn(4,oddPos) );
+        long ans = (powerxn(4,oddPos)% MOD ) * (powerxn(5,evenPos)% MOD ) % MOD ;
+
+        return (int) ans  ;
+
+    }
+
+    public long powerxn(long x , long n){
+        if(n==0){
+            return 1;
+        }
+
+        long temp = powerxn(x,n/2);
+
+        if(n%2==0){
+            return (temp*temp) %MOD;
+        }else{
+            return  x*(temp*temp) %MOD;
+        }
+
+    }
+}
