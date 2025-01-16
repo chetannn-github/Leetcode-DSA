b1a9c39b39bfba309d16d6620173dcf056d576b9
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-16 08:36:27 +0530

    Time: 6 ms (6.29%) | Memory: 56.8 MB (55.01%) - LeetSync

---
+++ 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+        // right se bade wale nikal lo aur left se bade wale nikal krr subtract krr do!!
+        System.out.println(countSubarraysWithMax(nums,left));
+        System.out.println(countSubarraysWithMax(nums,right+1));
+        return countSubarraysWithMax(nums,left) - countSubarraysWithMax( nums, right+1);
+    }
+
+    public int countSubarraysWithMax(int[] nums, int limit) {
+        
+        int start = 0;
+        int subarrays = 0;
+        int n = nums.length;
+
+
+        for(int end = 0; end<n; end++){
+
+            if(nums[end]>=limit){
+                subarrays += (end-start+1) * (n - end);
+                start = end+1;
+            }
+
+
+        }
+
+        return subarrays;
+    }
+}
