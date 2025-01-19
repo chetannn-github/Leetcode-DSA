c1704c91a9aeeb40f6a6250bf9ab814fc7c141fb
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-19 17:13:03 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (16.37%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,46 @@
+class Solution {
+    HashMap<Character, String> hm = new HashMap<>();
+    List<String> solutions = new ArrayList<>();
+    int n;
+
+    public List<String> letterCombinations(String digits) {
+
+        n = digits.length();
+        if(n==0){
+            return solutions;
+        }
+        hm.put('2', "abc");
+        hm.put('3', "def");
+        hm.put('4', "ghi");
+        hm.put('5', "jkl");
+        hm.put('6', "mno");
+        hm.put('7', "pqrs");
+        hm.put('8', "tuv");
+        hm.put('9', "wxyz");
+
+        StringBuilder sb = new StringBuilder();
+        solve(digits, sb, 0);
+        return solutions;
+    }
+
+    public void solve(String digits, StringBuilder sb , int start){
+        if(sb.length() ==n){
+            solutions.add(sb.toString());
+            return ;
+        }
+
+        for(int i=start; i<n; i++){
+            char ch = digits.charAt(i);
+            String st = hm.get(ch);
+
+            for(int j=0; j<st.length(); j++){
+                sb.append(st.charAt(j));
+                solve(digits,sb, i+1);
+                sb.setLength(sb.length()-1);
+            }
+        }
+
+    }
+
+
+}
