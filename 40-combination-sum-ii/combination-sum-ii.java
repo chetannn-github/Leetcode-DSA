e36257c7cfad75579f06988facffb0824681a733
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-19 21:52:06 +0530

    Time: 4 ms (44.17%) | Memory: 43.4 MB (50.03%) - LeetSync

---
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,87 @@
+// tle
+
+// class Solution {
+    
+//     HashSet<List<Integer>> result = new HashSet<>();
+
+//     public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        
+//         Arrays.sort(nums);
+//         List<Integer> temp = new ArrayList<>();
+//         solve(nums,target,temp,0);
+//         List<List<Integer>> ans = new ArrayList<>();
+
+//         for(List<Integer> combination : result){
+//             ans.add(combination);
+//         }
+//         return ans;
+//     }
+
+//     public void solve(int[] nums, int target, List<Integer> temp,int start){
+//         if(target==0){
+//             result.add(new ArrayList<>(temp));
+//         }
+//         else if(target<0 || start>=nums.length){
+//             return;
+//         }
+
+        
+//         for(int i=start; i<nums.length; i++){
+//             int num = nums[i];
+
+//             temp.add(nums[i]);
+//             solve(nums,target-num,temp,i+1);
+
+//             temp.remove(temp.size()-1);
+
+//         }
+            
+            
+        
+//     }
+// }
+
+
+
+
+class Solution {
+    
+    List<List<Integer>> result = new ArrayList<>();
+
+    public List<List<Integer>> combinationSum2(int[] nums, int target) {
+        Arrays.sort(nums);
+
+        List<Integer> temp = new ArrayList<>();
+        solve(nums,target,temp,0);
+        
+        return result;
+    }
+
+    public void solve(int[] nums, int target, List<Integer> temp,int start){
+        if(target==0){
+            result.add(new ArrayList<>(temp));
+        }else if(target<0 ){
+            return;
+        }
+        
+        for(int i=start; i<nums.length; i++){
+            if(i>start && nums[i] == nums[i-1]){
+                continue;
+            }
+            int num = nums[i];
+
+            temp.add(nums[i]);
+            solve(nums,target-num,temp,i+1);
+
+            temp.remove(temp.size()-1);
+
+        }
+
+        
+
+        
+            
+            
+        
+    }
+}
