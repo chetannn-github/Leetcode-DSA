c279e827f85e4a3f6b6ed9289351052dc7039181
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-20 20:03:35 +0530

    Time: 6 ms (86.66%) | Memory: 51.3 MB (52.62%) - LeetSync

---
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.java
@@ -0,0 +1,43 @@
+class Solution {
+    List<List<Integer>> result = new ArrayList<>();
+    
+
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        List<Integer> temp = new ArrayList<>();
+
+        solve(nums,temp,0);
+        // System.out.print(result);
+
+        return result;
+    }
+
+    public void solve(int[] nums, List<Integer> temp, int start){
+        int size = temp.size();
+        
+        if(size>=2){
+            if(temp.get(size-1) < temp.get(size-2) ){
+                return;
+            }else{
+                result.add(new ArrayList<>(temp));
+            }
+            
+            
+        }
+        
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=start; i<nums.length; i++){
+            if(hs.contains(nums[i])){
+                continue;
+            }
+                
+            temp.add(nums[i]);
+
+            solve(nums,temp,i+1);
+
+            temp.remove(temp.size()-1);
+            hs.add(nums[i]);
+            
+        }
+
+    }
+}
