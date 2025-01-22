25f6894ab53ae6131549fb250ca83a2886dd70c4
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-22 21:01:32 +0530

    Time: 4 ms (100.00%) | Memory: 57.6 MB (78.33%) - LeetSync

---
+++ 2228-watering-plants-ii/watering-plants-ii.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minimumRefill(int[] plants, int capA, int capB) {
+        int currCapA= capA;
+        int currCapB= capB;
+
+        int left = 0;
+        int right = plants.length-1;
+        int count = 0;
+
+        while(left<=right){
+            if(left == right){
+                if(plants[left]>Math.max(currCapA,currCapB)){
+                    count++;
+                }
+            }else{
+                if(plants[left]>currCapA){
+                    count++;
+                    currCapA = capA;
+                }
+                    currCapA -= plants[left];
+                
+                
+                
+                if(plants[right]>currCapB){
+                    count++;
+                    currCapB = capB;
+                }
+                    currCapB -= plants[right];
+                
+                
+            }
+
+            left++;
+            right--;
+        }
+
+        return count;
+    }
+}
