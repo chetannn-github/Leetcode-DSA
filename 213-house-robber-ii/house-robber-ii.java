aa3c718109b428da1a22a5f32fd014aa49af5b9a
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-24 08:23:37 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (38.13%) - LeetSync

---
+++ 213-house-robber-ii/house-robber-ii.java
@@ -0,0 +1,33 @@
+class Solution {
+    int[] dp ;
+    public int rob(int[] nums) {
+        int n = nums.length;
+
+        if(n==1){
+            return nums[0];
+        }
+        dp = new int[n];
+        
+        Arrays.fill(dp,-1);
+        int opt1 = solve(nums,0,n-2);
+
+        Arrays.fill(dp,-1);
+        int opt2 = solve(nums,1,n-1);
+        
+        return Math.max(opt1,opt2);
+    }
+
+    public int solve(int[] nums, int start, int end){
+        if(start > end){
+            return 0;
+        }
+        if(dp[start]==-1){
+            int option1 = nums[start] + solve(nums, start+2,end);
+            int option2 =  solve(nums, start+1,end);
+            dp[start] = Math.max(option1, option2);
+        }
+        
+        return dp[start];
+
+    }
+}
