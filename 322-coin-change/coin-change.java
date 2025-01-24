bcfac20d91ef72c265216f14cdcb67d7eb3c75cd
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-24 18:48:18 +0530

    Time: 30 ms (23.71%) | Memory: 44.9 MB (36.75%) - LeetSync

---
+++ 322-coin-change/coin-change.java
@@ -0,0 +1,40 @@
+class Solution {
+    
+    int[] dp = new int[10001];
+    public int coinChange(int[] coins, int amount) {
+        Arrays.fill(dp,-1);
+        int result = solve(coins, amount); 
+        return result == Integer.MAX_VALUE ? -1 : result;
+       
+    }
+
+    public int solve(int[] coins, int amount){
+        if(amount==0){
+            return 0; 
+        }
+        if(amount<0){
+            return Integer.MAX_VALUE;
+        }
+
+        if(dp[amount] != -1){
+            return dp[amount];
+        }
+
+        int min = Integer.MAX_VALUE;
+
+        for(int i=0; i<coins.length; i++){
+            int coinsRequired = solve(coins, amount - coins[i]);
+
+            if(coinsRequired != Integer.MAX_VALUE){
+                min = Math.min(coinsRequired +1 , min);
+            }
+           
+
+        }
+        dp[amount] = min;
+
+        return dp[amount];
+    }
+}
+
+
