1a9c9c687eebcffbbe46689dd4904ddd51eb0a07
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-24 20:11:17 +0530

    Time: 3 ms (8.57%) | Memory: 42.6 MB (7.17%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,47 @@
+class Solution {
+    Set<String> hs = new HashSet<>();
+    int n ;
+    int dp[] = new int[101];
+
+    public int numDecodings(String s) {
+        for (int i = 1; i <= 26; i++) {
+            String key = String.valueOf(i);
+            hs.add(key);
+        }
+        Arrays.fill(dp,-1);
+        n = s.length();
+        
+        return solve(s,0);
+        
+    }
+
+    public int solve(String s, int start){
+        
+        if(start == n){
+            return 1;
+        }
+
+        if(dp[start] != -1){
+            return dp[start];
+        }
+
+        String one = s.substring(start, start+1);
+        int r1 = 0;
+        int r2 = 0;
+        if(hs.contains(one)){
+           r1 = solve(s,start+1);
+        }
+
+        dp[start] = r1;
+        if(start+1 == n){
+            return r1;
+        }
+
+        String two = s.substring(start, start+2);
+        if(hs.contains(two)){
+            r2 = solve(s,start+2);
+        }
+        dp[start] = r1+r2;
+        return r1+r2;
+    }
+}
