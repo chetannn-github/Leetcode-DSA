d7f3524182d157b8af5a5c5b00ab55e84a4a6350
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-25 18:15:18 +0530

    Time: 356 ms (14.00%) | Memory: 76.5 MB (18.68%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -0,0 +1,62 @@
+// recursion TLE
+// class Solution {
+//     public int lengthOfLIS(int[] nums) {
+//         return solve(nums,Integer.MIN_VALUE,0);
+//     }
+
+//     public int solve(int[] nums, int last, int start){
+//         if(start >= nums.length){
+//             return 0;
+//         }
+
+//         int taken =0;
+//         if(nums[start]> last){
+//             taken = 1 + solve(nums,nums[start], start+1);
+//         }
+
+//         int skip  = solve(nums,last, start+1);
+
+
+//         return Math.max(skip, taken);
+        
+
+//     }
+// }
+
+
+class Solution {
+    int dp[][];
+    public int lengthOfLIS(int[] nums) {
+        dp = new int[nums.length][nums.length];
+
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
+
+        return solve(nums,-1,0);
+    }
+
+    public int solve(int[] nums, int last, int start){
+        if(start >= nums.length){
+            return 0;
+        }
+        if(last!=-1 && dp[start][last] != -1){
+            return dp[start][last];
+        }
+        int taken =0;
+        if(last == -1 || nums[start]> nums[last]){
+            taken = 1 + solve(nums,start, start+1);
+        }
+
+        int skip  = solve(nums,last, start+1);
+
+        if(last!=-1){
+            dp[start][last] = Math.max(skip, taken);
+        }
+
+
+        return Math.max(skip, taken);
+        
+
+    }
+}
