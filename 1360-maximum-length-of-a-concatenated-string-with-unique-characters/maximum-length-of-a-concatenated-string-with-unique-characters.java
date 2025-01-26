22c066d2c7a842d27e14b7892c7508352d8ded89
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-01-26 19:08:43 +0530

    Time: 8 ms (78.11%) | Memory: 44.8 MB (57.63%) - LeetSync

---
+++ 1360-maximum-length-of-a-concatenated-string-with-unique-characters/maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -0,0 +1,52 @@
+class Solution {
+    int n;
+    public int maxLength(List<String> arr) {
+        HashSet<Character> temp = new HashSet<>();
+        n = arr.size();
+    
+        return solve(arr,0, temp);
+    }
+
+    public int solve(List<String> arr, int start,HashSet<Character> temp){
+        if(start == n){
+            return 0;
+        }
+
+
+        // take  it -- with some condition kii koi bhi letter phele se toh nhi h
+        boolean isPresent = true;
+        String curr = arr.get(start);
+        int length = curr.length();
+        HashSet<Character> checkDup = new HashSet<>();
+        for(int i=0; i<length; i++){
+            char ch = curr.charAt(i);
+            if(temp.contains(ch)){
+                isPresent = false;
+                break;
+            }
+
+            if(checkDup.contains(ch)){
+                isPresent = false;
+                break;
+            }else{
+                checkDup.add(ch);
+            }
+        }
+        int taken = 0;
+        if(isPresent){
+            for(int i=0; i<curr.length(); i++){
+                temp.add(curr.charAt(i));
+            }
+            taken = length + solve(arr,start+1,temp);
+            for(int i=0; i<curr.length(); i++){
+                temp.remove(curr.charAt(i));
+            }
+        }
+        // dont take 
+        int notTaken = solve(arr,start+1,temp);
+
+        return Math.max(taken , notTaken);
+        
+
+    }
+}
