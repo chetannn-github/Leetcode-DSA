b25a48ba2d55a505ded4cacddd71f3a917287c18
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-15 16:55:51 +0530

    Time: 10 ms (77.67%) | Memory: 55.1 MB (66.45%) - LeetSync

--- 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/count-number-of-nice-subarrays.java
@@ -1,20 +1,45 @@
+// class Solution {
+
+//     public int numberOfSubarrays(int[] nums, int k) {
+//         int currSum = 0; 
+//         int  ans = 0;
+//         Map<Integer, Integer> hm = new HashMap<>();
+//         hm.put(0,1);
+
+//         for (int i = 0; i < nums.length; i++) {
+//             currSum += nums[i] &1 ;
+//             if (hm.containsKey(currSum - k)) {
+//                 ans = ans + hm.get(currSum - k);
+//             }
+//             hm.put(currSum, hm.getOrDefault(currSum, 0) + 1);
+//         }
+
+//         return ans;
+//     }
+// }
+
+
 class Solution {
 
     public int numberOfSubarrays(int[] nums, int k) {
-        int currSum = 0; 
-        int  ans = 0;
-        Map<Integer, Integer> hm = new HashMap<>();
-        hm.put(0,1);
+        return atMost(nums, k) - atMost(nums, k - 1);
+    }
 
-        for (int i = 0; i < nums.length; i++) {
-            currSum += nums[i] &1 ;
-            if (hm.containsKey(currSum - k)) {
-                ans = ans + hm.get(currSum - k);
+    private int atMost(int[] nums, int k) {
+        int size = 0;
+        int ans = 0;
+        int start = 0;
+
+        for (int end = 0; end < nums.length; end++) {
+            size += nums[end] % 2;
+
+            while (size > k) {
+                size -= nums[start] % 2;
+                start++;
             }
-            hm.put(currSum, hm.getOrDefault(currSum, 0) + 1);
+            ans += end - start + 1;
         }
 
         return ans;
     }
 }
-
