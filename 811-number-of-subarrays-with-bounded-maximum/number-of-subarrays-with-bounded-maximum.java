e5704d197d93d1ff3e2edf824db5c5ba4d947fd7
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-15 17:13:26 +0530

    Time: 4 ms (23.69%) | Memory: 56.6 MB (73.60%) - LeetSync

--- 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.java
+++ 811-number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.java
@@ -1,12 +1,39 @@
+// class Solution {
+//     public int numSubarrayBoundedMax(int[] nums, int left, int right) {
+//         // right se bade wale nikal lo aur left se bade wale nikal krr subtract krr do!!
+       
+//         return countSubarraysWithMax(nums,left) - countSubarraysWithMax( nums, right+1);
+//     }
+
+//     public int countSubarraysWithMax(int[] nums, int limit) {
+        
+//         int start = 0;
+//         int subarrays = 0;
+//         int n = nums.length;
+
+
+//         for(int end = 0; end<n; end++){
+
+//             if(nums[end]>=limit){
+//                 subarrays += (end-start+1) * (n - end);
+//                 start = end+1;
+//             }
+
+
+//         }
+
+//         return subarrays;
+//     }
+// }
+
 class Solution {
     public int numSubarrayBoundedMax(int[] nums, int left, int right) {
-        // right se bade wale nikal lo aur left se bade wale nikal krr subtract krr do!!
+        // right se chote wale nikal lo aur left se chote wale nikal krr subtract krr do!!
        
-        return countSubarraysWithMax(nums,left) - countSubarraysWithMax( nums, right+1);
+        return countSubarraysWithMin(nums,right) - countSubarraysWithMin( nums, left-1);
     }
 
-    public int countSubarraysWithMax(int[] nums, int limit) {
-        
+    public int countSubarraysWithMin(int[] nums, int limit) {
         int start = 0;
         int subarrays = 0;
         int n = nums.length;
@@ -14,12 +41,12 @@ class Solution {
 
         for(int end = 0; end<n; end++){
 
-            if(nums[end]>=limit){
-                subarrays += (end-start+1) * (n - end);
-                start = end+1;
+            while(end<n && nums[end]>limit){
+                start = ++end;
             }
-
-
+            if(end<n && nums[end]<=limit){
+                subarrays += end - start + 1;
+            }
         }
 
         return subarrays;
