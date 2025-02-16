d3dd00b09c91ce2612ab9936ff0c2716979421e9
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-16 11:51:52 +0530

    Time: 43 ms (21.77%) | Memory: 45 MB (6.78%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.java
@@ -22,3 +22,13 @@ class Solution {
         return queue.peek();
     }
 }
+
+// class Solution {
+//     public int findTheWinner(int n, int k) {
+//         int winner=0;
+//         for (int i = 1; i <= n; i++) {
+//             winner = (winner + k) % i;
+//         }
+//         return winner + 1;
+//     }
+// }
