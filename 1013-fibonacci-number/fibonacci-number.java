93e453803ea485dbb70c8799cbabd7cac94285ea
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-16 09:14:18 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (10.74%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -1,20 +1,36 @@
-class Solution {
+// class Solution {
     
-    public int fib(int n) {
-        int dp[];
-        dp = new int[n+1];
-        return solve(n,dp);
+//     public int fib(int n) {
+//         int dp[];
+//         dp = new int[n+1];
+//         return solve(n,dp);
+//     }
+
+//     public int solve(int n,int[] dp){
+//         if(n==0 || n==1){
+//             return dp[n] =  n;
+//         }
+
+//         if(dp[n]!=0){
+//             return dp[n];
+//         }
+
+//         return dp[n] = fib(n-1) + fib(n-2);
+//     }
+// }
+
+
+class Solution {
+    public int fibo(int n,int[] dp){
+        if(n<2) return n;
+        dp[0]=0;
+        dp[1]=1;
+        if(dp[n]!=0) return dp[n];
+        return dp[n]= fibo(n-1,dp)+fibo(n-2,dp);
     }
-
-    public int solve(int n,int[] dp){
-        if(n==0 || n==1){
-            return dp[n] =  n;
-        }
-
-        if(dp[n]!=0){
-            return dp[n];
-        }
-
-        return dp[n] = fib(n-1) + fib(n-2);
+    public int fib(int n) {
+       // if(n<2) return n;
+        int[] dp=new int[n+1];
+        return fibo(n,dp);
     }
 }
