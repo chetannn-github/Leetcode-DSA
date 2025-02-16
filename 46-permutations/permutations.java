2209d3724585b9cb96a198276d7af82a20fba22c
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-16 08:44:23 +0530

    Time: 2 ms (40.62%) | Memory: 45.1 MB (6.09%) - LeetSync

--- 46-permutations/permutations.java
+++ 46-permutations/permutations.java
@@ -1,26 +1,26 @@
 class Solution {
-    private List<List<Integer>> result;
-    private Set<Integer> st;
+    List<List<Integer>> result;
+    Set<Integer> st;
 
-    public void solve(List<Integer> temp, int[] nums) {
-        if (temp.size() == nums.length) {
-            result.add(new ArrayList<>(temp));
-            return;
-        }
+    // public void solve(List<Integer> temp, int[] nums) {
+    //     if (temp.size() == nums.length) {
+    //         result.add(new ArrayList<>(temp));
+    //         return;
+    //     }
 
-        for (int num : nums) {
-            if (!st.contains(num)) {
-                temp.add(num);
-                st.add(num);
+    //     for (int num : nums) {
+    //         if (!st.contains(num)) {
+    //             temp.add(num);
+    //             st.add(num);
 
-                solve(temp, nums);
+    //             solve(temp, nums);
 
-                st.remove(num);
-                temp.remove(temp.size() - 1);
-            }
-        }
-    }
-
+    //             st.remove(num);
+    //             temp.remove(temp.size() - 1);
+    //         }
+    //     }
+    // }
+    
     public List<List<Integer>> permute(int[] nums) {
         result = new ArrayList<>();
         st = new HashSet<>();
@@ -30,5 +30,21 @@ class Solution {
 
         return result;
     }
+    public void solve(List<Integer> temp, int[] nums) {
+        if (temp.size() == nums.length) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
 
+        for(int i=0; i<nums.length; i++){
+            if (!st.contains(i)) {
+                temp.add(nums[i]);
+                st.add(i);
+
+                solve(temp, nums);
+                temp.remove(temp.size()-1);
+                st.remove(i);
+            }
+        }
+    }
 }
