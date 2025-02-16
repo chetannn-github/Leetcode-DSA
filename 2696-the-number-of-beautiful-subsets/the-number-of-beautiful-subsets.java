adab56a6628e82f1e1520713781eae48268429ba
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-16 19:05:08 +0530

    Time: 264 ms (90.25%) | Memory: 45 MB (61.64%) - LeetSync

--- 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.java
+++ 2696-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.java
@@ -1,3 +1,7 @@
+// mene phele yhh smjhh liya thaa kii hume distinct subsets chaiyee isliye commented wala code uske liyee hhh
+
+// second approach is that why i am storing subsets in array me hashmap me krugaa milegaa bhii jaldi aur jayada dikkt bhii nhii hhh
+
 // class Solution {
 //     int result = 0;
 //     int n;
@@ -6,19 +10,23 @@
 //         // Arrays.sort(nums);
 //         List<Integer> temp = new ArrayList<>();
 //         solve(nums,k,temp,0);
-//         return result;
+//         return result-1; // kuki empty wala bhii add hogyaa hh
 
 //     }
 
 //     public void solve(int[] nums, int k, List<Integer> temp, int start){
 //         int size = temp.size();
-//         if(size>0){
-//             result++;
+//         result++;
+//         if(start == n){
+//             return;
 //         }
         
 
 //         for(int i = start; i<n; i++){
-//             if(size ==0 || checkDifference(temp,k , nums[i])){
+//             // if(i>start && nums[i] == nums[i-1]){
+//             //     continue;
+//             // }
+//             if(temp.size() == 0 || checkDifference(temp,k , nums[i])){
 //                 temp.add(nums[i]);
 //                 solve(nums,k,temp,i+1);
 //                 temp.remove(temp.size()-1);
@@ -41,33 +49,29 @@
 class Solution {
     int result = 0;
     int n;
-    
-
     public int beautifulSubsets(int[] nums, int k) {
         n = nums.length;
-        
-       HashMap<Integer,Integer> hm = new HashMap<>();
+        HashMap<Integer,Integer> hm = new HashMap<>();
         solve(nums,k,0,hm);
-        return result;
-
+        return result-1;
     }
 
     public void solve(int[] nums, int k, int start,HashMap<Integer,Integer> hm){
-       
+        result++;
         if(start == nums.length){
             return;
         }
         
-        
         for(int i = start; i<n; i++){
-            if(hm.getOrDefault(nums[i]+k, 0)>0 || hm.getOrDefault(nums[i]-k, 0)>0 ){continue;}
+            boolean isPresent = hm.getOrDefault(nums[i]+k, 0)>0 || hm.getOrDefault(nums[i]-k, 0)>0;
+            if(isPresent){
+                continue;
+            }
                
             hm.put(nums[i], hm.getOrDefault(nums[i],0)+1);
-            result++;
-            solve(nums,k,i+1,hm);
-            hm.put(nums[i], hm.getOrDefault(nums[i],0)-1);
-                
             
+            solve(nums,k,i+1,hm);
+            hm.put(nums[i], hm.getOrDefault(nums[i],0)-1);  
         }
     }
 
