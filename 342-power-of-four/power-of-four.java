5bc62b8009ce9d158cc601a0e4c979c2db26775b
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-16 07:38:27 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (34.19%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,10 +1,23 @@
+// class Solution {
+//     public boolean isPowerOfFour(int n) {
+//         if(n<=0){
+//             return false;
+//         }else if(n==1){
+//             return true;
+//         }
+//         return n%4 ==0 && isPowerOfFour(n/4);
+//     }
+// }
+
+
+
 class Solution {
     public boolean isPowerOfFour(int n) {
-        if(n<=0){
+
+        if(n<=0)
+        {
             return false;
-        }else if(n==1){
-            return true;
         }
-        return n%4 ==0 && isPowerOfFour(n/4);
+       return ((n & (n-1))==0 && (n-1)%3==0);
     }
 }
