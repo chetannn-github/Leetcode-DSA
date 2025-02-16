50fe17811a7a6f99c33b232a7f24df220ad2cc82
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-16 07:22:24 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (78.95%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -7,13 +7,31 @@
 //     }
 // }
 
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         if(n<=0){
+//             return false;
+//         }else if(n==1){
+//             return true;
+//         }
+//         return n%2 ==0 && isPowerOfTwo(n/2);
+//     }
+// }
+
 class Solution {
     public boolean isPowerOfTwo(int n) {
+        return solve((long)n,1);
+    }
+
+    public boolean solve(long n, long curr){
         if(n<=0){
             return false;
-        }else if(n==1){
+        }else if(n == curr){
             return true;
+        }else if(n<curr){
+            return false;
         }
-        return n%2 ==0 && isPowerOfTwo(n/2);
+
+        return solve(n,curr*2);
     }
 }
