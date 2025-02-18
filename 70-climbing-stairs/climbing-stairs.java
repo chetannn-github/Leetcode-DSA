f86edff46bd7083ca6870cc61655d1134f3569e0
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-18 19:48:55 +0530

    Time: 0 ms (100.00%) | Memory: 40.4 MB (64.60%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,10 +1,8 @@
 class Solution {
 
     public int climbStairs(int n) { 
-        int[] dp = new int[46];
+        int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-
-        
         return solve(n,dp);
     }
 
