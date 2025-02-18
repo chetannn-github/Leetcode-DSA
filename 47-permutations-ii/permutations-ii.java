b7cf603a6d024c26196d32c8dcc4054d4990eb18
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-18 06:50:43 +0530

    Time: 3 ms (44.15%) | Memory: 45 MB (57.65%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,6 +1,7 @@
 class Solution {
     List<List<Integer>> solutions = new ArrayList<>();
     HashMap<Integer,Integer> hm = new HashMap<>();
+    HashSet<Integer> hs = new HashSet<>();
 
     public List<List<Integer>> permuteUnique(int[] nums) {
         for(int num : nums){
@@ -22,18 +23,44 @@ class Solution {
         for(int i=0; i<nums.length; i++){
             int num = nums[i];
             
-            if((i==0 ||  num!= nums[i-1]) && hm.getOrDefault(num,0)>0){
-                hm.put(num, hm.getOrDefault(num,0)-1);
-                temp.add(num);
+            if((i!=0 &&  num == nums[i-1]) || hm.getOrDefault(num,0)==0){continue;}
 
-                solve(nums, temp);
+            hm.put(num, hm.get(num)-1);
+            temp.add(num);
 
-                hm.put(num, hm.getOrDefault(num,0)+1);
-                temp.remove(temp.size()-1);
+            solve(nums, temp);
 
-            }
+            hm.put(num, hm.get(num)+1);
+            temp.remove(temp.size()-1);
+            
         }
 
        
     }
+
+    // public void solve(int[] nums, List<Integer> temp){
+    //     if(temp.size() == nums.length){
+    //         solutions.add(new ArrayList<>(temp));
+    //         return;
+    //     }
+
+
+    //     for(int i=0; i<nums.length; i++){
+    //         int num = nums[i];
+
+    //         if(temp.size()==0 && i!=0 && nums[i]== nums[i-1]){ 
+    //             continue;
+    //         }else if(!hs.contains(i)){
+    //             hs.add(i);
+    //             temp.add(num);
+
+    //             solve(nums, temp);
+
+    //             hs.remove(i);
+    //             temp.remove(temp.size()-1);
+
+    //         }
+    //     }
+
+    // }
 }
