c0dc9c5fe59d6f7e340d9e45b1652e1483fa2d01
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-18 13:54:18 +0530

    Time: 2 ms (65.73%) | Memory: 42.9 MB (36.91%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
@@ -1,49 +1,14 @@
-// class Solution {
-//     public List<Integer> diffWaysToCompute(String exp) {
-//         List<Integer> solutions = new ArrayList<>();
-//         int n = exp.length();
-
-//         for(int i=0; i<n; i++){
-//             char ch = exp.charAt(i);
-
-//             if(ch=='+' || ch=='-' || ch=='*'){
-//                 List<Integer> left = diffWaysToCompute(exp.substring(0,i));
-//                 List<Integer> right = diffWaysToCompute(exp.substring(i+1));
-
-//                 for(Integer num1 : left){
-//                     for(Integer num2 : right){
-//                         if (ch == '+') solutions.add(num1 + num2);
-//                         else if (ch == '-') solutions.add(num1 - num2);
-//                         else solutions.add(num1 * num2);
-//                     }
-//                 }
-//             }
-//         }
-//         if(solutions.size()==0){
-//             solutions.add(Integer.parseInt(exp));
-//         }
-//         return  solutions;
-//     }
-
-    
-
-// }
-
 class Solution {
     public List<Integer> diffWaysToCompute(String exp) {
-        int n = exp.length();
-        return solve(exp,0,n);
-    }
-
-    public List<Integer> solve(String exp,int start, int end) {
         List<Integer> solutions = new ArrayList<>();
+        int n = exp.length();
 
-        for(int i=start; i<end; i++){
+        for(int i=0; i<n; i++){
             char ch = exp.charAt(i);
 
             if(ch=='+' || ch=='-' || ch=='*'){
-                List<Integer> left = solve(exp,start,i);
-                List<Integer> right = solve(exp,i+1, end);
+                List<Integer> left = diffWaysToCompute(exp.substring(0,i));
+                List<Integer> right = diffWaysToCompute(exp.substring(i+1));
 
                 for(Integer num1 : left){
                     for(Integer num2 : right){
@@ -54,11 +19,47 @@ class Solution {
                 }
             }
         }
-
         if(solutions.size()==0){
-            solutions.add(Integer.parseInt(exp.substring(start,end)));
+            solutions.add(Integer.parseInt(exp));
         }
-        return solutions;
+        return  solutions;
     }
 
+    
+
 }
+
+// class Solution {
+//     public List<Integer> diffWaysToCompute(String exp) {
+//         int n = exp.length();
+        
+//         return solve(exp,0,n);
+//     }
+
+//     public List<Integer> solve(String exp,int start, int end) {
+//         List<Integer> solutions = new ArrayList<>();
+
+//         for(int i=start; i<end; i++){
+//             char ch = exp.charAt(i);
+
+//             if(ch=='+' || ch=='-' || ch=='*'){
+//                 List<Integer> left = solve(exp,start,i);
+//                 List<Integer> right = solve(exp,i+1, end);
+
+//                 for(Integer num1 : left){
+//                     for(Integer num2 : right){
+//                         if (ch == '+') solutions.add(num1 + num2);
+//                         else if (ch == '-') solutions.add(num1 - num2);
+//                         else solutions.add(num1 * num2);
+//                     }
+//                 }
+//             }
+//         }
+
+//         if(solutions.size()==0){
+//             solutions.add(Integer.parseInt(exp.substring(start,end)));
+//         }
+//         return solutions;
+//     }
+
+// }
