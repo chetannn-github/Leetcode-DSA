32f1b0d8b36577700017c7fe2fb8a047948ca58c
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-18 07:48:22 +0530

    Time: 5 ms (96.54%) | Memory: 51.1 MB (69.37%) - LeetSync

--- 491-non-decreasing-subsequences/non-decreasing-subsequences.java
+++ 491-non-decreasing-subsequences/non-decreasing-subsequences.java
@@ -6,34 +6,29 @@ class Solution {
         List<Integer> temp = new ArrayList<>();
 
         solve(nums,temp,0);
-        // System.out.print(result);
-
         return result;
     }
 
     public void solve(int[] nums, List<Integer> temp, int start){
         int size = temp.size();
-        
         if(size>=2){
-            if(temp.get(size-1) < temp.get(size-2) ){
-                return;
-            }else{
-                result.add(new ArrayList<>(temp));
-            }
-            
-            
+            result.add(new ArrayList<>(temp));   
         }
         
         HashSet<Integer> hs = new HashSet<>();
         for(int i=start; i<nums.length; i++){
+            
             if(hs.contains(nums[i])){
                 continue;
             }
-                
+            
+            if(size!=0 && temp.get(size-1) > nums[i]){
+                continue;
+            }
+            hs.add(nums[i]);
             temp.add(nums[i]);
-hs.add(nums[i]);
-            solve(nums,temp,i+1);
 
+            solve(nums,temp,i+1);
             temp.remove(temp.size()-1);
             
             
