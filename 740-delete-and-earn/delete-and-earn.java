d4dd5d3e42ad39a42db3544281c51f47759c30cb
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-20 07:11:39 +0530

    Time: 5 ms (63.43%) | Memory: 46.2 MB (11.78%) - LeetSync

--- 740-delete-and-earn/delete-and-earn.java
+++ 740-delete-and-earn/delete-and-earn.java
@@ -1,53 +1,47 @@
 class Solution {
-    
     int n;
     int dp[];
-    int unique[] ;
-    HashMap<Integer,Integer> hm = new HashMap<>();
-
     public int deleteAndEarn(int[] nums) {
-     for(int num : nums) {
-            hm.put(num, hm.getOrDefault(num, 0) + num);
-        }
-
-        n = hm.size();
-        unique = new int[n];
-        int idx = 0;
-
-        for(int key: hm.keySet()){
-            unique[idx] = key;
-            idx++;
-        }
-
-        Arrays.sort(unique);
+        n = nums.length;
+        Arrays.sort(nums);
         dp = new int[n];
         Arrays.fill(dp, -1);
-
-        return solve(0);
+        return solve(nums,0);
     }
 
-    public int solve(int start) {
-        if (start >= n) {
+    
+
+    public int solve(int[] nums, int start){
+        if(start>=n){
             return 0;
         }
-
-        if (dp[start] != -1){
+        if(dp[start]!= -1){
             return dp[start];
         }
+        
+        // choice one  --> skip
+        int opt1 = solve(nums, start+1);
 
-        int skip = solve(start + 1);
+        // choice one  --> use it
+        int validity = nums[start]+2;
+        int j = start;
+        int opt2 = 0;
 
-        int currNum = unique[start];
-        int take = hm.get(currNum);
-        int nextidx = start + 1;
-
-        while (nextidx < n && unique[nextidx] == currNum + 1 ){
-            nextidx++;
+        while(j<n && nums[j] == nums[start]){
+            opt2 += nums[start];
+            j++;
+        }
+        while(j<n && nums[j]<validity){
+            j++;
         }
 
-        take += solve(nextidx);
+        opt2 += solve(nums,j);
+       
 
-        return dp[start] = Math.max(skip, take);
+        
+
+        return dp[start] =  Math.max(opt1,opt2);
+        
     }
 }
 
