030b5e293ac657cae39b59fa100ae24c86552b0b
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-20 07:27:18 +0530

    Time: 79 ms (75.60%) | Memory: 74 MB (22.49%) - LeetSync

--- 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.java
+++ 3437-maximum-total-damage-with-spell-casting/maximum-total-damage-with-spell-casting.java
@@ -49,59 +49,53 @@
 // }
 
 
-
-import java.util.*;
-
 class Solution {
     int n;
     long[] dp;
-    int[] unique;
-    Map<Integer, Long> hm = new HashMap<>();
+   
 
     public long maximumTotalDamage(int[] nums) {
+        n = nums.length;
         
-        for (int num : nums) {
-            hm.put(num, hm.getOrDefault(num, 0L) + num);
-        }
-
-        n = hm.size();
-        unique = new int[n];
-        int idx = 0;
-
-        for (int key: hm.keySet()){
-            unique[idx] = key;
-            idx++;
-        }
-
-        Arrays.sort(unique);
+        Arrays.sort(nums);
         dp = new long[n];
         Arrays.fill(dp, -1);
 
-        return solve(0);
+        return solve(nums,0);
     }
 
-    public long solve(int start) {
-        if (start >= n) {
+    public long solve(int[] nums, int start){
+        if(start>=n){
             return 0;
         }
 
-        if (dp[start] != -1){
+
+        if(dp[start]!= -1){
             return dp[start];
         }
+        
+        // choice one  --> skip
+        
 
-        long skip = solve(start + 1);
+        // choice one  --> use it
+        int validity = nums[start]+3;
+        int j = start;
+        long opt2 = 0;
 
-        int currNum = unique[start];
-        long take = hm.get(currNum);
-        int nextidx = start + 1;
+        while(j<n && nums[j] == nums[start]){
+            opt2 += nums[start];
+            j++;
+        }
+        long opt1 = solve(nums, j);
 
-        while (nextidx < n && (unique[nextidx] == currNum + 1 || unique[nextidx] == currNum + 2)) {
-            nextidx++;
+        while(j<n && nums[j]<validity){
+            j++;
         }
 
-        take += solve(nextidx);
-
-        return dp[start] = Math.max(skip, take);
+        opt2 += solve(nums,j);
+       
+        return dp[start] =  Math.max(opt1,opt2);
+        
     }
 }
 
