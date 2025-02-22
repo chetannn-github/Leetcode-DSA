a47b82a4949908f2a7dba9d4f5be2dda4aecc348
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-22 19:58:37 +0530

    Time: 21 ms (5.11%) | Memory: 46.7 MB (6.87%) - LeetSync

---
+++ 36-valid-sudoku/valid-sudoku.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValidSudoku(char[][] board) {
+        HashSet<String> hs = new HashSet<>();
+
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+                char ch = board[i][j];
+                if(ch =='.') continue;
+                int boxX = i/3;
+                int boxY = j/3; 
+                String idx = "*"+ch+"*";
+                if(hs.contains(i+idx) || hs.contains(idx+j) || hs.contains("!"+idx+"!"+boxY+"$"+boxX)){
+                    return false;
+                }
+                hs.add(i+idx);
+                hs.add(idx+j);
+                hs.add("!"+idx+"!"+boxY+"$"+boxX);
+            }
+        }
+        return true;
+    }
+}
