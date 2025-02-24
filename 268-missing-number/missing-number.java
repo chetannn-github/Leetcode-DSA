522db45f05b28f8fa23712539af7807d113434d7
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-24 18:46:28 +0530

    Time: 1 ms (36.09%) | Memory: 45.8 MB (13.03%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -14,13 +14,48 @@
 
 
   
+// class Solution {
+//     public int missingNumber(int[] nums) {
+//         int missing = nums.length;
+//         for (int i=0;i<nums.length;i++){
+//             missing ^= nums[i] ^ i;
+//         }
+//         return missing;
+
+//     }
+// }
+
+
+// method 3 if numbers are from 0 to n then cyclic sort
+
 class Solution {
     public int missingNumber(int[] nums) {
-        int missing = nums.length;
-        for (int i=0;i<nums.length;i++){
-            missing ^= nums[i] ^ i;
+        int start = 0;
+        int n = nums.length;
+
+        while(start<n){
+            if(nums[start] == n){
+                start++;
+            }else if( nums[start] != start){
+                // swap  b/w nums[start] index and start index
+                int idx = nums[start];
+                nums[start] ^= nums[idx];
+                nums[idx] ^= nums[start];
+                nums[start] ^= nums[idx];
+            }else{
+                start++;
+            }
         }
-        return missing;
+
+
+        for(int i=0; i<n; i++){
+            if(nums[i]!= i){
+                return i;
+            }
+        }
+
+        return n;
 
     }
 }
+
