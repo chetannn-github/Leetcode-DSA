2d3bb9391d098ceb642d746d890a99c066f100ca
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-24 22:02:58 +0530

    Time: 1 ms (99.89%) | Memory: 42.2 MB (80.71%) - LeetSync

---
+++ 67-add-binary/add-binary.java
@@ -0,0 +1,93 @@
+class Solution {
+    public String addBinary(String a, String b) {
+        int l1 = a.length()-1;
+        int l2 = b.length()-1;
+        StringBuilder sb = new StringBuilder();
+        int carry = 0;
+        
+        while(l1>=0 || l2>=0){
+            
+            if(l1<0){
+                if(b.charAt(l2)=='1'){
+                    if(carry == 1){
+                        sb.append("0");
+                    }else{
+                        sb.append("1");
+                    }
+                }else{
+                    if(carry == 1){
+                        sb.append("1");
+                        carry = 0;
+                    }else{
+                        sb.append("0");
+                        carry = 0;
+                    }
+                }
+            }else if(l2<0){
+                if(a.charAt(l1)=='1'){
+                    if(carry == 1){
+                        sb.append("0");
+                    }else{
+                        sb.append("1");
+                    }
+                }else{
+                    if(carry == 1){
+                        sb.append("1");
+                        carry = 0;
+                    }else{
+                        sb.append("0");
+                        carry = 0;
+                    }
+                }
+            }else{
+                if(carry == 1){
+                    if(a.charAt(l1) == '1'){
+                        if(b.charAt(l2)== '1'){
+                            sb.append("1");
+                        }else{
+                            sb.append("0");
+                        }
+                    }else{
+                        if(b.charAt(l2)== '1'){
+                            sb.append("0");
+                            carry = 1;
+                        }else{
+                            sb.append("1");
+                            carry = 0;
+                        }
+                    }
+                }else{
+                    if(a.charAt(l1) == '1'){
+                        if(b.charAt(l2)== '1'){
+                            sb.append("0");
+                            carry = 1;
+                        }else{
+                            sb.append("1");
+                        }
+                    }else{
+                        if(b.charAt(l2)== '1'){
+                            sb.append("1");
+                        }else{
+                            sb.append("0");
+                        }
+                    }
+                }
+                
+            }
+
+            l1--;
+            l2--;
+
+        }
+
+        if(carry==1){
+                sb.append("1");
+        }
+
+
+        return sb.reverse().toString();
+
+    }
+
+   
+}
