9ca4757f3878ae52b37b71f825b5976d8415d4c3
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-26 11:06:58 +0530

    Time: 3 ms (44.79%) | Memory: 62.3 MB (56.07%) - LeetSync

---
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean doesValidArrayExist(int[] nums) {
+        // ek bar start 0 se krke dekhte hh
+
+        int first = 0;
+        int next = nums[0];
+        int n = nums.length;
+
+        if(n==1) return nums[0]==0;
+
+        for(int i=1; i<n-1;i++){
+            next ^= nums[i];
+        }
+
+        if((first ^ next) == nums[n-1]){
+            return true;
+        }
+
+        first = 1;
+        next = 1 ^ nums[0];
+
+        for(int i=1; i<n-1;i++){
+            next ^= nums[i];
+        }
+        if((first ^ next) == nums[n-1]){
+            return true;
+        }
+        
+        return false;
+    }
+}
