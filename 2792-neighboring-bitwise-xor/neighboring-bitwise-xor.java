f3518dac4d937404a787c1d659349b6d351f234b
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-26 11:10:29 +0530

    Time: 2 ms (100.00%) | Memory: 63 MB (17.01%) - LeetSync

--- 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
+++ 2792-neighboring-bitwise-xor/neighboring-bitwise-xor.java
@@ -1,31 +1,42 @@
-class Solution {
-    public boolean doesValidArrayExist(int[] nums) {
-        // ek bar start 0 se krke dekhte hh
+// class Solution {
+//     public boolean doesValidArrayExist(int[] nums) {
+//         // ek bar start 0 se krke dekhte hh
 
-        int first = 0;
-        int next = nums[0];
-        int n = nums.length;
+//         int first = 0;
+//         int next = nums[0];
+//         int n = nums.length;
 
-        if(n==1) return nums[0]==0;
+//         if(n==1) return nums[0]==0;
 
-        for(int i=1; i<n-1;i++){
-            next ^= nums[i];
-        }
+//         for(int i=1; i<n-1;i++){
+//             next ^= nums[i];
+//         }
 
-        if((first ^ next) == nums[n-1]){
-            return true;
-        }
+//         if((first ^ next) == nums[n-1]){
+//             return true;
+//         }
 
-        first = 1;
-        next = 1 ^ nums[0];
+//         first = 1;
+//         next = 1 ^ nums[0];
 
-        for(int i=1; i<n-1;i++){
-            next ^= nums[i];
-        }
-        if((first ^ next) == nums[n-1]){
-            return true;
-        }
+//         for(int i=1; i<n-1;i++){
+//             next ^= nums[i];
+//         }
+//         if((first ^ next) == nums[n-1]){
+//             return true;
+//         }
         
-        return false;
+//         return false;
+//     }
+// }
+
+class Solution {
+
+    public boolean doesValidArrayExist(int[] derived) {
+        int XOR = 0;
+        for (int element : derived) {
+            XOR = XOR ^ element;
+        }
+        return XOR == 0;
     }
 }
