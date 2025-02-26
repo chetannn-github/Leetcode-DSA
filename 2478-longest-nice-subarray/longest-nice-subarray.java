e988873dee9c8c75c6d3bd45d6576aa9795dc690
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-26 08:37:48 +0530

    Time: 6 ms (17.82%) | Memory: 55.3 MB (93.40%) - LeetSync

---
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int longestNiceSubarray(int[] nums) {
+        int start = 0;
+        int n = nums.length;
+        int maxLength = 1;
+
+        for(int end = 1; end<n; end++){
+            // sbse ensure krnaa padegaa jb tum nyaa member jod rhoo hoo toh sbhii
+            // doston se puchoo are you finee????
+            // end se puchna shuru krugaa jisme mna kiyaa usse aur usse phele waalo se dosti khtmm
+
+            for(int i = end-1; i>=start; i--){
+                if((nums[i] & nums[end] )!= 0){
+                    start = i+1;
+                    break;
+                }
+            }
+            
+
+            maxLength = Math.max(maxLength, end-start+1);
+        }
+
+        return maxLength;
+    }
+}
