59ba1f600426fd6a8f37c1d05a700e567eae3bf7
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-02-27 17:37:33 +0530

    Time: 173 ms (12.66%) | Memory: 54.8 MB (31.00%) - LeetSync

---
+++ 1748-best-team-with-no-conflicts/best-team-with-no-conflicts.java
@@ -0,0 +1,59 @@
+class Solution {
+    int data[][];
+    int n; 
+    int[][] dp;
+    public int bestTeamScore(int[] scores, int[] ages) {
+        n = scores.length;
+        data = new int[n][2];
+        dp = new int[n][n];
+
+        
+        for(int i=0;i<n;i++){
+            data[i][0] = ages[i];
+            data[i][1] = scores[i];
+            Arrays.fill(dp[i], -1);
+        }
+
+        Arrays.sort(data, Comparator
+            .comparingInt((int[] a) -> a[0]) 
+            .thenComparingInt(a -> a[1])
+        );
+
+        return solve(-1,0);
+    }
+
+    public int solve(int lastIdx, int start){
+        if(start>=n){
+            return 0;
+        }
+
+        int take = 0;
+        int score = data[start][1];
+        int age = data[start][0];
+
+        if(lastIdx!= -1 && dp[lastIdx][start] != -1){
+            return dp[lastIdx][start];
+        }
+
+        if(lastIdx != -1){
+            int lastScore = data[lastIdx][1];
+            int lastAge = data[lastIdx][0];
+            int lastIdxForNextCall = score > lastScore ? start : lastIdx;
+
+            if(lastAge==age){
+                take = score + solve( lastIdxForNextCall, start+1);
+            }else if(score >= lastScore){
+                take = score + solve(start , start+1);
+            }
+        }else{
+            take = score + solve(start , start+1);
+        }
+        
+        int skip = solve(lastIdx,start+1);
+
+        if(lastIdx!= -1){
+            dp[lastIdx][start] = Math.max(take,skip);
+        }
+        return Math.max(take,skip);
+    }
+}
