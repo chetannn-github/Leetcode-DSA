bbaa6c9be32fd1ed521839836eece70751b0e8eb
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-03-04 16:50:12 +0530

    Time: 755 ms (12.06%) | Memory: 49.7 MB (5.11%) - LeetSync

--- 647-palindromic-substrings/palindromic-substrings.java
+++ 647-palindromic-substrings/palindromic-substrings.java
@@ -1,58 +1,46 @@
 class Solution {
     int n;
-    HashMap<String,Integer> dp;
+    int[][] dp;
     public int countSubstrings(String s) {
         n = s.length();
-        dp = new HashMap<>();
+        
+        dp = new int[n][n];
+        for(int[] row : dp){
+            Arrays.fill(row,-1);
+        }
 
-        StringBuilder sb = new StringBuilder();  
         int ans = 0;
         for(int i=0; i<n; i++){
-            ans += solve(s,sb,i);
+            ans += solve(s,i,i);
         }
         return ans;
     }
 
-    public int solve(String s,StringBuilder sb, int start){
-        int size = sb.length(); 
-        String key = sb.toString();
-        
-        int max;
-        if(dp.containsKey(key)){
-            max = dp.get(key);
-        }else{
-            max = isPalindrome(key,size);
-        }
-        if(start>=n){
-            return max;
-        }
-       
-        
-        sb.append(s.charAt(start));
-        int take = solve(s,sb,start+1);
-        sb.setLength(size);
-
-        max += take;
-        return max;
-    }
-
-    public int isPalindrome(String s, int size) {
-        int left = 0;
-        int right = size-1;
-        if(size==0){
-            dp.put(s,0);
+    public int solve(String s, int start, int end){
+        int count;
+        if(end>=n){
             return 0;
         }
+        if(dp[start][end]!=-1){
+            return dp[start][end];
+        }
+        count = isPalindrome(s,start,end);
+        
+        int take = solve(s,start,end+1);
+        
+        count += take;
+        return dp[start][end] = count;
+    }
+
+    public int isPalindrome(String s, int left, int right) {
         
         while (left < right) {
             if (s.charAt(left) != s.charAt(right)) {
-                dp.put(s,0);
                 return 0;
             }
             left++;
             right--;
         }
-        dp.put(s,1);
         return 1;
     }
 }
