b2ab549d2dd7ef77bc29d006867ecdd3a1ba84a7
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-03-07 22:01:25 +0530

    Time: 0 ms (100.00%) | Memory: 46.1 MB (22.75%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -1,21 +1,48 @@
+// class Solution {
+//     public int search(int[] nums, int target) {
+//         int start = 0;
+//         int end = nums.length-1;
+
+//         while(end >= start){
+//             int mid = start + ((end - start)>>1) ;
+
+//             if(nums[mid] == target){
+//                 return mid;
+//             }else if(nums[mid] > target){
+//                 end = mid - 1;
+//             }else{
+//                 start = mid + 1;
+//             }
+
+//         }
+
+//         return -1;
+//     }
+// }
+
 class Solution {
     public int search(int[] nums, int target) {
         int start = 0;
         int end = nums.length-1;
 
-        while(end >= start){
-            int mid = start + ((end - start)>>1) ;
-
-            if(nums[mid] == target){
-                return mid;
-            }else if(nums[mid] > target){
-                end = mid - 1;
-            }else{
-                start = mid + 1;
-            }
-
-        }
-
-        return -1;
+        return binarySearch(nums,target, start, end);
     }
+
+
+    public int binarySearch(int[] nums, int target,int start,int end){
+        int mid = start + ((end - start)>>1) ;
+
+        if(start>end) return -1;
+
+        if(nums[mid] == target){
+            return mid;
+        }else if(nums[mid] > target){
+           return binarySearch(nums,target,start,mid-1);
+        }
+        return binarySearch(nums,target,mid+1,end);
+    
+    
+    }
+
+        
 }
