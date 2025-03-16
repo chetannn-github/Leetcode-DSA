9200130796760727676e742b4491c02f207dfaa2
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-03-16 08:36:38 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (32.66%) - LeetSync

--- 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
+++ 1092-maximum-difference-between-node-and-ancestor/maximum-difference-between-node-and-ancestor.java
@@ -1,35 +1,57 @@
+// class Solution {
+//     int maxDiff;
+//     List<Integer> ancestors;
+//     public int maxAncestorDiff(TreeNode root) {
+//         maxDiff = 0;
+//         ancestors = new ArrayList<>();
+//         dfs(root);
+//         return maxDiff;
+//     }
+
+//     public void dfs(TreeNode root){
+//         if(root == null) return ;
+//         if(root.left == null && root.right == null) return;
+
+//         ancestors.add(root.val);
+
+//         if(root.left !=null){
+//             dfs(root.left);
+//             checkDiff(root.left.val);
+
+//         }
+//         if(root.right !=null){
+//             dfs(root.right);
+//             checkDiff(root.right.val);
+//         }
+
+//         ancestors.remove(ancestors.size()-1);
+//     }
+
+//     public void checkDiff(int child){
+//         for(int i=0; i<ancestors.size(); i++){
+//             maxDiff = Math.max(maxDiff,Math.abs(child - ancestors.get(i)));
+//         }
+//     }
+// }
 class Solution {
     int maxDiff;
-    List<Integer> ancestors;
     public int maxAncestorDiff(TreeNode root) {
         maxDiff = 0;
-        ancestors = new ArrayList<>();
-        dfs(root);
+
+        dfs(root,root.val,root.val);
         return maxDiff;
     }
 
-    public void dfs(TreeNode root){
+    public void dfs(TreeNode root,int min,int max){
         if(root == null) return ;
-        if(root.left == null && root.right == null) return;
-
-        ancestors.add(root.val);
-
-        if(root.left !=null){
-            dfs(root.left);
-            checkDiff(root.left.val);
-
-        }
-        if(root.right !=null){
-            dfs(root.right);
-            checkDiff(root.right.val);
+        if(root.left == null && root.right == null){
+            maxDiff = Math.max(maxDiff,max-min);
+            return;
+            
         }
 
-        ancestors.remove(ancestors.size()-1);
-    }
+        if(root.left !=null) dfs(root.left,Math.min(min,root.left.val),Math.max(max,root.left.val));
+        if(root.right !=null) dfs(root.right,Math.min(min,root.right.val),Math.max(max,root.right.val));
 
-    public void checkDiff(int child){
-        for(int i=0; i<ancestors.size(); i++){
-            maxDiff = Math.max(maxDiff,Math.abs(child - ancestors.get(i)));
-        }
     }
 }
