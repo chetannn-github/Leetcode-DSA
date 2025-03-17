b13a7fea64f1371539f453c7648e9e4df5c9fd0c
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-03-17 21:01:53 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (16.18%) - LeetSync

---
+++ 933-increasing-order-search-tree/increasing-order-search-tree.java
@@ -0,0 +1,36 @@
+class Solution {
+    public TreeNode increasingBST(TreeNode root) {
+     List<Integer> list = new ArrayList<>();
+        
+        inorder(root,list);
+        TreeNode newRoot = insert(null,list.get(0));
+        for(int i=1; i<list.size(); i++){
+            insert(newRoot,list.get(i));
+        }
+
+        return newRoot;
+    }
+
+    public void inorder(TreeNode root,List<Integer> list){
+        if(root == null ) return;
+
+        inorder(root.left,list);
+        list.add(root.val);
+        inorder(root.right,list);
+    }
+
+    TreeNode insert(TreeNode root,int val){
+        if(root == null){
+            return new TreeNode(val);
+        }
+
+        if(root.val > val){
+            root.left = insert(root.left,val);
+        }
+        if(root.val < val){
+            root.right = insert(root.right,val);
+        }
+
+        return root;
+    }
+}
