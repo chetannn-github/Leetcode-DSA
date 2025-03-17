44f86c4a9d584114632df904183839b10110a589
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-03-17 20:50:01 +0530

    Time: 1 ms (100.00%) | Memory: 46 MB (7.80%) - LeetSync

--- 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
+++ 653-two-sum-iv-input-is-a-bst/two-sum-iv-input-is-a-bst.java
@@ -1,16 +1,45 @@
+// class Solution {
+//     HashSet<Integer> hs;
+//     public boolean findTarget(TreeNode root, int k) {
+//         hs = new HashSet<>();
+//         return solve(root,k);
+//     }
+
+//     public boolean solve(TreeNode root, int k){
+//         if(root == null) return false;
+//         if(hs.contains(k-root.val)) return true;
+
+//         hs.add(root.val);
+
+//         return solve(root.left,k) || solve(root.right,k);
+//     }
+// }
+
+
+
 class Solution {
-    HashSet<Integer> hs;
     public boolean findTarget(TreeNode root, int k) {
-        hs = new HashSet<>();
-        return solve(root,k);
+        return dfs(root, root, k);
     }
 
-    public boolean solve(TreeNode root, int k){
-        if(root == null) return false;
-        if(hs.contains(k-root.val)) return true;
+    private boolean dfs(TreeNode node, TreeNode root, int k) {
+        if (node == null) 
+        return false;
 
-        hs.add(root.val);
+        int target = k - node.val;
+        if (target != node.val && search(root, target)) 
+        return true;
 
-        return solve(root.left,k) || solve(root.right,k);
+        return dfs(node.left, root, k) || dfs(node.right, root, k);
+    }
+
+    private boolean search(TreeNode node, int val) {
+        while (node != null) {
+            if (node.val == val) 
+            return true;
+            else if (node.val < val) node = node.right;
+            else node = node.left;
+        }
+        return false;
     }
 }
