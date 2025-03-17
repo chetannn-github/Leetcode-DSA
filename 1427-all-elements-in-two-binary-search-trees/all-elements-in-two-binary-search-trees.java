18feb9a9c69b15b53a5a12dce004bfc98a568174
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-03-17 15:28:18 +0530

    Time: 7 ms (98.54%) | Memory: 46.4 MB (56.36%) - LeetSync

--- 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
+++ 1427-all-elements-in-two-binary-search-trees/all-elements-in-two-binary-search-trees.java
@@ -1,23 +1,46 @@
-class Solution {
-    
-    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        List<Integer> list = new ArrayList<>();
+// class Solution { 
+//     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+//         List<Integer> list = new ArrayList<>();
         
-        inorder(root1,list);
-        inorder(root2,list);
+//         inorder(root1,list);
+//         inorder(root2,list);
 
-        Collections.sort(list);
-        return list;
+//         Collections.sort(list);
+//         return list;
+//     }
+
+//     public void inorder(TreeNode root,List<Integer> list){
+//         if(root == null ) return;
+
+//         inorder(root.left,list);
+//         list.add(root.val);
+//         inorder(root.right,list);
+//     }
+
+// }
+
+class Solution {
+public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        Queue<Integer> queue = new LinkedList<Integer>();
+        List<Integer> res = new ArrayList<Integer>();
+        inOrder1(root1, queue);
+        inOrder2(root2, queue, res);
+        res.addAll(queue);
+        return res;
     }
-
-    public void inorder(TreeNode root,List<Integer> list){
-        if(root == null ) return;
-
-        inorder(root.left,list);
-        list.add(root.val);
-        inorder(root.right,list);
+    void inOrder1(TreeNode node, Queue<Integer> queue) {
+        if(node==null) return;
+        inOrder1(node.left, queue);
+        queue.offer(node.val);
+        inOrder1(node.right, queue);
+    } 
+    void inOrder2(TreeNode node, Queue<Integer> queue, List<Integer> res) {
+        if(node==null) return;
+        inOrder2(node.left, queue, res);
+        while(!queue.isEmpty() && queue.peek()<=node.val) {
+            res.add(queue.poll());
+        }
+        res.add(node.val);
+        inOrder2(node.right, queue, res);
     }
-
-    
-   
 }
