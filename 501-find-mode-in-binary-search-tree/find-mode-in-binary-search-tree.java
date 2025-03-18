1c7dcd830c98a2c0c151f66ed3a07ebd81f2bc21
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-03-18 13:03:16 +0530

    Time: 0 ms (100.00%) | Memory: 45.1 MB (66.92%) - LeetSync

---
+++ 501-find-mode-in-binary-search-tree/find-mode-in-binary-search-tree.java
@@ -0,0 +1,69 @@
+class Solution {
+    int prev ,curr, maxFreq, currFreq;
+    List<Integer> ans;
+    public int[] findMode(TreeNode root) {
+        ans = new ArrayList<>();
+        maxFreq = 1;
+
+        prev = Integer.MAX_VALUE;
+        currFreq = 1;
+        inorder(root);
+
+        maxFreq = Math.max(currFreq,maxFreq);
+        
+        
+        prev = Integer.MAX_VALUE;
+        currFreq = 1;
+        inorderModified(root);
+        // System.out.println(ans.toString());
+
+        int size = ans.size();
+        int[] arr = new int[size];
+        for(int i=0; i<size; i++){
+            arr[i] = ans.get(i);
+        }
+        return arr;
+    
+
+    }
+
+    public void inorder(TreeNode root){
+        if(root == null) return ;
+        inorder(root.left);
+
+        curr = root.val;
+        if(prev == Integer.MAX_VALUE){
+            prev = curr;
+        }else if(curr == prev){
+            currFreq++;
+        }else if(curr != prev){
+            maxFreq = Math.max(currFreq,maxFreq);
+            currFreq = 1;
+        }
+        prev = curr;
+
+        inorder(root.right);
+    }
+
+    public void inorderModified(TreeNode root){
+        if(root == null) return ;
+
+        inorderModified(root.left);
+        curr = root.val;
+        if(maxFreq == 1){
+            ans.add(curr);
+        }else if(prev == Integer.MAX_VALUE){
+            prev = curr;
+        }else if(curr == prev){
+            currFreq++;
+            if(currFreq == maxFreq){
+                ans.add(curr);
+            }
+        }else if(curr != prev){
+            currFreq = 1;
+        }
+        prev = curr;
+
+        inorderModified(root.right);
+    }
+}
