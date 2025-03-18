2cec6e30ca2888b680dbcac6ccf119882bd478ee
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-03-18 06:07:07 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (89.97%) - LeetSync

--- 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.java
+++ 799-minimum-distance-between-bst-nodes/minimum-distance-between-bst-nodes.java
@@ -1,52 +1,77 @@
-class Solution {
+// class Solution {
     
-    int minDiff;
+//     int minDiff;
+//     public int minDiffInBST(TreeNode root) {
+//         minDiff = Integer.MAX_VALUE;
+//         dfs(root);
+//         return minDiff;
+//     }
+
+//     public void dfs(TreeNode root){
+//         if(root == null) return;
+//         dfs(root.left);
+//         int min = IP(root.left);
+//         int max = IS(root.right);
+
+//         if(min != -1){
+//             minDiff = Math.min(root.val-min, minDiff);
+//         }
+
+//         if(max != -1){
+//              minDiff = Math.min(max - root.val, minDiff);
+//         }
+
+//         dfs(root.right);
+        
+//         return ;
+//     }
+
+//     public int IP(TreeNode root){
+//         if(root == null) return -1;
+
+//         int inorderPredecessor = root.val;
+//         while(root.right !=null){
+//             root = root.right;
+//             inorderPredecessor = root.val;
+//         }
+
+//         return inorderPredecessor;
+//     }
+
+//     public int IS(TreeNode root){
+//         if(root == null) return -1;
+
+//         int inorderSuccessor = root.val;
+//         while(root.left!=null){
+//             root = root.left;
+//             inorderSuccessor = root.val;
+//         }
+
+//         return inorderSuccessor;
+//     }
+// }
+
+
+class Solution { 
+    int ans = Integer.MAX_VALUE;
+    int pred = -1;
     public int minDiffInBST(TreeNode root) {
-        minDiff = Integer.MAX_VALUE;
-        dfs(root);
-        return minDiff;
+        inorder(root);
+        return ans;
     }
 
-    public void dfs(TreeNode root){
+    public void inorder(TreeNode root){
         if(root == null) return;
-        dfs(root.left);
-        int min = IP(root.left);
-        int max = IS(root.right);
         
-        if(min != -1){
-            minDiff = Math.min(root.val-min, minDiff);
+        inorder(root.left); 
+
+        if(pred != -1) {  
+            ans = Math.min(ans, root.val - pred);
         }
 
-        if(max != -1){
-             minDiff = Math.min(max - root.val, minDiff);
-        }
+        pred = root.val; 
 
-        dfs(root.right);
-        
-        return ;
+        inorder(root.right); 
     }
 
-    public int IP(TreeNode root){
-        if(root == null) return -1;
-
-        int inorderPredecessor = root.val;
-        while(root.right !=null){
-            root = root.right;
-            inorderPredecessor = root.val;
-        }
-
-        return inorderPredecessor;
-    }
-
-    public int IS(TreeNode root){
-        if(root == null) return -1;
-
-        int inorderSuccessor = root.val;
-        while(root.left!=null){
-            root = root.left;
-            inorderSuccessor = root.val;
-        }
-
-        return inorderSuccessor;
-    }
 }
