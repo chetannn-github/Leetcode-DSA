93fd2a413392ff502b66fb66d43a4e32765915e8
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-03-19 10:53:10 +0530

    Time: 149 ms (58.69%) | Memory: 68.5 MB (5.72%) - LeetSync

--- 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
@@ -1,30 +1,66 @@
+// class Solution {
+//     public long findScore(int[] nums) {
+//         int n = nums.length;
+//         PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) ->(
+//             a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]
+//         )); 
+//         HashSet<Integer> hs = new HashSet<>(); 
+
+//         for (int i = 0; i < n; i++) {
+//             pq.add(new int[]{nums[i], i});  
+//         }
+
+//         long score = 0;
+//         while (!pq.isEmpty()) {
+//             int[] min = pq.remove();
+//             int minVal = min[0];
+//             int idx = min[1];
+
+//             if (hs.contains(idx)) continue;
+
+//             score += minVal;
+//             hs.add(idx);
+
+//             if(idx > 0) hs.add(idx - 1);
+//             if(idx < n-1) hs.add(idx + 1);
+//         }
+
+//         return score;
+//     }
+// }
+
 class Solution {
     public long findScore(int[] nums) {
         int n = nums.length;
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) ->(
-            a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]
-        )); 
+        int[][] mat = new int[n][2];
         HashSet<Integer> hs = new HashSet<>(); 
 
         for (int i = 0; i < n; i++) {
-            pq.add(new int[]{nums[i], i});  
+            mat[i][0] = nums[i];
+            mat[i][1] = i;        
         }
 
+        Arrays.sort(mat,(a, b) ->(
+            a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]
+        ));
+
+    
         long score = 0;
-        while (!pq.isEmpty()) {
-            int[] min = pq.remove();
-            int minVal = min[0];
-            int idx = min[1];
+        for(int i=0; i<n; i++) {
+
+            int minVal = mat[i][0];
+            int idx = mat[i][1];
 
             if (hs.contains(idx)) continue;
 
             score += minVal;
             hs.add(idx);
 
-            if(idx > 0) hs.add(idx - 1);
+            if(idx > 0) hs.add(idx- 1);
             if(idx < n-1) hs.add(idx + 1);
         }
 
         return score;
     }
 }
+
