acd66e92278ac62c81e421e60f7c86ff7b105b1a
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-03-19 10:44:34 +0530

    Time: 235 ms (12.92%) | Memory: 61.4 MB (20.55%) - LeetSync

---
+++ 2695-find-score-of-an-array-after-marking-all-elements/find-score-of-an-array-after-marking-all-elements.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public long findScore(int[] nums) {
+        int n = nums.length;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) ->(
+            a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]
+        )); 
+        HashSet<Integer> hs = new HashSet<>(); 
+
+        for (int i = 0; i < n; i++) {
+            pq.add(new int[]{nums[i], i});  
+        }
+
+        long score = 0;
+        while (!pq.isEmpty()) {
+            int[] min = pq.remove();
+            int minVal = min[0];
+            int idx = min[1];
+
+            if (hs.contains(idx)) continue;
+
+            score += minVal;
+            hs.add(idx);
+
+            if(idx > 0) hs.add(idx - 1);
+            if(idx < n-1) hs.add(idx + 1);
+        }
+
+        return score;
+    }
+}
