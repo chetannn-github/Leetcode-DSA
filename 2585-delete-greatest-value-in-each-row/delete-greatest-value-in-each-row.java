16c8a61cd88a38f112b1c35de73a5108fc4baafc
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-03-19 07:07:10 +0530

    Time: 9 ms (16.01%) | Memory: 44.6 MB (46.18%) - LeetSync

--- 2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.java
+++ 2585-delete-greatest-value-in-each-row/delete-greatest-value-in-each-row.java
@@ -1,20 +1,48 @@
+// class Solution {
+//     public int deleteGreatestValue(int[][] grid) {
+//         int rows = grid.length;
+//         int cols = grid[0].length;
+
+//         for(int[] row : grid){
+//             Arrays.sort(row);
+//         }
+
+//         for(int i=0; i<cols; i++){
+//             for(int j=0; j<rows; j++){
+//                 grid[0][i] = Math.max(grid[j][i],grid[0][i]);
+//             }
+//         }
+//         int sum = 0;
+//         for(int i=0;i<cols; i++){
+//             sum += grid[0][i];
+//         }
+//         return sum;
+//     }
+// }
+
+
 class Solution {
     public int deleteGreatestValue(int[][] grid) {
         int rows = grid.length;
         int cols = grid[0].length;
+        List<PriorityQueue<Integer>> list = new ArrayList<>();
+
+        
 
         for(int[] row : grid){
-            Arrays.sort(row);
-        }
-
-        for(int i=0; i<cols; i++){
-            for(int j=0; j<rows; j++){
-                grid[0][i] = Math.max(grid[j][i],grid[0][i]);
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+            for(int val : row){
+                pq.add(val);
             }
+            list.add(pq);
         }
         int sum = 0;
-        for(int i=0;i<cols; i++){
-            sum += grid[0][i];
+        for(int j=0; j<cols; j++){
+            int max = -1;
+            for(int i=0; i<rows; i++){
+                max = Math.max(list.get(i).remove(),max);
+            }
+            sum += max;
         }
         return sum;
     }
