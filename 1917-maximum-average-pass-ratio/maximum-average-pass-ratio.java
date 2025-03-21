4fdb2b211b1b4e6dee10fb88939bf1e8afbe9570
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-03-21 07:59:05 +0530

    Time: 492 ms (95.09%) | Memory: 96.2 MB (35.36%) - LeetSync

---
+++ 1917-maximum-average-pass-ratio/maximum-average-pass-ratio.java
@@ -0,0 +1,28 @@
+class Solution {
+    public double maxAverageRatio(int[][] classes, int k) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
+            double gainA = ((double) (a[0] + 1) / (a[1] + 1)) - ((double) a[0] / a[1]);
+            double gainB = ((double) (b[0] + 1) / (b[1] + 1)) - ((double) b[0] / b[1]);
+            return Double.compare(gainB, gainA);
+        });
+        double avg = 0.0;
+
+        for(int[] c : classes){
+            pq.add(new int[]{c[0],c[1]});
+            avg += ((double) c[0]/c[1]);
+        }
+
+        while(k!=0){
+            int[] c = pq.remove();
+            avg -= ((double) c[0]/c[1]);
+            c[0] += 1;
+            c[1] += 1;
+
+            avg += ((double) c[0]/c[1]);
+            pq.add(c);
+            k--;
+        }
+
+        return (double) avg/classes.length;  
+    }
+}
