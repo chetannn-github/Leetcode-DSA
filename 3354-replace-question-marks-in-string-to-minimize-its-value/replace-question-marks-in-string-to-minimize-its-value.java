a1d5d524dd05f0a2b62963ab2ec18e542f49d846
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-03-23 17:31:32 +0530

    Time: 126 ms (31.33%) | Memory: 55.9 MB (7.23%) - LeetSync

---
+++ 3354-replace-question-marks-in-string-to-minimize-its-value/replace-question-marks-in-string-to-minimize-its-value.java
@@ -0,0 +1,56 @@
+class Solution {
+    public String minimizeStringValue(String s) {
+        int[] freq = new int[26];
+        int n = s.length();
+        HashSet<Integer> questionMarkIndices = new HashSet<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
+            return freq[a] == freq[b] ? a - b : freq[a] - freq[b];
+        });
+
+        for(int i=0; i<n; i++){
+            char currChar = s.charAt(i);
+            if(currChar != '?'){
+                freq[(int)(currChar - 'a')]++;
+            }else{
+                questionMarkIndices.add(i);
+            }
+            
+        }
+
+        for(int i=0; i<26;i++){
+            pq.add(i);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        List<Integer> replacement = new ArrayList<>();
+
+        for(int i=0; i<n; i++){
+            char currChar = s.charAt(i);
+
+            if(currChar == '?'){
+                int ch = pq.remove();
+                replacement.add(ch);
+                
+                freq[ch]++;
+                pq.add(ch);
+            }
+        }
+
+        Collections.sort(replacement);
+
+        int replacementIdx = 0;
+        for(int i=0; i<n; i++){
+            if(questionMarkIndices.contains(i)){
+                char replacedChar =(char) (replacement.get(replacementIdx) + 'a');
+                sb.append(replacedChar);
+                replacementIdx++;
+            }else{
+                sb.append(s.charAt(i));
+            }
+        }
+
+        return sb.toString();
+
+        
+    }
+}
