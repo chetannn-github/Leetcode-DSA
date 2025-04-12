75df3bfeba7d1af406d568f7f4878eee870633c1
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-04-12 10:13:20 +0530

    Time: 225 ms (5.01%) | Memory: 44.6 MB (98.80%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.java
@@ -0,0 +1,27 @@
+class Solution {
+    int n;HashSet<Integer> visited;
+    public int removeStones(int[][] stones) {
+        int groups = 0;
+        n = stones.length;
+        visited = new HashSet<>();
+
+        for(int i=0; i<n; i++){
+            if(!visited.contains(i)){
+                dfs(stones,i);
+                groups++;
+            }
+        }
+
+        return n - groups;
+    }
+
+    public void dfs(int[][] stones,int curr){
+        visited.add(curr);
+
+        for(int i=0; i<n; i++){
+            if(!visited.contains(i) && (stones[curr][0] == stones[i][0]  || stones[curr][1] == stones[i][1] )){
+                dfs(stones,i);
+            }
+        }
+    }
+}
