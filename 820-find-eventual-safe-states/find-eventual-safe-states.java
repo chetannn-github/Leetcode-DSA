8c07cdd5e4949f983aa2598d6110bfd3cd38b2a9
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-04-14 18:31:11 +0530

    Time: 16 ms (56.90%) | Memory: 55.2 MB (95.50%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -0,0 +1,38 @@
+class Solution {
+    boolean[] bl;
+    HashSet<Integer> visited;
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        bl = new boolean[graph.length];
+        visited = new HashSet<>();
+
+
+        for(int i=0; i<graph.length; i++){
+            if(!visited.contains(i)){
+                dfs(graph,i);
+            }
+        }
+        List<Integer> ans = new ArrayList<>();
+        for(int i=0; i<graph.length; i++){
+            if(bl[i]){
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+
+    public boolean dfs(int[][] graph, int src){
+        visited.add(src);
+        boolean ans = true;
+        for(int nbr : graph[src]){
+            if(!visited.contains(nbr)){
+                ans = ans && dfs(graph,nbr);
+            }else {
+                ans = ans && bl[nbr];
+            }
+        }
+
+        return bl[src] = ans;
+    }
+
+}
