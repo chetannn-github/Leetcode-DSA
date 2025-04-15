3d68058a9079160e0e86876ef7949b9c041d39d7
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-04-15 17:38:24 +0530

    Time: 48 ms (63.33%) | Memory: 68.9 MB (81.21%) - LeetSync

---
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/reorder-routes-to-make-all-paths-lead-to-the-city-zero.java
@@ -0,0 +1,33 @@
+class Solution {
+    List<List<List<Integer>>> adj;
+    int count;
+    public int minReorder(int n, int[][] edges) {
+        adj = new ArrayList<>();
+        count = 0;
+        for(int i=0; i<n; i++) adj.add(new ArrayList<>());
+
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+
+            adj.get(u).add(Arrays.asList(v,1));
+            adj.get(v).add(Arrays.asList(u,-1));
+
+        }
+
+        dfs(0,-1);
+        return count;
+    }
+
+    public void dfs(int curr,int parent){
+        
+        for(List<Integer> nbr : adj.get(curr)){
+            int nb = nbr.get(0);
+            if(nb != parent){
+                boolean real = nbr.get(1) == 1 ;
+                if(real){count++;}
+                dfs(nb,curr);
+            }
+        }
+    }
+}
