eceb91f258c4cf8f5968d0cfd9acf95a005272a0
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-04-18 11:33:56 +0530

    Time: 728 ms (11.06%) | Memory: 158.7 MB (5.04%) - LeetSync

---
+++ 1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.java
@@ -0,0 +1,54 @@
+class Solution {
+    public int minCostConnectPoints(int[][] points) {
+        int n = points.length;
+        List<List<List<Integer>>> adj = new ArrayList<>();
+
+        for(int i=0; i<n;i++) adj.add(new ArrayList<>());
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                int x1 = points[i][0];
+                int x2 = points[j][0];
+                int y1 = points[i][1];
+                int y2 = points[j][1];
+
+                int wt = Math.abs(x1-x2) + Math.abs(y1-y2);
+                
+                adj.get(i).add(Arrays.asList(j,wt));
+                adj.get(j).add(Arrays.asList(i,wt));
+            }
+        }
+
+        return prims(adj,n);
+    }
+
+
+    public int prims(List<List<List<Integer>>> adj,int n){
+        boolean[] inMST = new boolean[n];
+        int sum = 0;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(a[1]-b[1]));
+        pq.add(new int[]{0,0});
+        
+
+        while(!pq.isEmpty()){
+            int[] curr = pq.remove();
+            int u = curr[0];
+            int wt = curr[1];
+
+            if(inMST[u]) continue;
+            inMST[u] = true;
+            sum += wt;
+
+            for(List<Integer> nbr : adj.get(u)){
+                int v = nbr.get(0);
+                int w = nbr.get(1);
+
+                if(!inMST[v]){
+                    pq.add(new int[]{v,w});
+                }
+            }
+
+        }
+        return sum;
+    }
+}
