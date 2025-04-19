110d871cf4ef9f2b00b733b2d0b652f9ced1464c
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-04-19 13:12:02 +0530

    Time: 59 ms (30.81%) | Memory: 45.2 MB (67.83%) - LeetSync

---
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int minimumEffortPath(int[][] heights) {
+        int r = heights.length, c = heights[0].length;
+        int[][] dist = new int[r][c];
+        int[][] dirn = {{1,0},{-1,0},{0,1},{0,-1}};
+
+        for(int[] row : dist){
+            Arrays.fill(row,Integer.MAX_VALUE);
+        }
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(a[0]-b[0]));
+        pq.add(new int[]{0,0,0});
+        dist[0][0] = 0;
+
+        
+        while(!pq.isEmpty()){
+            int[] curr = pq.remove();
+            int wt = curr[0];
+
+            for(int[] dir : dirn){
+                int x = curr[1] + dir[0];
+                int y = curr[2] + dir[1];
+
+                if(x>=0 && y>=0 && y<c && x < r){
+                   int effort = Math.max(Math.abs(heights[x][y] - heights[curr[1]][curr[2]]), wt);
+
+                    if(effort < dist[x][y]){
+                        dist[x][y] =  effort;
+                        pq.add(new int[]{effort,x,y});               
+                    } 
+                }
+
+                
+            }
+        }
+
+        return dist[r-1][c-1];
+    }
+}
