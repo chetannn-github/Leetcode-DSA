bed793d3008ea0ae6663df093c9a6e02321102bc
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-05-21 12:16:53 +0530

    Time: 4 ms (15.02%) | Memory: 41.8 MB (33.37%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,58 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        Trie prefixTree = new Trie();
+
+        for(String str : strs){
+            prefixTree.insert(str);
+        }
+        int lengthOfCommonPrefix = prefixTree.getLongestCommanPrefix();
+        return lengthOfCommonPrefix == 0 ? new String("") : strs[0].substring(0,lengthOfCommonPrefix);
+    }
+}
+
+class Trie {
+    TrieNode root;
+    boolean emptyString = false;
+    Trie() {
+        root = new TrieNode();
+    }
+    void insert(String val) {
+        if(val.equals("")){
+            emptyString = true;
+        }
+        TrieNode curr = root;
+        for(char ch : val.toCharArray()){
+            if(curr.children.get(ch) == null){
+                curr.children.put(ch,new TrieNode());
+            }
+            curr = curr.children.get(ch);
+        }
+        curr.isEndOfWord = true;
+    }
+
+    int getLongestCommanPrefix() {
+        if(emptyString) return 0;
+
+        TrieNode curr = root;
+        int idx = 0;
+        while(!curr.isEndOfWord && curr.children.size() == 1){
+            for(char key : curr.children.keySet()){
+                curr = curr.children.get(key);
+            }
+            idx++;
+        }
+
+        return idx;
+    }
+
+
+}
+
+class TrieNode {
+    HashMap<Character,TrieNode> children;
+    boolean isEndOfWord;
+    TrieNode() {
+        children = new HashMap<>();
+        isEndOfWord = false;
+    }
+}
