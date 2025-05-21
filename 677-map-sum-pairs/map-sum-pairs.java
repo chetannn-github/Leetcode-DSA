1989b1f6caba6b724d454d8bcff33efc7a1ef333
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-05-21 11:49:10 +0530

    Time: 11 ms (67.84%) | Memory: 42.8 MB (28.65%) - LeetSync

---
+++ 677-map-sum-pairs/map-sum-pairs.java
@@ -0,0 +1,55 @@
+class MapSum {
+    Trie prefixTree;
+    public MapSum() {
+        prefixTree = new Trie();
+    }
+    
+    public void insert(String key, int val) {
+        int pheleWaliValue = 0;
+        if(prefixTree.addedStrings.containsKey(key)){
+            pheleWaliValue = prefixTree.addedStrings.get(key);
+        }
+        prefixTree.addedStrings.put(key,val);
+        TrieNode curr = prefixTree.root;
+
+        for(char ch : key.toCharArray()){
+            if(curr.children.get(ch) == null){
+                curr.children.put(ch,new TrieNode(val));
+            }else{
+               curr.children.get(ch).value += val - pheleWaliValue ;  
+            }
+            curr = curr.children.get(ch);
+        }
+    }
+    
+    public int sum(String prefix) {
+        TrieNode current = prefixTree.root;
+        for (char ch : prefix.toCharArray()) {
+            current = current.children.get(ch);
+            if (current == null) return 0;
+        }
+        return current.value;
+
+        
+    }
+}
+
+
+class Trie{
+    TrieNode root;
+    HashMap<String,Integer> addedStrings;
+    Trie() {
+        root = new TrieNode(0);
+        addedStrings = new HashMap<>();
+    }
+}
+
+class TrieNode {
+    int value;
+    HashMap<Character,TrieNode> children;
+
+    TrieNode(int val) {
+        value = val;
+        children = new HashMap<>();
+    }
+}
