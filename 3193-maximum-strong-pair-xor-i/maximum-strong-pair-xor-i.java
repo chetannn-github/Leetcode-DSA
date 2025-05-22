139773c0385a66b2a03637f4dced33c28275bd1f
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-05-22 13:42:37 +0530

    Time: 1 ms (100.00%) | Memory: 43.5 MB (30.86%) - LeetSync

--- 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.java
+++ 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.java
@@ -4,8 +4,8 @@ class Solution {
 
         for(int num1 : nums){
             for(int num2 : nums) {
-                if(Math.min(num1,num2) >= Math.abs(num1 - num2)){
-                    maxXOR = Math.max(maxXOR, num1 ^ num2);
+                if(maxXOR < (num1 ^ num2 ) &&  Math.min(num1,num2) >= Math.abs(num1 - num2)){
+                    maxXOR = num1 ^ num2;
                 }
             }
         }
