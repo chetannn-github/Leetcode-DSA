d87f164faa40a29423c4f41bf9ffc55de180bf4c
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-05-26 20:26:08 +0530

    Time: 1 ms (23.87%) | Memory: 42.9 MB (46.53%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.java
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.java
@@ -1,25 +1,51 @@
+// class Solution {
+//     public int findKthPositive(int[] arr, int k) {
+//         int[] isPresent = new int[1001];
+
+//         for(int num : arr) {
+//             isPresent[num] = 1;
+//         }
+//         int i=1;
+//         for(; i<1001; i++) {
+//             int num = isPresent[i];
+//             if(num == 0) {
+//                 k--;
+//                 if(k == 0) {
+//                     return i;
+//                 }
+//             }
+//         }
+
+//         while(k-->0) { i++;
+//         }
+//         return i -1;
+
+
+//     }
+// }
+
+
+
+
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int[] isPresent = new int[1001];
+        int missingCount = 0;
+        int currentNum = 1;
+        int i = 0;
 
-        for(int num : arr) {
-            isPresent[num] = 1;
-        }
-        int i=1;
-        for(; i<1001; i++) {
-            int num = isPresent[i];
-            if(num == 0) {
-                k--;
-                if(k == 0) {
-                    return i;
+        while (missingCount < k) {
+            
+            if (i < arr.length && arr[i] == currentNum) {
+                i++;
+            } else {
+                missingCount++;
+                if (missingCount == k) {
+                    return currentNum;
                 }
             }
+            currentNum++;
         }
-
-        while(k-->0) { i++;
-        }
-        return i -1;
-
-
+        return -1; 
+        
     }
 }
