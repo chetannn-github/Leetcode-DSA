e2872e555f4e09cd397f28babb502e1f552373e0
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-05-29 20:50:48 +0530

    Time: 5 ms (99.35%) | Memory: 44.7 MB (82.55%) - LeetSync

--- 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
+++ 2469-longest-subsequence-with-limited-sum/longest-subsequence-with-limited-sum.java
@@ -2,15 +2,13 @@ class Solution {
     public int[] answerQueries(int[] nums, int[] queries) {
         Arrays.sort(nums);
         
-        int pre[] = new int[nums.length];
-        pre[0] = nums[0];
+
         for(int i=1; i<nums.length; i++){
-            pre[i] = pre[i-1] + nums[i];
+            nums[i] = nums[i-1] + nums[i];
         }
 
-
         for(int i=0; i<queries.length; i++){
-            queries[i] = findUpperBound(pre,queries[i]);
+            queries[i] = findUpperBound(nums,queries[i]);
         }
         return queries;
     }
