f701071d28416d9c0f99eda06b4b5260f7d238e9
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-06 11:53:36 +0530

    Time: 3 ms (100.00%) | Memory: 45 MB (55.77%) - LeetSync

--- 155-min-stack/min-stack.java
+++ 155-min-stack/min-stack.java
@@ -1,31 +1,75 @@
-class MinStack {
-    Stack<Integer> st;
-    Stack<Integer> min;
-    Integer minVal = Integer.MAX_VALUE;
+// class MinStack {
+//     Stack<Integer> st;
+//     Stack<Integer> min;
+//     Integer minVal = Integer.MAX_VALUE;
     
+//     public MinStack() {
+//         st = new Stack<>();
+//         min = new Stack<>();
+//     }
+    
+//     public void push(int val) {
+//         st.push(val);
+//         minVal = Math.min(val,minVal);
+//         min.push(minVal);
+//     }
+    
+//     public void pop() {
+//         st.pop();
+//         min.pop();
+//         minVal = min.isEmpty() ? Integer.MAX_VALUE : min.peek();
+//     }
+    
+//     public int top() {
+//         return st.peek();
+//     }
+    
+//     public int getMin() {
+//         return min.peek();
+//     }
+// }
+
+
+
+class Node {
+    int val;
+    int min;
+    Node next;
+
+    Node(int val,int min, Node next) {
+        this.val = val;
+        this.min = min;
+        this.next = next;
+    } 
+}
+
+class MinStack {
+    
+    Node head;
     public MinStack() {
-        st = new Stack<>();
-        min = new Stack<>();
     }
     
     public void push(int val) {
-        st.push(val);
-        minVal = Math.min(val,minVal);
-        min.push(minVal);
+        if(head == null) {
+            head = new Node(val,val,null);
+        }else {
+            Node temp = new Node(val,Math.min(val,head.min), head);
+            head = temp;
+        }
     }
     
     public void pop() {
-        st.pop();
-        min.pop();
-        minVal = min.isEmpty() ? Integer.MAX_VALUE : min.peek();
+        head = head.next;
     }
     
     public int top() {
-        return st.peek();
+        return  head.val;
     }
     
     public int getMin() {
-        return min.peek();
+        return head.min;
     }
 }
+
+
 
