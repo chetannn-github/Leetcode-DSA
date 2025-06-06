3acfa40bd2eb62f15176e29aaf5c8f50181d9171
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-06 17:20:10 +0530

    Time: 28 ms (89.50%) | Memory: 55.4 MB (55.00%) - LeetSync

--- 937-online-stock-span/online-stock-span.java
+++ 937-online-stock-span/online-stock-span.java
@@ -1,21 +1,42 @@
-class StockSpanner {
-    List<Integer> st;
-    int size = 0;
-    public StockSpanner() {
-        this.st = new ArrayList<>();
-    }
-    
-    public int next(int price) {
-        st.add(price);
-        size++;
-        int count = 0;
+// class StockSpanner {
+//     List<Integer> st;
+//     int size = 0;
 
-        for(int i= size-1; i>=0; i--){
-            if(price < st.get(i)) break;
-            count++;
+//     public StockSpanner() {
+//         this.st = new ArrayList<>();
+//     }
+    
+//     public int next(int price) {
+//         st.add(price);
+//         size++;
+//         int count = 0;
+
+//         for(int i= size-1; i>=0; i--){
+//             if(price < st.get(i)) break;
+//             count++;
+//         }
+
+//         return count;
+        
+//     }
+// }
+
+
+class StockSpanner {
+    Stack<int[]> st;
+
+    public StockSpanner() {
+        st = new Stack<>();
+    }
+
+    public int next(int price) {
+        int span = 1;
+
+        while (!st.isEmpty() && st.peek()[0] <= price) {
+            span += st.pop()[1];
         }
 
-        return count;
-        
+        st.push(new int[]{price, span});
+        return span;
     }
 }
