18bcd397bcfe297bf5e4c731380ccdcdd49fabca
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-09 10:06:42 +0530

    Time: 2 ms (30.14%) | Memory: 58.2 MB (29.09%) - LeetSync

---
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -0,0 +1,14 @@
+
+var removeDuplicates = function(nums) {
+    let left = 0;
+    
+    for (let i=1 ; i<nums.length ; i++){
+        if(nums[left] !== nums[i]){
+            nums[left+1] = nums[i];
+            left++;
+        }
+
+    }
+    
+    return  left + 1;
+};
