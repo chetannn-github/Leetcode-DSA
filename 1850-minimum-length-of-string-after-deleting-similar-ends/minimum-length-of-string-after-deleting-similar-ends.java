381305908a68feea499837793379188a0af2b193
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-09 20:18:01 +0530

    Time: 6 ms (14.73%) | Memory: 45.4 MB (32.71%) - LeetSync

--- 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
+++ 1850-minimum-length-of-string-after-deleting-similar-ends/minimum-length-of-string-after-deleting-similar-ends.java
@@ -12,24 +12,18 @@ class Solution {
                 start++;
                 end--;
 
-                
                 while(start<n && s.charAt(start-1) == s.charAt(start)){
                     start++;
                 }
-                if(start == end){
-                    return 1;
-                }
-    
+                
                 while(end>=0 && s.charAt(end+1) == s.charAt(end)){
                    end--;
                 }
-                if(start == end){
-                    return 1;
-                }
+                
             }else{ 
                 return end - start +1;
             }
         }
-        return  0; 
+        return start == end ? 1 :  0; 
     }
 }
