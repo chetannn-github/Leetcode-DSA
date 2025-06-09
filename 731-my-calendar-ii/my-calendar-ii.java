3024379d6bb2a6e1b050d8f23ced8a65f71181b2
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-09 20:51:39 +0530

    Time: 716 ms (13.01%) | Memory: 46 MB (37.97%) - LeetSync

--- 731-my-calendar-ii/my-calendar-ii.java
+++ 731-my-calendar-ii/my-calendar-ii.java
@@ -1,35 +1,28 @@
 class MyCalendarTwo {
-
-    private TreeMap<Integer, Integer> bookingCount;
-    private int maxOverlappedBooking;
+    private TreeMap<Integer, Integer> hm;
+    private int maxOverlappingEvents;
 
     public MyCalendarTwo() {
-        bookingCount = new TreeMap<>();
-        maxOverlappedBooking = 2;
+        hm = new TreeMap<>();
+        maxOverlappingEvents = 2;
     }
 
     public boolean book(int start, int end) {
-        // Increase the booking count at 'start' and decrease at 'end'.
-        bookingCount.put(start, bookingCount.getOrDefault(start, 0) + 1);
-        bookingCount.put(end, bookingCount.getOrDefault(end, 0) - 1);
+        
+        hm.put(start, hm.getOrDefault(start, 0) + 1);
+        hm.put(end, hm.getOrDefault(end, 0) - 1);
 
         int overlappedBooking = 0;
 
-        // Calculate the prefix sum of bookings.
-        for (Map.Entry<Integer, Integer> entry : bookingCount.entrySet()) {
-            overlappedBooking += entry.getValue();
+        for (Integer key : hm.keySet()) {
+            overlappedBooking += hm.get(key);
 
-            // If the number of overlaps exceeds the allowed limit, rollback and
-            // return false.
-            if (overlappedBooking > maxOverlappedBooking) {
-                // Rollback changes.
-                bookingCount.put(start, bookingCount.get(start) - 1);
-                bookingCount.put(end, bookingCount.get(end) + 1);
+            if (overlappedBooking > maxOverlappingEvents) {
+                hm.put(start, hm.get(start) - 1);
+                hm.put(end, hm.get(end) + 1);
 
-                // Clean up if the count becomes zero to maintain the map clean.
-                if (bookingCount.get(start) == 0) {
-                    bookingCount.remove(start);
-                }
+                if (hm.get(start) == 0) hm.remove(start);
+                if (hm.get(end) == 0) hm.remove(end);
 
                 return false;
             }
