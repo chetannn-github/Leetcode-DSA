e1a90877fc9f7cab33427d1ea733c9d6d2efc69f
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-14 09:36:44 +0530

    Time: 0 ms (100.00%) | Memory: 41.9 MB (56.92%) - LeetSync

--- 3398-make-a-square-with-the-same-color/make-a-square-with-the-same-color.java
+++ 3398-make-a-square-with-the-same-color/make-a-square-with-the-same-color.java
@@ -1,61 +1,7 @@
-// class Solution {
-//     public boolean canMakeSquare(char[][] grid) {
-//         int count = 0;
-//         for(int i=0; i<2; i++){
-//             for(int j=0; j<2; j++){
-//                 if(grid[i][j]=='B'){
-//                     count--;
-//                 }else{
-//                     count++;
-//                 }
-                
-//             }
-//         }if(Math.abs(count)>=2){return true;}
-//         count = 0;
-//         for(int i=1; i<3; i++){
-//             for(int j=0; j<2; j++){
-//                 if(grid[i][j]=='B'){
-//                     count--;
-//                 }else{
-//                     count++;
-//                 }
-                
-//             }
-//         }if(Math.abs(count)>=2){return true;}
-//         count = 0;
-//         for(int i=0; i<2; i++){
-//             for(int j=1; j<3; j++){
-//                 if(grid[i][j]=='B'){
-//                     count--;
-//                 }else{
-//                     count++;
-//                 }
-                
-//             }
-//         }if(Math.abs(count)>=2){return true;}
-//         count = 0;
-//         for(int i=1; i<3; i++){
-//             for(int j=1; j<3; j++){
-//                 if(grid[i][j]=='B'){
-//                     count--;
-//                 }else{
-//                     count++;
-//                 }
-                
-//             }
-//         }if(Math.abs(count)>=2){return true;}
-//         return false;
-//     }
-    
-// }
-
 class Solution {
     public boolean canMakeSquare(char[][] grid) {
-        
-        int n = grid.length-1;
-
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++){
+        for(int i=0; i<2; i++){
+            for(int j=0; j<2; j++){
                 int count  = 1;
 
                 if (grid[i][j]==grid[i][j+1]) count++;
