18dedf6229b535d6c4127152680ce4d2fac75bce
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-14 09:25:32 +0530

    Time: 8 ms (56.61%) | Memory: 70.6 MB (99.07%) - LeetSync

--- 1972-rotating-the-box/rotating-the-box.java
+++ 1972-rotating-the-box/rotating-the-box.java
@@ -1,35 +1,33 @@
 class Solution {
     public char[][] rotateTheBox(char[][] mat) {
         // always remeberr apply all ops to row 
-        int length = mat.length;
-        char[][] ans = new char[mat[0].length][length];
+        int r = mat.length , c = mat[0].length;
+        char[][] ans = new char[c][r];
 
-        for(int i=0;i<mat.length; i++){
-            int lastDot = -1;
-            for(int j=mat[0].length-1; j>=0; j--){
-                if(lastDot==-1 && mat[i][j]=='.' ) {
-                   lastDot = j;
-                }else if(mat[i][j]=='*' ) {
-                   lastDot = -1;
-                }else if(lastDot!=-1 && mat[i][j]=='#') {
-                    mat[i][j]='.' ;
-                    mat[i][lastDot]='#';
-                    lastDot--; // kukii i am sure isse peeche wala bhii star nhii ho skta hhh wo dot hogaa yaa ban jaaegaa
+        for(int i=0;i<r; i++){
+            int lastHash = -1;
+            for(int j=0; j < c; j++){
+                if(lastHash==-1 && mat[i][j]=='#' ) {
+                   lastHash = j;
+                }else if(mat[i][j]=='*') {
+                   lastHash = -1;
+                }else if(lastHash!=-1 && mat[i][j]=='.') {
+                    mat[i][j]='#' ;
+                    mat[i][lastHash]='.';
+                    lastHash++;
                }
             }
             
         }
 
         for(int i=0;i<ans.length; i++){
-            char[] temp = new char[length];
-            for(int j=0;j<length; j++){
-                temp[length-1-j] = mat[j][i];
+            char[] temp = new char[r];
+            for(int j=0;j<r; j++){
+                temp[r-1-j] = mat[j][i];
             }
             ans[i] = temp;
         }
 
-        
-        
         return ans;
         
     }
