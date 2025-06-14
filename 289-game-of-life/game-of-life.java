c4bc837c04c18421efca2c241f68c1f0cb2ff13e
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-14 08:35:34 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (9.10%) - LeetSync

--- 289-game-of-life/game-of-life.java
+++ 289-game-of-life/game-of-life.java
@@ -1,6 +1,4 @@
 // great question of bit manipulationn!!!!!!!
-
-
 class Solution {
     public void gameOfLife(int[][] board) {
         int rows = board.length, cols = board[0].length;
@@ -10,9 +8,9 @@ class Solution {
                 int neighbours = calcNeighbours( board,rows, cols, i, j);
 
                 if(board[i][j]==0 && neighbours ==3){
-                    board[i][j]=2;
-                }else if(board[i][j]==1 && (neighbours ==2 || neighbours ==3)){
-                    board[i][j]=3;
+                    board[i][j]= 2; // 10 in binary
+                }else if(board[i][j] == 1 && (neighbours == 2 || neighbours == 3)){
+                    board[i][j]=3; // 11 in binary
                 }
             }
         }
@@ -27,11 +25,12 @@ class Solution {
 
     public int calcNeighbours(int[][] board,int rows, int cols, int r, int c){
         int count = 0;
-        for(int i=Math.max(0, r-1); i<=Math.min(r+1,rows-1); i++){
-            for(int j=Math.max(0, c-1); j<=Math.min(c+1,cols-1); j++){
-                count += (board[i][j]&1);
+        for(int i = Math.max(0, r-1); i <= Math.min(r+1,rows-1); i++){
+            for(int j = Math.max(0, c-1); j <= Math.min(c+1,cols-1); j++){
+                if(i == r && j == c) continue;
+                count += (board[i][j] & 1 );
             }
         }
-        return count -(board[r][c]&1);
+        return count;
     }
 }
