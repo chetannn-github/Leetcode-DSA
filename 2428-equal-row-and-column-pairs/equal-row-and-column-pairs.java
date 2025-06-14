73901c0f3932b3f2410ba9214ebe993e520f6e4f
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-14 12:57:10 +0530

    Time: 40 ms (28.95%) | Memory: 50.8 MB (48.09%) - LeetSync

--- 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
+++ 2428-equal-row-and-column-pairs/equal-row-and-column-pairs.java
@@ -1,15 +1,45 @@
+// class Solution {
+//     public int equalPairs(int[][] grid) {
+//         HashMap<List<Integer> , Integer> hm = new HashMap<>();
+//         for(int i=0; i<grid.length; i++){
+//             List<Integer> row = new ArrayList<>();
+//             for (int j = 0; j < grid[i].length; j++) {
+//                 row.add(grid[i][j]);
+//             }
+//             hm.put(row,hm.getOrDefault(row,0)+1);
+//         }
+
+//         // transponse 
+//         for(int i=0; i<grid.length; i++){
+//             for(int j=i+1; j<grid.length; j++){
+//                 grid[i][j] = grid[j][i] ^ grid[i][j];
+//                 grid[j][i] = grid[j][i] ^ grid[i][j];
+//                 grid[i][j] = grid[j][i] ^ grid[i][j];
+//             }
+//         }
+
+
+//         int count = 0;
+//         for(int i=0; i<grid.length; i++){
+//             List<Integer> row = new ArrayList<>();
+//             for (int j = 0; j < grid[i].length; j++) {
+//                 row.add(grid[i][j]);
+//             }
+//             count += hm.getOrDefault(row,0);
+//         }
+
+//         return count;
+//     }
+// }
+
 class Solution {
     public int equalPairs(int[][] grid) {
-        HashMap<List<Integer> , Integer> hm = new HashMap<>();
+        HashMap<String, Integer> hm = new HashMap<>();
         for(int i=0; i<grid.length; i++){
-            List<Integer> row = new ArrayList<>();
-            for (int j = 0; j < grid[i].length; j++) {
-                row.add(grid[i][j]);
-            }
+            String row = Arrays.toString(grid[i]);
             hm.put(row,hm.getOrDefault(row,0)+1);
         }
 
-        // transponse 
         for(int i=0; i<grid.length; i++){
             for(int j=i+1; j<grid.length; j++){
                 grid[i][j] = grid[j][i] ^ grid[i][j];
@@ -18,16 +48,11 @@ class Solution {
             }
         }
 
-
         int count = 0;
         for(int i=0; i<grid.length; i++){
-            List<Integer> row = new ArrayList<>();
-            for (int j = 0; j < grid[i].length; j++) {
-                row.add(grid[i][j]);
-            }
+            String row = Arrays.toString(grid[i]);
             count += hm.getOrDefault(row,0);
         }
-
         return count;
     }
 }
