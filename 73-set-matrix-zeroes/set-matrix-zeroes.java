39e88a46e318b8aeee3250b380f3ecf894880753
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-14 07:29:45 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (44.40%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,30 +1,91 @@
+// class Solution {
+//     public void setZeroes(int[][] matrix) {
+//         HashSet<Integer> row = new HashSet<>();
+//         HashSet<Integer> col = new HashSet<>();
+//         int count = 0;
+//         int n = matrix[0].length;
+
+//         for(int[] r : matrix ){
+//             for(int el : r){
+//                 if(el==0){
+//                     row.add(count/n);
+//                     col.add(count%n);
+//                 }
+//                 count++;
+//             }
+//         }
+
+
+//         for(int i=0; i<matrix.length; i++){
+//             for(int j=0; j<matrix[0].length; j++){
+//                 if(row.contains(i) || col.contains(j)){
+//                     matrix[i][j] = 0;
+//                 }
+//             }
+//         }
+
+//         return;
+        
+//     }
+// }
+
 class Solution {
     public void setZeroes(int[][] matrix) {
-        HashSet<Integer> row = new HashSet<>();
-        HashSet<Integer> col = new HashSet<>();
-        int count = 0;
-        int n = matrix[0].length;
+        int rows = matrix.length;
+        int cols = matrix[0].length;
 
-        for(int[] r : matrix ){
-            for(int el : r){
-                if(el==0){
-                    row.add(count/n);
-                    col.add(count%n);
-                }
-                count++;
+        boolean firstRowHasZero = false;
+        boolean firstColHasZero = false;
+
+        for (int c = 0; c < cols; c++) {
+            if (matrix[0][c] == 0) {
+                firstRowHasZero = true;
+                break;
             }
         }
 
+        for (int r = 0; r < rows; r++) {
+            if (matrix[r][0] == 0) {
+                firstColHasZero = true;
+                break;
+            }
+        }
 
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix[0].length; j++){
-                if(row.contains(i) || col.contains(j)){
-                    matrix[i][j] = 0;
+        for (int r = 1; r < rows; r++) {
+            for (int c = 1; c < cols; c++) {
+                if (matrix[r][c] == 0) {
+                    matrix[r][0] = 0;
+                    matrix[0][c] = 0;
                 }
             }
         }
 
-        return;
-        
+        for (int r = 1; r < rows; r++) {
+            if (matrix[r][0] == 0) {
+                for (int c = 1; c < cols; c++) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+
+        for (int c = 1; c < cols; c++) {
+            if (matrix[0][c] == 0) {
+                for (int r = 1; r < rows; r++) {
+                    matrix[r][c] = 0;
+                }
+            }
+        }
+
+        if (firstRowHasZero) {
+            for (int c = 0; c < cols; c++) {
+                matrix[0][c] = 0;
+            }
+        }
+
+        if (firstColHasZero) {
+            for (int r = 0; r < rows; r++) {
+                matrix[r][0] = 0;
+            }
+        }        
     }
 }
