8eaa2dcb182ab0931097b6ce099d483dd1221e49
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-14 07:08:19 +0530

    Time: 1 ms (100.00%) | Memory: 45.8 MB (28.21%) - LeetSync

--- 3330-modify-the-matrix/modify-the-matrix.java
+++ 3330-modify-the-matrix/modify-the-matrix.java
@@ -7,14 +7,10 @@ class Solution {
             }
         }
 
-        int r = 0;
         for(int[] row : matrix){
             for(int j=0; j<row.length; j++){
-                if(row[j]==-1){
-                    row[j] = max[j];
-                }
+                row[j] = row[j]==-1 ? max[j] : row[j];
             }
-            r++;
         }
         return matrix;
     }
