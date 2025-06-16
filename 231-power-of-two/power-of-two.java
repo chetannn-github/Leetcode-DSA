2a98095c04573768ff49cfc7a614fe7b38436653
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-16 07:34:12 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (64.18%) - LeetSync

--- 231-power-of-two/power-of-two.java
+++ 231-power-of-two/power-of-two.java
@@ -1,11 +1,8 @@
-// class Solution {
-//     public boolean isPowerOfTwo(int n) {
-//       if (n<=0){return false;}
-//       else{
-//      boolean bt = ((n&(n-1)) == 0 );
-//      return bt;}
-//     }
-// }
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+     return n > 0  && ((n & (n-1)) == 0);
+    }
+}
 
 // class Solution {
 //     public boolean isPowerOfTwo(int n) {
@@ -18,20 +15,20 @@
 //     }
 // }
 
-class Solution {
-    public boolean isPowerOfTwo(int n) {
-        return solve((long)n,1);
-    }
+// class Solution {
+//     public boolean isPowerOfTwo(int n) {
+//         return solve((long)n,1);
+//     }
 
-    public boolean solve(long n, long curr){
-        if(n<=0){
-            return false;
-        }else if(n == curr){
-            return true;
-        }else if(n<curr){
-            return false;
-        }
+//     public boolean solve(long n, long curr){
+//         if(n<=0){
+//             return false;
+//         }else if(n == curr){
+//             return true;
+//         }else if(n<curr){
+//             return false;
+//         }
 
-        return solve(n,curr*2);
-    }
-}
+//         return solve(n,curr*2);
+//     }
+// }
