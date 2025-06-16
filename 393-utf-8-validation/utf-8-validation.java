3721c78e822d6aeff1546278737e48e053cabae2
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-16 08:25:53 +0530

    Time: 2 ms (56.58%) | Memory: 44.8 MB (53.03%) - LeetSync

--- 393-utf-8-validation/utf-8-validation.java
+++ 393-utf-8-validation/utf-8-validation.java
@@ -1,51 +1,29 @@
 class Solution {
     public boolean validUtf8(int[] data) {
-        int start = 0;
+        // ese smjo peeche senapti aaege as per power of king
         int n = data.length;
-        boolean ans = true;
-        while(start<n){
-            int num = data[start];
-            if(num>247){
-                return false;
-            }else if(num>=240){
-                ans = ans && checkNext(data,start+1,3,n);
-                start +=4;
-            }else if(num>=224){
-                ans = ans && checkNext(data,start+1,2,n);
-                start +=3;
-            }else if(num>=192){
-                ans = ans &&checkNext(data,start+1,1,n);
-                start +=2;
-            }else if(num>=128){
-                return false;
-            }
-            else if((num & (1<<8)) == 0){
-                start++;
-            }
+        int senapatiNeeded = 0;
 
-            if(!ans){
-                return ans;
-            }
+        for(int d : data) {
+            int kingType = findKingType(d);
+            if(d > 247) return false;
+            else if(kingType == 0) {
+                if(senapatiNeeded == 0) return false;
+                else senapatiNeeded--;
+            }else if(kingType != 0 && senapatiNeeded != 0) return false;
+            else senapatiNeeded += kingType - 1;
         }
 
-        return ans;
+        return senapatiNeeded == 0;
     }
 
-    public boolean checkNext(int[] data, int start, int x,int n){
-        if(start + x - 1 > n-1){
-            return false;
-        }
-
-        while(x>0){
-            if (!(data[start]>=128 && data[start]<=191 )){
-                return false;
-            }
-            x--;
-            start++;
-        }
-
-
-        return true;
-        
+    public int findKingType (int num) {
+        if(num >= 0 && num <= 127) return 1;
+        else if (num >= 192 && num <= 223) return 2;
+        else if (num>=224 && num <= 239) return 3;
+        else if (num >=240 && num <=247) return 4;
+        else return 0;
     }
+
+    
 }
