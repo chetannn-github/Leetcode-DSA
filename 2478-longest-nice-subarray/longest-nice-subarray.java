1c61555507e74eb56e00fbe3045e2db69900a7dc
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-16 16:38:07 +0530

    Time: 4 ms (25.30%) | Memory: 55.9 MB (45.56%) - LeetSync

--- 2478-longest-nice-subarray/longest-nice-subarray.java
+++ 2478-longest-nice-subarray/longest-nice-subarray.java
@@ -4,53 +4,19 @@ class Solution {
         int n = nums.length;
         int maxLength = 1;
 
-        for(int end = 1; end<n; end++){
+        for(int end = 1; end<n; end++) {
             // sbse ensure krnaa padegaa jb tum nyaa member jod rhoo hoo toh sbhii
             // doston se puchoo are you finee????
             // end se puchna shuru krugaa jisme mna kiyaa usse aur usse phele waalo se dosti khtmm
 
-            // for(int i = end-1; i>=start; i--){
-            //     if((nums[i] & nums[end] )!= 0){
-            //         start = i+1;
-            //         break;
-            //     }
-            // }
-            int i = end-1;
-            while(i>=start){
-                if((nums[i] & nums[end] )!= 0){
+            for(int i = end-1; i>=start; i--) {
+                if((nums[i] & nums[end]) != 0) {
                     start = i+1;
                     break;
                 }
-                i--;
             }
-            
-
             maxLength = Math.max(maxLength, end-start+1);
         }
-
         return maxLength;
     }
 }
-
-
-// class Solution {
-    // public int longestNiceSubarray(int[] nums) {
-    //     int start = 0;
-    //     int n = nums.length;
-    //     int maxLength = 1;
-
-    //     for(int end = 1; end<n; end++){
-    //         int i = start;
-    //         while(i<end){
-    //             if((nums[i++] & nums[end] )!= 0){
-    //                 start = i;
-    //             }
-    //         }
-            
-
-    //         maxLength = Math.max(maxLength, end-start+1);
-    //     }
-
-    //     return maxLength;
-    // }
-// }
