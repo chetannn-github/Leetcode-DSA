09042cdbe7574cdb8718ab5b4c873b19c6c9d245
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-16 09:18:29 +0530

    Time: 1 ms (99.40%) | Memory: 45.7 MB (64.65%) - LeetSync

--- 260-single-number-iii/single-number-iii.java
+++ 260-single-number-iii/single-number-iii.java
@@ -1,28 +1,24 @@
 class Solution {
     public int[] singleNumber(int[] nums) {
         int xor = 0;
-        for (int num : nums){
+        for (int num : nums) {
             xor ^= num; 
         }
             
         int count = 0;
-        while(xor!=0){
-            if((xor & 1)==1){break;}
+        while(xor!=0) {
+            if((xor & 1)==1) break;
             count++;
             xor >>= 1;  
         }
 
-        int rightMost = 1<<count;
-
-        // int rightMost = xor&-xor; 
-        // int rightMost = (xor &(xor-1))^xor;
+        int mask = 1<<count;
 
         int ans[]={0,0};
-        for (int num : nums){
-            if((num&rightMost)==0){
+        for (int num : nums) {
+            if((num & mask)==0) {
                 ans[0] ^= num;
-            }
-            else{
+            }else{
                 ans[1] ^= num;
             }
         }
