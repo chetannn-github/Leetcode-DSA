bc97cde2b2dc9fe4a8839616dfbd535b72bf7aa9
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-17 07:49:58 +0530

    Time: 192 ms (17.89%) | Memory: 69.1 MB (25.75%) - LeetSync

--- 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -1,30 +1,72 @@
+// class Solution {
+//     public boolean hasAllCodes(String s, int k) {
+//         int n = s.length();
+
+//         HashSet<StringBuilder> hs = new HashSet<>();
+//         StringBuilder sb = new StringBuilder();
+        
+//         for(int i=0; i<k; i++) {
+//             sb.append(s.charAt(i));
+//         }
+        
+//         hs.add(new StringBuilder(sb));
+
+//         if(hs.size() == Math.pow(2,k)) {
+//             return true;
+//         } 
+        
+//         for(int i=k; i<n; i++) {
+//             sb.deleteCharAt(0);
+//             sb.append(s.charAt(i));
+//             hs.add(new StringBuilder(sb));
+
+//             if(hs.size() == (1<<k)) {
+//                 return true;
+//             } 
+//         }
+//         return false;
+//     }
+// } 
+    
+
 class Solution {
+    char[] code;
+    int k;
     public boolean hasAllCodes(String s, int k) {
-        int length = s.length();
-        if(length<k) return false;
+        int n = s.length();
+        if(n<k) return false;
+        this.k = k;
 
         HashSet<String> hs = new HashSet<>();
-        StringBuilder sb = new StringBuilder();
-        for(int i=0; i<k; i++){
-            sb.append(s.charAt(i));
+        code = new char[k];
+        
+        for(int i=0; i<k; i++) {
+            code[i] = s.charAt(i);
         }
         
-        hs.add(sb.toString());
+        hs.add(new String(code));
 
-        if(hs.size() == Math.pow(2,k)){
+        if(hs.size() == (1<<k)) {
             return true;
         } 
+        
+        for(int i=k; i<n; i++) {
+            // add curr and delete first char
+            updateCodeArray(s.charAt(i));
+            hs.add(new String(code));
 
-        for(int i=k; i<length;i++){
-            sb.deleteCharAt(0);
-            sb.append(s.charAt(i));
-            hs.add(sb.toString());
-
-            if(hs.size() == Math.pow(2,k)){
+            if(hs.size() == (1<<k)) {
                 return true;
             } 
         }
         return false;
+    }
+
+    public void updateCodeArray(char ch) {
+        for(int i = 0; i<k-1; i++) {
+            code[i] = code[i+1];
+        }
+        code[k-1] = ch;
     }
 } 
     
