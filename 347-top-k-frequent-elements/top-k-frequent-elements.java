422b8248d2378a83c430329e3470a703d2328e3c
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-18 20:47:05 +0530

    Time: 14 ms (58.08%) | Memory: 48.7 MB (52.66%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -6,19 +6,18 @@ class Solution {
         for(int num : nums){
              hm.put(num, hm.getOrDefault(num, 0) + 1);
         }
-        ArrayList<int[]> list = new ArrayList<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b) -> (hm.get(a) - hm.get(b)));
 
         for(Integer key : hm.keySet()){
-            list.add(new int[] {key,hm.get(key)});
+            pq.add(key);
+            if(pq.size() > k) pq.remove();
         }
-        
-        Collections.sort(list,(a,b)->(b[1]-a[1]));
       
-        int[] array = new int[k];
+        int[] result = new int[k];
         for (int i = 0; i < k; i++) {
-            array[i] = list.get(i)[0];
+            result[i] = pq.remove();
         }
        
-        return array;
+        return result;
     }
 }
