64038068f301b452f951cc14f5a29c1dc2b11c11
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-18 20:54:09 +0530

    Time: 101 ms (13.97%) | Memory: 50.7 MB (92.20%) - LeetSync

--- 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.java
+++ 2113-find-the-kth-largest-integer-in-the-array/find-the-kth-largest-integer-in-the-array.java
@@ -1,19 +1,6 @@
 class Solution {
     public String kthLargestNumber(String[] nums, int k) {
-        PriorityQueue<String> pq = new PriorityQueue<>((a,b)->{
-            int l1 = a.length();
-            int l2 = b.length();
-
-            if(l1!= l2) return l2-l1;
-            
-            for(int i=0; i<l1; i++){
-                if(a.charAt(i) != b.charAt(i)){
-                    return (b.charAt(i)-a.charAt(i));
-                }
-            }
-            return l1-l2;
-
-        });
+        PriorityQueue<String> pq = new PriorityQueue<>((a,b)-> getBiggerNumber(a,b));
 
         for(String num : nums){
             pq.add(num);
@@ -23,5 +10,19 @@ class Solution {
             pq.remove();
         }
         return pq.peek();
+    }
+
+    public int getBiggerNumber(String num1, String num2) {
+        int l1 = num1.length();
+        int l2 = num2.length();
+
+        if(l1 != l2) return l2-l1;
+        
+        for(int i=0; i<l1; i++){
+            if(num1.charAt(i) != num2.charAt(i)){
+                return (num2.charAt(i)-num1.charAt(i));
+            }
+        }
+        return l1-l2;
     }
 }
