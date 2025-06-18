4db21a098ed182d639b83f72f6563bd9f8342708
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-18 07:14:55 +0530

    Time: 4 ms (13.19%) | Memory: 57.2 MB (26.55%) - LeetSync

--- 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
+++ 1586-longest-subarray-of-1s-after-deleting-one-element/longest-subarray-of-1s-after-deleting-one-element.java
@@ -1,18 +1,49 @@
+// class Solution {
+//     public int longestSubarray(int[] nums) {
+//         // esi subarray jisme max ek baar zero aaskta hh
+//         int start = 0;
+//         int zero = 0;
+//         int maxLength = 0;
+//         int n = nums.length;
+        
+//         for(int end=0; end<n; end++){
+//             if(nums[end]==0){
+//                 zero++;
+//             }
+
+//             while(zero>1){
+//                 if(nums[start]==0){
+//                     zero--;
+//                 }
+//                 start++;
+//             }
+
+//             maxLength = Math.max(maxLength, end-start+1 - 1);
+//         }
+
+//         return maxLength;
+//     }
+// }
+
+
 class Solution {
     public int longestSubarray(int[] nums) {
         // esi subarray jisme max ek baar zero aaskta hh
         int start = 0;
-        int zero = 0;
+        int ones = 0;
         int maxLength = 0;
         int n = nums.length;
-        
+        int maxFreq = 0;
         for(int end=0; end<n; end++){
-            if(nums[end]==0){
-                zero++;
+            if(nums[end]==1){
+                ones++;
             }
-            while(zero>1){
-                if(nums[start]==0){
-                    zero--;
+
+            maxFreq = Math.max(maxFreq,ones);
+            
+            while(end - start + 1 > 1 + maxFreq){
+                if(nums[start]==1){
+                    ones--;
                 }
                 start++;
             }
