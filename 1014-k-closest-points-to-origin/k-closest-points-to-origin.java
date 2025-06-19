7d6a4f8a7951a06b8efbb3d7f4bf3821cd02daa6
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-19 07:28:01 +0530

    Time: 65 ms (10.33%) | Memory: 50.7 MB (79.79%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,11 +1,12 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->(
-            (Math.sqrt(Math.pow(a[0],2) + Math.pow(a[1],2)) - Math.sqrt(Math.pow(b[0],2) + Math.pow(b[1],2))) >0 ? 1 : -1
+            compareDistanceBtwPoints(b,a)
         ));
 
         for(int[] row : points){
             pq.add(row);
+            if(pq.size() > k) pq.remove();
         }
         int[][] ans = new int[k][2];
         int idx = 0;
@@ -15,5 +16,12 @@ class Solution {
 
         return ans;
 
+    }
+
+
+    public int compareDistanceBtwPoints (int[] a, int[] b) {
+        double distanceOne = Math.sqrt(Math.pow(a[0],2) + Math.pow(a[1],2));
+        double distanceTwo = Math.sqrt(Math.pow(b[0],2) + Math.pow(b[1],2));
+        return distanceOne - distanceTwo > 0 ? 1 : -1;
     }
 }
