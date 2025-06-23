5551460cccc2163a77da2c435f68a2022df9b64b
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-23 16:35:56 +0530

    Time: 106 ms (54.77%) | Memory: 79.8 MB (85.50%) - LeetSync

--- 1962-single-threaded-cpu/single-threaded-cpu.java
+++ 1962-single-threaded-cpu/single-threaded-cpu.java
@@ -44,50 +44,95 @@
 //     }
 // }
 
+// class Solution {
+//     public int[] getOrder(int[][] tasks) {
+//         // starting time ke hisab se sort krke rkhaa hh
+//         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
+//             if(tasks[a][0] != tasks[b][0]){
+//                 return tasks[a][0] - tasks[b][0];
+//             }else if(tasks[a][1] != tasks[b][1]){
+//                 return tasks[a][1] - tasks[b][1];
+//             }
+//             return a - b;
+//         });
+//         int idx = 0;
+//         for(int[] task : tasks){
+//             pq.add(idx);
+//             idx++;
+//         }
+
+//         int[] ans = new int[tasks.length];
+//         idx = 0;
+//         PriorityQueue<Integer> pendingTasks = new PriorityQueue<>((a,b)->{
+//             // duration ke hisaab se 
+//             if(tasks[a][1] != tasks[b][1]){
+//                 return tasks[a][1] - tasks[b][1];
+//             }
+//             return a - b;
+//         });
+        
+//         int nextTime = 0;
+//         int currTime = 0;
+
+//         while(idx != tasks.length){
+            
+//             if(pendingTasks.isEmpty()){
+//                 pendingTasks.add(pq.remove());
+//                 currTime = tasks[pendingTasks.peek()][0];
+//             }
+//             int currTaskIdx = pendingTasks.remove();
+//             nextTime = currTime + tasks[currTaskIdx][1];
+
+//             ans[idx++] = currTaskIdx;
+//             while(!pq.isEmpty() && nextTime >= tasks[pq.peek()][0]){
+//                 pendingTasks.add(pq.remove());
+//             }
+
+//             currTime = nextTime;
+//         }
+
+//         return ans;
+//     }
+// }
+
+
+
 class Solution {
     public int[] getOrder(int[][] tasks) {
-        // starting time ke hisab se sort krke rkhaa hh
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
-            if(tasks[a][0] != tasks[b][0]){
-                return tasks[a][0] - tasks[b][0];
-            }else if(tasks[a][1] != tasks[b][1]){
-                return tasks[a][1] - tasks[b][1];
-            }
-            return a - b;
-        });
-        int idx = 0;
-        for(int[] task : tasks){
-            pq.add(idx);
-            idx++;
+        int n = tasks.length;
+
+        // [start, duration, index] 
+        int[][] taskList = new int[n][3];
+        for (int i = 0; i < n; i++) {
+            taskList[i][0] = tasks[i][0];
+            taskList[i][1] = tasks[i][1];
+            taskList[i][2] = i;
         }
 
-        int[] ans = new int[tasks.length];
-        idx = 0;
-        PriorityQueue<Integer> pendingTasks = new PriorityQueue<>((a,b)->{
-            // duration ke hisaab se 
-            if(tasks[a][1] != tasks[b][1]){
-                return tasks[a][1] - tasks[b][1];
-            }
-            return a - b;
+        Arrays.sort(taskList, (a, b) -> (a[0] - b[0]));
+
+       
+        PriorityQueue<int[]> pending = new PriorityQueue<>((a, b) -> {
+            if (a[1] != b[1]) return a[1] - b[1]; // duration
+            return a[2] - b[2]; // index
         });
-        
-        int nextTime = 0;
-        int currTime = 0;
 
-        while(idx != tasks.length){
-            if(!pq.isEmpty() && pendingTasks.isEmpty()){
-                pendingTasks.add(pq.remove());
-                currTime = tasks[pendingTasks.peek()][0];
-            }
-            int currTaskIdx = pendingTasks.remove();
-            nextTime = currTime + tasks[currTaskIdx][1];
+        int time = 0, i = 0, idx = 0;
+        int[] ans = new int[n];
 
-            ans[idx++] = currTaskIdx;
-            while(!pq.isEmpty() && nextTime >= tasks[pq.peek()][0]){
-                pendingTasks.add(pq.remove());
+        while (idx < n) {
+          
+            while (i < n && taskList[i][0] <= time) {
+                pending.add(taskList[i++]);
             }
 
-            currTime = nextTime;
+            if (!pending.isEmpty()) {
+                int[] task = pending.remove();
+                time += task[1];
+                ans[idx++] = task[2];
+            }else {
+                time = taskList[i][0];
+            }
         }
 
         return ans;
