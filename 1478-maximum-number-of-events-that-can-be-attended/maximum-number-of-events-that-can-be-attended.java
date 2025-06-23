e6badcebefc78f7d82ef313139b29ec322ef3585
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-23 17:13:34 +0530

    Time: 67 ms (51.24%) | Memory: 75.9 MB (36.01%) - LeetSync

--- 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
+++ 1478-maximum-number-of-events-that-can-be-attended/maximum-number-of-events-that-can-be-attended.java
@@ -1,57 +1,90 @@
-class Solution {
-    public int maxEvents(int[][] events) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
-            if(events[a][0] != events[b][0]){
-                return events[a][0] - events[b][0];
-            }else if(events[a][1] != events[b][1]){
-                return events[a][1] - events[b][1];
-            }
-            return a - b;
-        });
+// class Solution {
+//     public int maxEvents(int[][] events) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
+//             if(events[a][0] != events[b][0]){
+//                 return events[a][0] - events[b][0];
+//             }else if(events[a][1] != events[b][1]){
+//                 return events[a][1] - events[b][1];
+//             }
+//             return a - b;
+//         });
 
-        for(int i=0; i<events.length;i++){
-            pq.add(i);
-        }
+//         for(int i=0; i<events.length;i++){
+//             pq.add(i);
+//         }
 
-        PriorityQueue<Integer> pendingEvents = new PriorityQueue<>((a,b)->{
-            // kon jaldii end hone walaa
-            if(events[a][1] != events[b][1]){
-                return events[a][1] - events[b][1];
-            }else if(events[a][0] != events[b][0]){
-                return events[a][0] - events[b][0];
-            }
-            return a-b;
-        });
+//         PriorityQueue<Integer> pendingEvents = new PriorityQueue<>((a,b)->{
+//             // kon jaldii end hone walaa
+//             if(events[a][1] != events[b][1]){
+//                 return events[a][1] - events[b][1];
+//             }else if(events[a][0] != events[b][0]){
+//                 return events[a][0] - events[b][0];
+//             }
+//             return a-b;
+//         });
         
-        int ans = 0;
-        int currDay = 1;
-        int idx = 0;
+//         int ans = 0;
+//         int currDay = 1;
+//         int idx = 0;
 
-        while(true){
-            if(pq.isEmpty() && pendingEvents.isEmpty()){
-                break;
-            }
+//         while(true){
+//             if(pq.isEmpty() && pendingEvents.isEmpty()){
+//                 break;
+//             }
     
-            if(!pq.isEmpty() && pendingEvents.isEmpty()){
-                pendingEvents.add(pq.remove());
-                currDay = events[pendingEvents.peek()][0];
-            }
-            int currTaskIdx = pendingEvents.remove();
+//             if(!pq.isEmpty() && pendingEvents.isEmpty()){
+//                 pendingEvents.add(pq.remove());
+//                 currDay = events[pendingEvents.peek()][0];
+//             }
+//             int currTaskIdx = pendingEvents.remove();
 
-            if(currDay <= events[currTaskIdx][1]){
-                // System.out.println(currTaskIdx + " -> " + currDay);
-                ans++;
-                currDay++;
+//             if(currDay <= events[currTaskIdx][1]){
+//                 // System.out.println(currTaskIdx + " -> " + currDay);//
+//                 ans++;
+//                 currDay++;
                 
-            }
+//             }
 
             
-            while(!pq.isEmpty() && currDay >= events[pq.peek()][0]){
-                pendingEvents.add(pq.remove());
+//             while(!pq.isEmpty() && currDay >= events[pq.peek()][0]){
+//                 pendingEvents.add(pq.remove());
+//             }
+
+//         }
+
+//         return ans;
+//     }
+// }
+
+
+class Solution {
+    public int maxEvents(int[][] events) {
+        Arrays.sort(events, (a, b) -> (a[0] - b[0]));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int currDay = 0, i = 0, totalEventAttended = 0, n = events.length;
+        
+        while (!pq.isEmpty() || i < n) {
+            // add events that have been started
+            while(i<n && events[i][0] <= currDay) {
+                pq.add(events[i++][1]);
             }
 
-        }
+            if(pq.isEmpty()) {
+                currDay = events[i][0];
+            }else {
+                while(!pq.isEmpty() && pq.peek() < currDay) {
+                    pq.remove(); // remove expired events
+                }
 
-        return ans;
+                if(!pq.isEmpty()) {
+                    totalEventAttended++;
+                    currDay++;
+                    pq.remove();
+                }
+                
+            }
+        }
+        
+        return totalEventAttended;
     }
 }
