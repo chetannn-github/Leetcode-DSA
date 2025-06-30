133fdb2e61243e4e16032d93f4f44b9758a11324
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-06-30 13:39:37 +0530

    Time: 188 ms (28.57%) | Memory: 59.2 MB (53.57%) - LeetSync

--- 3466-number-of-subarrays-with-and-value-of-k/number-of-subarrays-with-and-value-of-k.java
+++ 3466-number-of-subarrays-with-and-value-of-k/number-of-subarrays-with-and-value-of-k.java
@@ -6,19 +6,15 @@ class Solution {
         for (int num : nums) {
             Map<Integer, Integer> curr = new HashMap<>();
 
-            // Extend previous subarrays
-            for (Map.Entry<Integer, Integer> entry : prev.entrySet()) {
-                int newAnd = entry.getKey() & num;
-                curr.put(newAnd, curr.getOrDefault(newAnd, 0) + entry.getValue());
+            for (Integer key : prev.keySet()) {
+                int newAnd = key & num;
+                curr.put(newAnd, curr.getOrDefault(newAnd, 0) + prev.get(key));
             }
 
-            // Start new subarray from current number
             curr.put(num, curr.getOrDefault(num, 0) + 1);
 
-            // Count subarrays with AND == k
             result += curr.getOrDefault(k, 0);
 
-            // Move to next index
             prev = curr;
         }
 
