0df24a93c675ded613cf9eeed513b9d673d2e8b5
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-07-01 10:05:46 +0530

    Time: 42 ms (78.85%) | Memory: 59.6 MB (69.23%) - LeetSync

---
+++ 3436-find-subarray-with-bitwise-or-closest-to-k/find-subarray-with-bitwise-or-closest-to-k.java
@@ -0,0 +1,49 @@
+class Solution {
+    int[] freq; 
+    int cumOR;
+    int start ;
+    public int minimumDifference(int[] nums, int k) {
+        int n = nums.length;
+        // if(n == 1) return Math.abs(k - nums[0]);
+
+        int minDiff = Integer.MAX_VALUE;
+        freq = new int[31];
+        cumOR = 0; start = 0;
+
+        for(int end = 0; end<n; end++){
+            cumOR |= nums[end];
+            updateBitsFreq(nums[end]);
+
+            while(cumOR >= k){
+                minDiff = Math.min(Math.abs(cumOR - k), minDiff);
+                if(minDiff == 0) return 0;
+                removeStartAndUpdateORAndBitsFreq(nums[start]);
+            }
+            if(start <= end) minDiff = Math.min(Math.abs(cumOR - k), minDiff);
+            if(minDiff == 0) return 0;
+        }
+        return minDiff == Integer.MAX_VALUE ? -1 : minDiff;
+    }
+
+    private void updateBitsFreq(int num) {
+        for(int i=0; i<32 && num > 0; i++) {
+            int bit = (num & 1);
+            freq[i] += bit;
+            num >>= 1;
+        }
+    }
+
+    private void removeStartAndUpdateORAndBitsFreq (int num) {
+        start++;
+        for (int i=0; i<32 && num>0; i++) {
+            int bit = (num & 1);
+            freq[i] -= bit;
+            num >>= 1;
+
+            if(freq[i] == 0) {
+                cumOR &=(~(1<< i));
+            }
+        }
+    }
+    
+}
