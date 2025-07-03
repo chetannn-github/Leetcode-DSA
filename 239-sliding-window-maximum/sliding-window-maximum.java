eef37d07e1dec29e1730b64de97d46bcb09499c7
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-07-03 08:02:23 +0530

    Time: 33 ms (39.99%) | Memory: 58.6 MB (63.68%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.java
+++ 239-sliding-window-maximum/sliding-window-maximum.java
@@ -1,46 +1,3 @@
-// class Solution {
-//     public int[] maxSlidingWindow(int[] nums, int k) {
-//         Deque<Integer> dq = new ArrayDeque<>();
-//         int n = nums.length;
-//         int currDequeElmIdx = 0;
-//         for(int i=0; i<k; i++) {
-//             dq.add(nums[i]);
-
-//             while(dq.peek() < nums[i]) {
-//                 dq.remove();
-//                 currDequeElmIdx++;
-//             }
-//         }
-
-//         int result[] = new int[n - k + 1];
-//         result[0] = dq.peek();
-//         int start = 1;
-
-//         // System.out.println(dq.toString());
-
-//         for(int i=k; i<n; i++) {
-        
-//             while(start > currDequeElmIdx) {
-//                 dq.remove();
-//                 currDequeElmIdx++;
-//             }
-//             dq.add(nums[i]);
-//             while(dq.peek() < nums[i]) {
-//                 dq.remove();
-//                 currDequeElmIdx++;
-//             }
-
-//             result[start] = dq.peek();
-//             start++;
-            
-//         }
-
-//         return result;
-//     }
-// }
-
-
-
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         Deque<Integer> dq = new ArrayDeque<>();
@@ -50,15 +7,15 @@ class Solution {
         for(int i = 0; i < n; i++) {
             // Remove indexes which are out of the window
             while(!dq.isEmpty() && dq.peekFirst() <= i - k) {
-                dq.pollFirst();
+                dq.removeFirst();
             }
 
             // Remove all smaller elements (from the back)
             while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) {
-                dq.pollLast();
+                dq.removeLast();
             }
 
-            dq.offerLast(i); // store index
+            dq.addLast(i); // store index
 
             // Add to result once the window is valid
             if(i >= k - 1) {
