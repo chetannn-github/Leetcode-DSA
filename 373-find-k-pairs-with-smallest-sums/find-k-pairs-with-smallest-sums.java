ba22955bc383a1a9eef45a1ca4d39cf464f3d26a
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-07-04 18:37:34 +0530

    Time: 93 ms (10.18%) | Memory: 59.9 MB (58.74%) - LeetSync

--- 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ 373-find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -24,28 +24,37 @@
 
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        PriorityQueue<List<Integer>> pq = new PriorityQueue<>((a,b)->(
-            b.get(0)+b.get(1) - a.get(0)-a.get(1)
-        ));
-       
-        for(int num1 : nums1){
-            for(int num2 : nums2){
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->(
+            nums1[b.firstArrayIdx]+nums2[b.secondArrayIdx] - (nums1[a.firstArrayIdx] + nums2[a.secondArrayIdx]))
+        );
+        
+        for(int i=0; i<nums1.length; i++){
+            for(int j=0; j<nums2.length; j++){
                 // this line is importantt
                 // k bhar loo uske baad hii check krege kii bhai jo mera sbse badaa hh kyaa alge wale bhii wese hii yaa usse bekaar mil rhee tohh nhi legee
-                if(!pq.isEmpty() && pq.size() == k && pq.peek().get(0) + pq.peek().get(1) <= num1 + num2) {
-                    break;
-                }
-                pq.add(Arrays.asList(num1, num2));
+                boolean isKSmallestPairsCompleted = (!pq.isEmpty() && pq.size() == k && nums1[pq.peek().firstArrayIdx] + nums2[pq.peek().secondArrayIdx] <= nums1[i] + nums2[j]) ;
+
+                if(isKSmallestPairsCompleted) break;
+                pq.add(new Pair(i,j));
                 if(pq.size()>k) pq.remove();
 
             }
 
         }
 
-        List<List<Integer>> ans = new ArrayList<>();
-        while(k--!=0){
-            ans.add(pq.remove());
+        List<List<Integer>> result = new ArrayList<>();
+        while(k-->0){
+            result.add(List.of(nums1[pq.peek().firstArrayIdx], nums2[pq.remove().secondArrayIdx]));
         }
-        return ans;
+        return result;
+    }
+}
+
+
+class Pair {
+    int firstArrayIdx,secondArrayIdx;
+    Pair(int firstArrayIdx,int secondArrayIdx) {
+        this.firstArrayIdx = firstArrayIdx;
+        this.secondArrayIdx = secondArrayIdx;
     }
 }
