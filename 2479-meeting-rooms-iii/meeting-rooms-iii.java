155096408b6ae6d6b183088b8374c2eef8b2bb42
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-07-04 20:38:02 +0530

    Time: 120 ms (5.98%) | Memory: 102.1 MB (6.68%) - LeetSync

--- 2479-meeting-rooms-iii/meeting-rooms-iii.java
+++ 2479-meeting-rooms-iii/meeting-rooms-iii.java
@@ -1,54 +1,59 @@
 class Solution {
-    public int mostBooked(int n, int[][] meetings) {
-        PriorityQueue<int[]> futureMeetings = new PriorityQueue<>((a,b)-> (a[0] - b[0]));
-        PriorityQueue<long[]> runningMeetings = new PriorityQueue<>((a,b)-> (int)(a[1]-b[1]));
-        PriorityQueue<Integer> emptyRooms = new PriorityQueue<>(); 
+    static class Pair {
+        int roomNumber;
+        long endTime;
 
-        int[] freq =  new int[n];
+        Pair(int roomNumber, long endTime) {
+            this.roomNumber = roomNumber;
+            this.endTime = endTime;
+        }
+    }
+
+    public int mostBooked(int n, int[][] meetings) {
+        PriorityQueue<int[]> futureMeetings = new PriorityQueue<>((a, b) -> (a[0] - b[0]));
+        PriorityQueue<Pair> runningMeetings = new PriorityQueue<>((a, b) -> Long.compare(a.endTime, b.endTime));
+        PriorityQueue<Integer> emptyRooms = new PriorityQueue<>();
+
+        int[] freq = new int[n];
         int maxFreq = 0;
         int mostFrequentRoom = 0;
 
-        for(int[] meeting : meetings) {
+        for (int[] meeting : meetings) {
             futureMeetings.add(meeting);
         }
 
-        for(int i=0; i<n; i++) {
+        for (int i = 0; i < n; i++) {
             emptyRooms.add(i);
         }
 
         long currTime = 0;
 
-        while(!futureMeetings.isEmpty()) {
-            if(runningMeetings.size() == 0 || !emptyRooms.isEmpty()) {
-                if(currTime < futureMeetings.peek()[0]) currTime = futureMeetings.peek()[0];
+        while (!futureMeetings.isEmpty()) {
+            if (runningMeetings.size() == 0 || !emptyRooms.isEmpty()) {
+                if (currTime < futureMeetings.peek()[0]) currTime = futureMeetings.peek()[0];
 
-                // check krr loo koi meeting finish toh nhii ho rhiii hain 
-                while(!runningMeetings.isEmpty() && runningMeetings.peek()[1] <= currTime) {
-                    emptyRooms.add((int)runningMeetings.remove()[0]);
+                while (!runningMeetings.isEmpty() && runningMeetings.peek().endTime <= currTime) {
+                    emptyRooms.add(runningMeetings.remove().roomNumber);
                 }
-                
 
                 int roomNumber = emptyRooms.remove();
                 long endingTime = currTime + (futureMeetings.peek()[1] - futureMeetings.remove()[0]);
-                runningMeetings.add(new long[] {roomNumber,endingTime});
+                runningMeetings.add(new Pair(roomNumber, endingTime));
 
                 freq[roomNumber]++;
-                boolean hasGotMostFrequentRoom = (freq[roomNumber] > maxFreq || 
-                (freq[roomNumber] == maxFreq && mostFrequentRoom > roomNumber));
+                boolean hasGotMostFrequentRoom = (freq[roomNumber] > maxFreq ||
+                        (freq[roomNumber] == maxFreq && mostFrequentRoom > roomNumber));
 
-                if(hasGotMostFrequentRoom) {
+                if (hasGotMostFrequentRoom) {
                     mostFrequentRoom = roomNumber;
                     maxFreq = freq[roomNumber];
                 }
-            }else if(emptyRooms.size() == 0) {
-                currTime = runningMeetings.peek()[1];
-                emptyRooms.add((int)runningMeetings.remove()[0]);
+            } else if (emptyRooms.size() == 0) {
+                currTime = runningMeetings.peek().endTime;
+                emptyRooms.add(runningMeetings.remove().roomNumber);
             }
-
-
         }
 
         return mostFrequentRoom;
-              
     }
 }
