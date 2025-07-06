22b7849805bb012ed1340d41fe72eb7a57423500
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-07-06 10:27:45 +0530

    Time: 3 ms (66.75%) | Memory: 41.7 MB (59.67%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,42 +1,86 @@
+// class Solution {
+//     public String reorganizeString(String s) {
+//         HashMap<Character,Integer> freq = new HashMap<>();
+        
+//         for(char ch : s.toCharArray()) {
+//             freq.put(ch, freq.getOrDefault(ch,0) + 1);
+//         }
+//         PriorityQueue<Character> pq = new PriorityQueue<>((a,b)-> (freq.get(b) - freq.get(a)));
+
+//         for(char key : freq.keySet()) {
+//             pq.add(key);
+//         }
+
+//         StringBuilder sb = new StringBuilder();
+//         char prev = '$';
+
+//         while(!pq.isEmpty()) {
+//             char curr = pq.remove();
+//             sb.append(curr);
+
+//             if(prev != '$') pq.add(prev);
+
+//             if(freq.get(curr) > 1) {
+//                 freq.put(curr,freq.get(curr) - 1);
+//                 prev = curr;
+//             }
+//             else {
+//                 freq.remove(curr);
+//                 prev = '$';
+//             }
+            
+//         }
+
+//         if(prev != sb.charAt(sb.length() - 1) && prev != '$') {
+//             sb.append(prev);
+//         }
+       
+
+//         return sb.length() == s.length() ? sb.toString() : "";
+
+//     }
+// }
+
+
 class Solution {
     public String reorganizeString(String s) {
-        HashMap<Character,Integer> freq = new HashMap<>();
+        int[] freq = new int[26];
         
-        for(char ch : s.toCharArray()) {
-            freq.put(ch, freq.getOrDefault(ch,0) + 1);
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
         }
-        PriorityQueue<Character> pq = new PriorityQueue<>((a,b)-> (freq.get(b) - freq.get(a)));
 
-        for(char key : freq.keySet()) {
-            pq.add(key);
+        PriorityQueue<Character> pq = new PriorityQueue<>((a, b) -> freq[b - 'a'] - freq[a - 'a']);
+
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            if (freq[ch - 'a'] > 0) {
+                pq.add(ch);
+            }
         }
 
         StringBuilder sb = new StringBuilder();
         char prev = '$';
 
-        while(!pq.isEmpty()) {
+        while (!pq.isEmpty()) {
             char curr = pq.remove();
             sb.append(curr);
 
-            if(prev != '$') pq.add(prev);
-
-            if(freq.get(curr) > 1) {
-                freq.put(curr,freq.get(curr) - 1);
-                prev = curr;
+            if (prev != '$') {
+                pq.add(prev);
             }
-            else {
-                freq.remove(curr);
+
+            if (freq[curr - 'a'] > 1) {
+                freq[curr - 'a']--;
+                prev = curr;
+            } else {
                 prev = '$';
             }
-            
         }
 
-        if(prev != sb.charAt(sb.length() - 1) && prev != '$') {
+        if (prev != '$' && prev != sb.charAt(sb.length() - 1)) {
             sb.append(prev);
         }
-       
 
         return sb.length() == s.length() ? sb.toString() : "";
-
     }
 }
