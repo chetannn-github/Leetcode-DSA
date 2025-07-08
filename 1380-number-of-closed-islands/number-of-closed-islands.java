da405a37bbd0041a8c566dc3ce0dd7b419376578
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-07-08 21:51:57 +0530

    Time: 5 ms (11.66%) | Memory: 44.8 MB (34.54%) - LeetSync

---
+++ 1380-number-of-closed-islands/number-of-closed-islands.java
@@ -0,0 +1,62 @@
+class Solution {
+    int rows,cols;
+    static int[][] dirns = {{-1,0},{1,0},{0,1},{0,-1}};
+    public int closedIsland(int[][] grid) {
+        this.rows = grid.length;
+        this.cols = grid[0].length;
+        List<Pair> list = new ArrayList<>();
+        
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                if(grid[i][j] == 0 && !isBoundary(i,j)) {
+                    list.add(new Pair(i,j));
+                }
+            }
+        }
+        
+        int totalClosedIslands = 0 ;
+        for(Pair p : list) {
+            if(grid[p.x][p.y] == 0 && dfs(p,grid)) totalClosedIslands++;
+        }
+        return totalClosedIslands;
+    }
+
+
+    public boolean dfs(Pair curr, int[][] grid) {
+        int x = curr.x, y = curr.y;
+        boolean ans = true;
+        if(isBoundary(x,y)) ans = false;;
+        grid[x][y] = 1;
+
+        for(int[] dirn : dirns) {
+            int nx = x + dirn[0];
+            int ny = y + dirn[1];
+
+            if(nx >= 0 && ny >= 0 && nx < rows && ny < cols) {
+                if(grid[nx][ny] == 0) {
+                    grid[nx][ny] = 1;
+                    if(!dfs(new Pair(nx,ny),grid)) ans = false;
+                    
+                }
+            }
+        }
+
+        return ans;
+        
+    }
+
+    public boolean isBoundary(int x, int y) {
+        return (x==0 || y==0 || x== rows-1 || y == cols-1);
+    }
+}
+
+
+class Pair {
+    int x, y;
+    Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    
+}
