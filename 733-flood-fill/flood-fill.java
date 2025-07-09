8ef3ca634b84380f2f00312f024e7f4f661c22bb
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-07-09 14:23:16 +0530

    Time: 2 ms (10.48%) | Memory: 45.3 MB (19.77%) - LeetSync

---
+++ 733-flood-fill/flood-fill.java
@@ -0,0 +1,41 @@
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        if(image[sr][sc] == color) return image;
+        
+        Queue<Pair> queue = new LinkedList<>();
+        int[][] dirns = {{-1,0},{1,0},{0,1},{0,-1}};
+        int rows = image.length;
+        int cols = image[0].length;
+        int val = image[sr][sc];
+        queue.add(new Pair(sr,sc));
+
+        while(!queue.isEmpty()) {
+            int currSize = queue.size();
+            while(currSize -->0) {
+                Pair curr = queue.remove();
+                image[curr.x][curr.y] = color;
+
+                for(int[] dirn : dirns) {
+                    int nx = dirn[0] + curr.x;
+                    int ny = dirn[1] + curr.y;
+
+                    if(nx >=0  && ny >=0 && nx < rows && ny < cols) {
+                        if(image[nx][ny] == val) {
+                            queue.add(new Pair(nx,ny));
+                        }
+                    }
+                }  
+            }
+        }
+
+        return image;
+    }
+}
+
+class Pair {
+    int x,y;
+    Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
