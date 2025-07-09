96b6ea761bfe5d62451b8eb99d47359e5648ce7f
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-07-09 10:43:40 +0530

    Time: 50 ms (59.51%) | Memory: 167.5 MB (58.53%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.java
@@ -0,0 +1,60 @@
+class Solution {
+    int rows, cols;
+    private static int[][] dirns = {{-1,0},{1,0},{0,1},{0,-1}};
+    public int[][] highestPeak(int[][] grid) {
+        this.rows = grid.length;
+        this.cols = grid[0].length;
+        int[][] result = new int[rows][cols];
+
+        Queue<Pair> queue = new LinkedList<>();
+
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                if(grid[i][j] == 1) {
+                    queue.add(new Pair(i,j));
+                    grid[i][j] = -1;
+                }
+            }
+        }
+
+        int distance = 0;
+        while(!queue.isEmpty()) {
+            int currSize = queue.size();
+
+            while(currSize-->0) {
+                Pair curr = queue.remove();
+
+                for(int[] dirn : dirns) {
+                    int nx = dirn[0] + curr.x;
+                    int ny = dirn[1] + curr.y;
+
+                    if(nx >=0  && ny >=0 && nx < rows && ny < cols) {
+                        if(grid[nx][ny] == 1) {
+                            grid[nx][ny] = -1;
+                            queue.add(new Pair(nx,ny));
+                        }else if(grid[nx][ny] == 0) {
+                            queue.add(new Pair(nx,ny));
+                            grid[nx][ny] = -1;
+                            result[nx][ny] = distance + 1;
+                        }
+                    }
+                }
+
+            }
+
+            distance++;
+        }
+        return result;
+    }
+}
+
+
+
+
+class Pair {
+    int x, y;
+    Pair(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }  
+}
