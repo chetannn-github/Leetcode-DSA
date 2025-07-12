7f7d7f08a0daba2c98c63015170aa489ed66794b
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-07-12 20:23:18 +0530

    Time: 9 ms (55.18%) | Memory: 54.6 MB (71.15%) - LeetSync

--- 881-loud-and-rich/loud-and-rich.java
+++ 881-loud-and-rich/loud-and-rich.java
@@ -1,23 +1,77 @@
+// class Solution {
+//     public int[] loudAndRich(int[][] richer, int[] quiet) {
+//         List<List<Integer>> adj = new ArrayList<>();
+//         List<List<Integer>> parents = new ArrayList<>();
+
+//         int n = quiet.length;
+//         int[] indegree = new int[n];
+        
+//         for(int i=0;i<n; i++) {
+//             adj.add(new ArrayList<>()); 
+//             parents.add(new ArrayList<>());
+//         }
+
+//         for(int[] edge : richer){
+//             adj.get(edge[0]).add(edge[1]);
+//             parents.get(edge[1]).add(edge[0]);
+
+//             indegree[edge[1]]++;
+//         }
+//         int[] ans = new int[n];
+//         Queue<Integer> queue = new LinkedList<>();
+//         for(int i=0; i<n; i++){
+//             if(indegree[i] == 0){
+//                 queue.add(i);
+//                 ans[i] = i;
+//             }
+//         }        
+
+//         while(!queue.isEmpty()){
+//             int curr = queue.remove();
+                
+//             for(int nbr : adj.get(curr)){
+//                 if(--indegree[nbr] == 0){
+//                     queue.add(nbr);
+//                     int minLevel = quiet[nbr];
+//                     int minLevelPerson = nbr;
+
+//                     for(int parent : parents.get(nbr)){
+//                         if(quiet[ans[parent]] < minLevel){
+//                             minLevel = quiet[ans[parent]];
+//                             minLevelPerson = ans[parent];
+//                         }
+//                     }
+//                     ans[nbr] = minLevelPerson;
+//                 }
+        
+//             } 
+//         }
+//         System.out.println(parents);
+//         return ans;
+//     }
+// }
+
+
+
 class Solution {
     public int[] loudAndRich(int[][] richer, int[] quiet) {
         List<List<Integer>> adj = new ArrayList<>();
-        List<List<Integer>> parents = new ArrayList<>();
 
         int n = quiet.length;
         int[] indegree = new int[n];
         
         for(int i=0;i<n; i++) {
             adj.add(new ArrayList<>()); 
-            parents.add(new ArrayList<>());
         }
 
         for(int[] edge : richer){
             adj.get(edge[0]).add(edge[1]);
-            parents.get(edge[1]).add(edge[0]);
-
             indegree[edge[1]]++;
         }
+
         int[] ans = new int[n];
+        for(int i=0; i<n; i++) ans[i] = i; 
+
         Queue<Integer> queue = new LinkedList<>();
         for(int i=0; i<n; i++){
             if(indegree[i] == 0){
@@ -28,24 +82,18 @@ class Solution {
 
         while(!queue.isEmpty()){
             int curr = queue.remove();
-                
+             
             for(int nbr : adj.get(curr)){
+                if(quiet[ans[curr]] < quiet[ans[nbr]]) {
+                    ans[nbr] = ans[curr];
+                }
                 if(--indegree[nbr] == 0){
                     queue.add(nbr);
-                    int minLevel = quiet[nbr];
-                    int minLevelPerson = nbr;
-
-                    for(int parent : parents.get(nbr)){
-                        if(quiet[ans[parent]] < minLevel){
-                            minLevel = quiet[ans[parent]];
-                            minLevelPerson = ans[parent];
-                        }
-                    }
-                    ans[nbr] = minLevelPerson;
                 }
         
             } 
         }
+        // System.out.println(parents);
         return ans;
     }
 }
