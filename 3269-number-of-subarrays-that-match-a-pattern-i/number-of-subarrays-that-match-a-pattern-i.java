d748419a0fe586eddf445358b211b9a3847e6179
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-07-17 10:08:48 +0530

    Time: 10 ms (5.18%) | Memory: 44.9 MB (10.98%) - LeetSync

---
+++ 3269-number-of-subarrays-that-match-a-pattern-i/number-of-subarrays-that-match-a-pattern-i.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int countMatchingSubarrays(int[] nums, int[] pattern) {
+        KMP kmp = new KMP();
+        StringBuilder patternString = new StringBuilder();
+        
+        for(int i=0; i<pattern.length; i++) {
+            if(pattern[i] == -1) pattern[i] = 2;
+            patternString.append(pattern[i]);
+        }
+        StringBuilder text = new StringBuilder();
+
+        for(int i=1; i<nums.length; i++) {
+            int curr = 0;
+            if(nums[i] > nums[i-1] ) curr = 1;
+            else if (nums[i] < nums[i-1] ) curr = 2;
+            text.append(curr);
+        }
+
+        return kmp.KMPSearch(text.toString(),patternString.toString());
+    }
+
+}
+
+
+class KMP {
+    
+    public int[] computePrefixFunction(String s) {
+        int n = s.length();
+        int[] LPS = new int[n];
+        for (int i = 1; i < n; i++) {
+            int j = LPS[i - 1];
+            while (j > 0 && s.charAt(i) != s.charAt(j)) {
+                j = LPS[j - 1];
+            }
+            if (s.charAt(i) == s.charAt(j)) {
+                j++;
+            }
+            LPS[i] = j;
+        }
+        return LPS;
+    }
+
+    
+    public int KMPSearch(String text, String pattern) {
+        String combined = pattern + "#" + text;
+        int[] LPS = computePrefixFunction(combined);
+        int n = pattern.length();
+        
+        int result = 0;
+        for (int i = n + 1; i < LPS.length; i++) {
+            if (LPS[i] == n) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
+
+
