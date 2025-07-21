098bccc78cd672ec855b8a9865db16665e67b624
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-07-21 18:07:15 +0530

    Time: 33 ms (34.83%) | Memory: 56.4 MB (91.95%) - LeetSync

---
+++ 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int minimumRounds(int[] nums) {
+        HashMap<Integer,Integer> hm = new HashMap<>();
+
+        for(int num : nums) hm.put(num,hm.getOrDefault(num,0)+1);
+
+
+        int ops = 0;
+
+        for(int key : hm.keySet()) {
+            int freq = hm.get(key);
+            if(freq % 3 == 0) {
+                ops += Math.ceil((double)freq/3);
+
+            }else{
+                int rem = freq % 3;
+                int threeOps = freq/3;
+
+                if(rem == 1) {
+                    threeOps--;
+                    rem += 3;
+                }
+
+                if(threeOps < 0) return -1;
+                int twoOps = rem / 2;
+                ops += threeOps + twoOps; 
+            }
+        }
+
+        return ops;
+    }
+}
