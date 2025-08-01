0465f570110d973672116da26290b4a9fe3611e0
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-01 23:01:15 +0530

    Time: 11 ms (100.00%) | Memory: 58.1 MB (42.09%) - LeetSync

--- 2725-mice-and-cheese/mice-and-cheese.java
+++ 2725-mice-and-cheese/mice-and-cheese.java
@@ -1,26 +1,45 @@
+// class Solution {
+//     public int miceAndCheese(int[] reward1, int[] reward2, int k) {
+//         int n = reward1.length;
+//         HashSet<Integer> added = new HashSet<>();
+//         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->(
+//             (reward1[b] - reward2[b]) -(reward1[a] - reward2[a]))
+//         );
+        
+//         for(int i=0; i<n; i++) pq.add(i);
+//         int totalPoints = 0;
+
+//         while(k-->0) {
+//             int currPointIdx = pq.remove();
+//             totalPoints += reward1[currPointIdx];
+//             added.add(currPointIdx);
+//         }
+
+//         for(int i=0; i<n; i++) {
+//             if(!added.contains(i)) {
+//                 totalPoints += reward2[i];
+//             }
+//         }
+
+//         return totalPoints;
+//     }
+// }
+
+
 class Solution {
     public int miceAndCheese(int[] reward1, int[] reward2, int k) {
-        int n = reward1.length;
-        HashSet<Integer> added = new HashSet<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->(
-            (reward1[b] - reward2[b]) -(reward1[a] - reward2[a]))
-        );
+        int totalPoints = 0, n = reward1.length;
+        int diff[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            diff[i] = reward1[i] - reward2[i];
+            totalPoints += reward2[i];
+        }
         
-        for(int i=0; i<n; i++) pq.add(i);
-        int totalPoints = 0;
-
-        while(k-->0) {
-            int currPointIdx = pq.remove();
-            totalPoints += reward1[currPointIdx];
-            added.add(currPointIdx);
+        Arrays.sort(diff);
+        for (int i = 0; i < k; i++){
+            totalPoints += diff[n - 1 - i];
         }
-
-        for(int i=0; i<n; i++) {
-            if(!added.contains(i)) {
-                totalPoints += reward2[i];
-            }
-        }
-
         return totalPoints;
     }
 }
