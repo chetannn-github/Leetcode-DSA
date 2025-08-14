b49ec2556c75be9b5a1b74458614cf340fce5475
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-14 11:02:05 +0530

    Time: 63 ms (41.04%) | Memory: 79.7 MB (29.40%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
@@ -2,20 +2,19 @@ class Solution {
     int n;
     long dp[][];
     public long maxAlternatingSum(int[] nums) {
-        
-        List<Integer> temp = new ArrayList<>();
         n = nums.length;
         dp = new long[n][2];
+
         for(long[] row : dp){
             Arrays.fill(row,-1);
         }
-        int isAdd = 1;
-        return solve(nums,0,isAdd);
+        
+        return solve(nums,0,1);
     }
 
 
     public long solve(int[] nums,int start,int isAdd){
-        if(start >=n){
+        if(start >= n){
             return 0;
         }
         if(dp[start][isAdd] != -1){
@@ -23,14 +22,14 @@ class Solution {
         }
         long taken;
 
-        if(isAdd ==1){
+        if(isAdd == 1){
             taken = nums[start] + solve(nums, start+1,0);
         }else{
             taken = -nums[start] + solve(nums, start+1,1);
         }
        
        
-        long notTaken = 0 + solve(nums, start+1,isAdd);
+        long notTaken = solve(nums, start+1,isAdd);
 
         return dp[start][isAdd] = Math.max(notTaken, taken);
     }
