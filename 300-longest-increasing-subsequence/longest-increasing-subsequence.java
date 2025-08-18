361cd4afd5915661b1d48d550b168f9f7a0adbda
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-18 07:30:51 +0530

    Time: 369 ms (14.68%) | Memory: 79.7 MB (8.88%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.java
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.java
@@ -1,33 +1,62 @@
-class Solution {
-    int dp[][];
-    public int lengthOfLIS(int[] nums) {
-        dp = new int[nums.length][nums.length];
+// class Solution {
+//     int dp[][];
+//     public int lengthOfLIS(int[] nums) {
+//         dp = new int[nums.length][nums.length];
 
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
-        }
+//         for(int[] row : dp){
+//             Arrays.fill(row,-1);
+//         }
         
-        return solve(nums,-1,0);
+//         return solve(nums,-1,0);
+//     }
+
+//     public int solve(int[] nums, int last, int start){
+//         if(start >= nums.length){
+//             return 0;
+//         }
+//         if(last != -1 && dp[start][last] != -1){
+//             return dp[start][last];
+//         }
+//         int taken = 0;
+//         if(last == -1 || nums[start] > nums[last]){
+//             taken = 1 + solve(nums,start, start+1);
+//         }
+
+//         int skip  = solve(nums,last, start+1);
+
+//         if(last!=-1){
+//             dp[start][last] = Math.max(skip, taken);
+//         }
+//         return Math.max(skip, taken);
+    
+//     }
+// }
+
+
+class Solution {
+    int[][] dp;
+    
+    public int lengthOfLIS(int[] nums) {
+        dp = new int[nums.length][nums.length + 1]; // +1 to handle -1 index case
+        for (int[] row : dp) {
+            Arrays.fill(row, -1);
+        }
+        return solve(nums, -1, 0);
     }
 
-    public int solve(int[] nums, int last, int start){
-        if(start >= nums.length){
-            return 0;
-        }
-        if(last != -1 && dp[start][last] != -1){
-            return dp[start][last];
-        }
+    private int solve(int[] nums, int last, int start) {
+        if (start == nums.length) return 0;
+
+        // shift last index by +1
+        if (dp[start][last + 1] != -1) return dp[start][last + 1];
+
         int taken = 0;
-        if(last == -1 || nums[start] > nums[last]){
-            taken = 1 + solve(nums,start, start+1);
+        if (last == -1 || nums[start] > nums[last]) {
+            taken = 1 + solve(nums, start, start + 1);
         }
 
-        int skip  = solve(nums,last, start+1);
+        int skip = solve(nums, last, start + 1);
 
-        if(last!=-1){
-            dp[start][last] = Math.max(skip, taken);
-        }
-        return Math.max(skip, taken);
-    
+        return dp[start][last + 1] = Math.max(skip, taken);
     }
 }
