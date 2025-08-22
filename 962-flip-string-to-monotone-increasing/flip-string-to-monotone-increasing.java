c1164a3d354b20f96e8f67abf2705d8fa0f56986
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-22 16:29:20 +0530

    Time: 79 ms (11.30%) | Memory: 69.5 MB (11.78%) - LeetSync

---
+++ 962-flip-string-to-monotone-increasing/flip-string-to-monotone-increasing.java
@@ -0,0 +1,50 @@
+class Solution {
+    int n;
+    int[][] dp;
+    public int minFlipsMonoIncr(String s) {
+        this.n = s.length();
+        dp = new int[n][2];
+        for(int[] row : dp) Arrays.fill(row,-1);
+
+        boolean isZero = s.charAt(0) == '0';
+        int flip = Integer.MAX_VALUE;
+        int notFlip = Integer.MAX_VALUE;
+
+        if(isZero) {
+            flip = 1 + solve(s,1, 1);
+            notFlip = solve(s,0,1);
+        }else {
+            flip = 1 + solve(s,0, 1);
+            notFlip = solve(s,1,1);
+        }
+
+        return Math.min(flip, notFlip);
+    }
+
+    public int solve(String s, int prevVal, int currIdx) {
+        if(currIdx >= n) return 0;
+
+        if(dp[currIdx][prevVal] != -1) return dp[currIdx][prevVal];
+    
+        boolean isZero = s.charAt(currIdx) == '0';
+        int flip = Integer.MAX_VALUE;
+        int notFlip = Integer.MAX_VALUE;
+
+        if(prevVal == 0) {
+            if(isZero) {
+                notFlip = solve(s,prevVal, currIdx + 1);
+            }else {
+                flip = 1 + solve(s,0, currIdx + 1);
+                notFlip = solve(s,1,currIdx + 1);
+            }
+        }else {
+            if(isZero) {
+                flip = 1 + solve(s,1, currIdx + 1);
+            }else {
+                notFlip = solve(s,prevVal, currIdx + 1);
+            }
+        }
+
+        return dp[currIdx][prevVal] = Math.min(flip, notFlip);
+    }
+}
