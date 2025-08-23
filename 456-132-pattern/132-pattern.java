14355ce4b7b14fed1e55618a68c66cfd1bf17539
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-23 13:11:14 +0530

    Time: 54 ms (5.27%) | Memory: 76.1 MB (40.39%) - LeetSync

--- 456-132-pattern/132-pattern.java
+++ 456-132-pattern/132-pattern.java
@@ -1,10 +1,17 @@
+// chetann broo this is crazy brain i have used ever
+// tell me tell you the story behind this i was learning unique bst count
+// wiki told us that it is equal to catayln number 
+// then another applications are given then there is thing called stack sortable permutation from 1 to n is also equal to catalyn number 
+// then i read little more about then it says some scientist researched that except 231 pattern all can be sorted by single stack then i got an idea i haved eariler solved 132 pattern cant we reverse thhe array and find 231 pattern 
+
+
 class Solution {
     public boolean find132pattern(int[] nums) {
         reverseArray(nums);
         return !checkStackSortable(nums);
     }
 
-
+    /// knuth algo to find stack sort 
     public static boolean checkStackSortable(int[] arr) {
         Stack<Integer> stack = new Stack<>();
         int n = arr.length;
