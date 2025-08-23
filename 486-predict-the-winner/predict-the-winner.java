53bb14c8059484121aa2555761669d2553c9b487
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-23 17:59:29 +0530

    Time: 1 ms (55.33%) | Memory: 41.3 MB (27.14%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.java
+++ 486-predict-the-winner/predict-the-winner.java
@@ -1,91 +1,25 @@
-// whyyy this fails because wo toh hamesha optimally khelegaa
-// tumm yhha greedly dekh rhe ho har step prrr 
-// optimally mtlab overalll win
-// so whenever options comes recursion comes in the picturee......
-
-// class Solution {
-//     public boolean predictTheWinner(int[] nums) {
-        
-//         int start =0;
-//         int end = nums.length-1;
-
-//         int count = 0;
-//         boolean first = true;
-//         int p1 = 0;
-
-//         while(count<nums.length){
-//             if(first && nums[start]<=nums[end]){
-//                 p1 += nums[end];
-//                 end--;
-//                 first = !first;
-//             }else if(first && nums[start]>nums[end]){
-//                 p1 +=  nums[start];
-//                 start++;
-//                 first = !first;
-//             }else if(!first && nums[start]<=nums[end]){
-//                 p1 -= nums[end];
-//                 end--;
-//                 first = !first;
-//             }else if(!first && nums[start]>nums[end]){
-//                 p1 -=  nums[start];
-//                 start++;
-//                 first = !first;
-//             }
-//             count++;
-//         }
-
-//         return p1>=0;
-//     }
-// }
-
-
-
-// class Solution {
-//     public boolean predictTheWinner(int[] nums) {
-//         int sum = 0;
-//         for(int num : nums){
-//             sum += num;
-//         }
-//         int p1 = solve(nums, 0, nums.length-1);
-//         int p2 = sum - p1;
-//         return p1>=p2;
-//     }
-
-//     public int solve(int[] nums, int start , int end){
-//         if(start>end){
-//             return 0;
-//         }
-//         if(start == end){
-//             return nums[end];
-//         }
-
-//         int option1 = nums[start] + Math.min(solve(nums,start+2,end), solve(nums,start+1,end-1));
-//         int option2 = nums[end] + Math.min(solve(nums,start+1,end-1), solve(nums,start,end-2));
-
-//         return Math.max(option1 , option2);
-//     }
-// }
-
-
-
 class Solution {
+    int[][][] dp;
     public boolean predictTheWinner(int[] nums) {
-        
-        int cummulativeScore = solve(nums, 0, nums.length-1);
-        return cummulativeScore>=0;
+        int n = nums.length;
+        dp = new int[n][n][2];
+
+        for(int[][] grid : dp) {
+            for(int[] row : grid) {
+                Arrays.fill(row,-1);
+            }
+        }
+        return solve(nums,0,n-1, 1) >=0 ;
     }
 
-    public int solve(int[] nums, int start , int end){
-        if(start>end){
-            return 0;
-        }
-        if(start == end){
-            return nums[end];
-        }
+    public int solve(int[] nums, int left, int right, int me) {
+        if(left > right) return 0;
+        if(dp[left][right][me] != -1) return dp[left][right][me];
 
-        int option1 = nums[start] - solve(nums,start+1,end);
-        int option2 = nums[end] - solve(nums,start,end-1);
+        
+        int opt1 = nums[left] - solve(nums,left+1, right, 1-me);
+        int opt2 = nums[right] - solve(nums,left, right-1, 1-me);
 
-        return Math.max(option1 , option2);
+        return dp[left][right][me] = Math.max(opt1, opt2);
     }
 }
