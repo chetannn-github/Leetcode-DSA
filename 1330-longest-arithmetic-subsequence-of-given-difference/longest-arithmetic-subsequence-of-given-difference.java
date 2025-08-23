d4037d64785b4b64d88862d1786db51bbffc78df
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-23 19:30:44 +0530

    Time: 47 ms (37.94%) | Memory: 57.8 MB (40.98%) - LeetSync

--- 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.java
+++ 1330-longest-arithmetic-subsequence-of-given-difference/longest-arithmetic-subsequence-of-given-difference.java
@@ -1,58 +1,48 @@
+// class Solution {
+//     int n;
+//     int[] dp;
+//     public int longestSubsequence(int[] arr, int d) {
+//         n = arr.length;
+//         dp = new int[n];
+
+//         Arrays.fill(dp, -1);
+//         int result = 0;
+//         for(int i=0; i<n; i++) {
+//             result = Math.max(result, 1 + solve(arr, d,i));
+//         }
+
+//         return result;
+//     }   
+
+//     public int solve(int[] arr, int d, int lastIdx) {
+//         if(lastIdx == n) return 0;
+//         if(dp[lastIdx] != -1) return dp[lastIdx];
+
+//         int result = 0;
+//         for(int i = lastIdx + 1; i<n; i++) {
+//             if(arr[i] - arr[lastIdx] == d) {
+//                 result = Math.max(result, 1 + solve(arr, d,i));
+//             }
+//         }
+
+
+//         return dp[lastIdx] = result;
+//     }
+// }
+
+
 class Solution {
-    HashMap<Integer,List<Integer>> hm;
-    int n;
-    int dp[];
-    public int longestSubsequence(int[] arr, int diff) {
-        int maxLength = 0;
-        n = arr.length;
-        dp = new int[n];
-        Arrays.fill(dp,-1);
+    public int longestSubsequence(int[] arr, int d) {
+        Map<Integer, Integer> dp = new HashMap<>();
+        int result = 1;
 
-        hm = new HashMap<>();
-        for(int i = 0; i<n; i++){
-            if(!(hm.containsKey(arr[i])) ){
-                List<Integer> occr = new ArrayList<>();
-                occr.add(i);
-                hm.put(arr[i],occr);
-                
-            }else{
-                List<Integer> occr = hm.get(arr[i]);
-                occr.add(i);
-                hm.put(arr[i], occr);
-            }
+        for (int curr : arr) {
+            dp.put(curr, dp.getOrDefault(curr - d, 0) + 1);
+            result = Math.max(result, dp.get(curr));
         }
 
-        for(int i=0;i<n; i++){
-            maxLength  = Math.max(1 + solve(diff, i, arr),maxLength);
-        }
-        return maxLength;
-    }
+        return result;
+    }   
 
-
-    public int solve(int diff,int last, int[] arr){
-        if(last==n-1){
-            return 0;
-        }
-        if(dp[last]!=-1){
-            return dp[last];
-        }
-        int next = arr[last] + diff ; // AP ka next term 
-
-        if(hm.containsKey(next)){
-            // aage aur investigation kroo kii koi last se bade index prr hh kya 
-            List<Integer> occ = hm.get(next);
-            
-            int max = 0;
-            for(int i = 0; i<occ.size(); i++){
-                int nextGoodIdx = occ.get(i);
-                if( nextGoodIdx > last){
-                    max = Math.max(max, 1 + solve(diff,nextGoodIdx,arr));
-                }
-            }
-            return dp[last] = max;
-        } 
-
-        return dp[last] = 0;
-    }
+    
 }
- 
