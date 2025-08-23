3a39b77a7d2a5fc4ac23cc9da8f99cdd7207b8d1
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-23 16:24:58 +0530

    Time: 14 ms (23.03%) | Memory: 125.5 MB (18.16%) - LeetSync

--- 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
+++ 2262-solving-questions-with-brainpower/solving-questions-with-brainpower.java
@@ -8,19 +8,14 @@ class Solution {
         return solve(questions,0);
     }
 
-    public long solve(int[][] questions,int start){
-        if(start>=n){
-            return 0;
-        }
 
-        if(dp[start]!= -1){
-            return dp[start];
-        }
+    public long solve(int[][] questions, int currIdx) {
+        if(currIdx >= n) return 0L;
+        if(dp[currIdx] != -1) return dp[currIdx];
 
-        long take = questions[start][0] + solve(questions,start+questions[start][1]+1);
+        long doit = questions[currIdx][0] + solve(questions,currIdx + questions[currIdx][1] + 1);
+        long skip = solve(questions,currIdx + 1);
 
-        long skip = solve(questions,start+1);
-
-        return dp[start] = Math.max(take,skip);
+        return dp[currIdx] = Math.max(doit, skip);
     }
 }
