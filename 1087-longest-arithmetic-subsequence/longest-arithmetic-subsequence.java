ab4db2605c3fea00ec0bb587c9b8aecf1c7638e0
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-23 19:43:21 +0530

    Time: 743 ms (18.73%) | Memory: 45.5 MB (91.44%) - LeetSync

--- 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
+++ 1087-longest-arithmetic-subsequence/longest-arithmetic-subsequence.java
@@ -1,59 +1,22 @@
 class Solution {
-    HashMap<Integer,List<Integer>> hm;
-    int n;
-    int dp[][];
-    public int longestArithSeqLength(int[] arr) {
-        
+    public int longestArithSeqLength(int[] nums) {
+        int n = nums.length;
+        HashSet<Integer> possibleD = new HashSet<>();
+        for(int i=0; i<n-1; i++) {
+            for(int j=i+1; j<n; j++) {
+                int d = nums[j] - nums[i];
+                possibleD.add(d);
+            }
+        }
         int maxLength = 0;
-        n = arr.length;
-        dp = new int[n][n];
-
-        hm = new HashMap<>();
-        for(int i = 0; i<n; i++){
-            if(!(hm.containsKey(arr[i])) ){
-                List<Integer> occr = new ArrayList<>();
-                occr.add(i);
-                hm.put(arr[i],occr);
-                
-            }else{
-                List<Integer> occr = hm.get(arr[i]);
-                occr.add(i);
-                hm.put(arr[i], occr);
+        for(int d : possibleD) {
+            HashMap<Integer,Integer> dp = new HashMap<>();
+            for(int num : nums) {
+                dp.put(num, dp  .getOrDefault(num - d, 0) + 1);
+                maxLength = Math.max(maxLength, dp  .get(num));
             }
-            Arrays.fill(dp[i],-1);
-        }
+        }   
 
-        for(int i=0;i<n; i++){
-            for(int j = i+1; j<n; j++){
-                maxLength  = Math.max(2 + solve(i, j, arr),maxLength);
-            }
-        }
         return maxLength;
-    }
-
-
-    public int solve(int first,int second, int[] arr){
-        
-        if(dp[first][second]!=-1){
-            return dp[first][second];
-        }
-        int next = arr[second] + arr[second] - arr[first] ; // AP ka next term 
-
-        if(hm.containsKey(next)){
-            // aage aur investigation kroo kii koi second se bade index prr hh kya 
-            
-            List<Integer> occ = hm.get(next);
-            
-            int max = 0;
-            for(int i = 0; i<occ.size(); i++){
-                int nextGoodIdx = occ.get(i);
-                if( nextGoodIdx > second){
-                    max = Math.max(max, 1 + solve(second,nextGoodIdx,arr));
-                }
-            }
-            return dp[first][second] = max;
-        } 
-
-        return dp[first][second] = 0;
     }
 }
