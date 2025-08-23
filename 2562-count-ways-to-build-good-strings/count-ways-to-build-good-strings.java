10b4f5c687c19f0835476e0b387f2be2a559d489
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-23 16:53:21 +0530

    Time: 18 ms (10.65%) | Memory: 55 MB (30.00%) - LeetSync

--- 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
+++ 2562-count-ways-to-build-good-strings/count-ways-to-build-good-strings.java
@@ -1,31 +1,32 @@
 class Solution {
-    int dp[];
-    int mod = 1_000_000_007;
+    long[] dp;
+    int MOD = 1_000_000_007;
     public int countGoodStrings(int low, int high, int zero, int one) {
-        dp = new int[high+1];
-        Arrays.fill(dp,-1);
-        return  solve(low,high,zero,one,0);
+        dp = new long[high + 1];
+        Arrays.fill(dp, -1);
+        long upperLimit = solve(high, zero, one,0);
+        Arrays.fill(dp, -1);
+        long lowerLimit = solve(low-1, zero, one,0);
+        long res = (upperLimit - lowerLimit + MOD) % MOD;
+        return (int) (res % MOD);  
     }
 
+    public long solve(int maxLimit, int zero, int one, int currLength) {
+        if(currLength >= maxLimit) return 0;
+        if(dp[currLength] != -1) return dp[currLength];
 
-    public int solve(int low, int high, int zero, int one, int currentLength){
-        
-        int ans = 0;
-        if(dp[currentLength] != -1){
-            return dp[currentLength];
-        }
-        if(currentLength + zero >= low && currentLength + zero <= high){
-            ans += 1 + solve(low,high,zero,one, currentLength+ zero);
-        }else if(currentLength + zero < low){
-            ans += solve(low,high,zero,one, currentLength+ zero);
-        }
-        
-        if(currentLength+ one >= low && currentLength + one <= high){
-            ans += 1 + solve(low,high,zero,one, currentLength+ one);
-        }else if(currentLength + one < low){
-            ans += solve(low,high,zero,one, currentLength+ one);
+        long addZero = 0, addOne = 0;
+
+        if(currLength + zero <= maxLimit) {
+            addZero = 1 + solve(maxLimit,zero, one, currLength + zero);
         }
 
-        return dp[currentLength] = ans % mod;
+        if(currLength + one <= maxLimit) {
+            addOne = 1 + solve(maxLimit,zero, one, currLength + one);
+        }
+
+        long res = (long) (addZero + addOne) % MOD;
+
+        return dp[currLength] = res;
     }
 }
