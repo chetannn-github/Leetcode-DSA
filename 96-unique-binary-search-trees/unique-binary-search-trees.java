ff5536a2b30181ed9f3eff70bf69d5d476bfb959
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-24 17:16:04 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (48.80%) - LeetSync

--- 96-unique-binary-search-trees/unique-binary-search-trees.java
+++ 96-unique-binary-search-trees/unique-binary-search-trees.java
@@ -1,14 +1,25 @@
 class Solution {
+    int[][] dp;
+    int NOT_VISITED_FLAG = -1;
     public int numTrees(int N) {
-        int[] C = new int[N+1];
-        C[0] = 1;
-for (int n=1; n<=N; n++) {
-    for (int i=0; i<n; i++) {
-        C[n] += C[i] * C[n-1-i];
+        dp = new int[N+1][N+1];
+        for(int[] row : dp ) Arrays.fill(row,NOT_VISITED_FLAG);
+
+        return solve(1,N);
     }
-}
 
-return C[N];
+    public int solve(int start, int end) {
+        if(start >= end ) return 1;
+        if(dp[start][end] != NOT_VISITED_FLAG) return dp[start][end];
 
+        int result = 0;
+        for(int i=start; i<=end; i++) {
+            int leftPossible = solve(start, i-1);
+            int rightPossible = solve(i+1, end);
+
+            result += leftPossible * rightPossible;
+        }
+
+        return dp[start][end] = result;
     }
 }
