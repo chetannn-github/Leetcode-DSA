b4b232c8924f4c6617bb3b0ec4679c87f8ecac30
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-25 18:15:24 +0530

    Time: 6 ms (5.67%) | Memory: 43.1 MB (5.00%) - LeetSync

--- 91-decode-ways/decode-ways.java
+++ 91-decode-ways/decode-ways.java
@@ -2,47 +2,38 @@ class Solution {
     Set<String> hs = new HashSet<>();
     int n ;
     int dp[];
-
     public int numDecodings(String s) {
         for (int i = 1; i <= 26; i++) {
             String key = String.valueOf(i);
             hs.add(key);
         }
+
         n = s.length();
         dp = new int[n];
         Arrays.fill(dp,-1);
 
         return solve(s,0);
-        
     }
 
-    public int solve(String s, int start){
-        
-        if(start == n){
-            return 1;
+
+    public int solve(String s, int curr) {
+        if(curr >= n ) return 1;
+        if(dp[curr] != - 1) return dp[curr];
+
+        int takeOne = 0, takeTwo = 0;
+        char currChar = s.charAt(curr);
+        if(hs.contains(currChar + "")) {
+            takeOne = solve(s,curr + 1);
         }
 
-        if(dp[start] != -1){
-            return dp[start];
+        if(curr + 1 != n) {
+            String key = currChar + "" + s.charAt(curr + 1);
+            if(hs.contains(key)) {
+                takeTwo = solve(s, curr+2);
+            }
         }
 
-        String one = s.substring(start, start+1);
-        int r1 = 0;
-        int r2 = 0;
-        if(hs.contains(one)){
-           r1 = solve(s,start+1);
-        }
+        return dp[curr] = takeOne + takeTwo;
 
-        dp[start] = r1;
-        if(start+1 == n){
-            return r1;
-        }
-
-        String two = s.substring(start, start+2);
-        if(hs.contains(two)){
-            r2 = solve(s,start+2);
-        }
-        dp[start] = r1+r2;
-        return r1+r2;
     }
 }
