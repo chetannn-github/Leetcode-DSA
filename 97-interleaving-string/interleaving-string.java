6c51ef35a95dcbcf9c8ad8854ca31c29e66609d9
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-25 16:59:15 +0530

    Time: 17 ms (5.63%) | Memory: 57.5 MB (5.09%) - LeetSync

--- 97-interleaving-string/interleaving-string.java
+++ 97-interleaving-string/interleaving-string.java
@@ -1,48 +1,42 @@
 class Solution {
-    int l1, l2, l3;
-    int dp[][][];
+    int n1, n2, n3;
+    int[][][] dp;
     public boolean isInterleave(String s1, String s2, String s3) {
-        l1 = s1.length();
-        l2 = s2.length();
-        l3 = s3.length();
-        dp = new int[l1+1][l2+1][l3];
+        n1 = s1.length();
+        n2 = s2.length();
+        n3 = s3.length();
+        if(n1 + n2 != n3) return false;
         
+        dp = new int[n1+1][n2+1][n3+1];
+
         for(int[][] grid: dp){
             for(int[] row : grid ){
                 Arrays.fill(row,-1);
             }
         }
-        if(l1+l2!= l3) return false;
-
+        
         return solve(s1,s2,s3,0,0,0);
     }
 
-    public boolean solve(String s1, String s2, String s3, int i, int j, int k){
-        if(k>=l3) return true;
-        if(dp[i][j][k] != -1){
-            return dp[i][j][k] == 0;
-        }
-        boolean ans = false;
-        
-        if(i>=l1){
-            if(s2.charAt(j) == s3.charAt(k)){
-                ans = ans || solve(s1,s2,s3,i,j+1,k+1);
-            }
-        }else if(j>=l2){
-            if(s1.charAt(i) == s3.charAt(k)){
-                ans = ans || solve(s1,s2,s3,i+1,j,k+1);
-            }
-        }else{
-            if(s1.charAt(i) == s3.charAt(k)){
-                ans = ans || solve(s1,s2,s3,i+1,j,k+1);
-            }
-            if(s2.charAt(j) == s3.charAt(k)){
-                ans = ans || solve(s1,s2,s3,i,j+1,k+1); 
-                // ek cheez batau agrr ans upr se true aagya toh fn call nhii hogiii 
-            }
-        }
-        dp[i][j][k] = ans ? 0 : 1;
 
-        return ans;
+    public boolean solve(String s1, String s2, String s3, int i,int j, int k) {
+        if(k >= n3) return true;
+
+        if(dp[i][j][k] != -1) return dp[i][j][k] == 0;
+
+        boolean result = false;
+        char ch3 = s3.charAt(k);
+
+        if(i < n1 && s1.charAt(i) == ch3) {
+            result |= solve(s1,s2,s3,i+1,j,k+1);
+        }
+
+        if(!result && j<n2 && s2.charAt(j) == ch3) {
+            result |= solve(s1,s2,s3,i,j+1,k+1);
+        }
+
+        dp[i][j][k] = result ? 0 : 1;
+    
+        return result;
     }
 }
