3a57492ae102255d6a890425888dd90ad268c66a
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-26 20:24:04 +0530

    Time: 165 ms (5.01%) | Memory: 120.7 MB (5.05%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,103 +1,28 @@
-// class Solution {
-//     int totalSum = 0;
-//     int n;
-//     int dp[][];
-//     public boolean canPartition(int[] nums) {
-//         n = nums.length;
-//         for(int num : nums){
-//             totalSum += num;
-//         }
-
-//         if(totalSum%2 != 0){
-//             return false;
-//         }
-
-//         dp = new int[n+1][totalSum +1];
-
-//         for(int row[] : dp){
-//             Arrays.fill(row, -1);
-//         }
-//         return solve(nums,0,0);
-//     }
-
-//     public boolean solve(int[] nums, int start,int sum){
-        
-//         if(2*sum == totalSum){
-//             dp[start][sum] =  0 ;
-//             return true;
-//         }
-
-//         if(2* sum>= totalSum ){
-//             dp[start][sum] =  1 ;
-//             return false;
-//         }
-
-//         if(dp[start][sum]!=-1){
-//             return dp[start][sum]== 0? true : false;
-//         }
-        
-//         boolean ans = false;
-//         for(int i= start ; i<n; i++){
-//             ans =  ans || solve(nums,i+1,sum+nums[i]);
-//             if(ans){
-//                 return true;
-//             }
-//         }
-//         dp[start][sum] = ans? 0 : 1;
-        
-//         return ans;
-//     }
-// }
-
 class Solution {
+    int N;
+    int[][] dp;
     int totalSum = 0;
-    int n;
-    int dp[][];
     public boolean canPartition(int[] nums) {
-        n = nums.length;
-        for(int num : nums){
-            totalSum += num;
-        }
+        N = nums.length;
+        
+        for(int num : nums) totalSum += num;
+        dp = new int[N][2 * totalSum + 1];
 
-        if(totalSum%2 != 0){
-            return false;
-        }
+        for(int[] row : dp) Arrays.fill(row,-1);
 
-        dp = new int[n+1][totalSum/2 + 1];
-
-        for(int row[] : dp){
-            Arrays.fill(row, -1);
-        }
-        return solve(nums,0,totalSum/2);
+        return solve(nums,0,0);
     }
 
-    public boolean solve(int[] nums, int start,int target){
-        
-        if(target==0){
-            return true;
-        }
 
-        if(start>=n){
-            return false;
-        }
-        
+    public boolean solve(int[] nums, int currIdx, int sum) {
+        if(currIdx == N) return sum == 0;
+        if(dp[currIdx][sum + totalSum] != -1) return dp[currIdx][sum + totalSum] == 1;
 
-        if(dp[start][target]!=-1){
-            return dp[start][target]== 0? true : false;
-        }
-        
-        boolean ans = false;
-        
-        if(target>= nums[start]) ans =  ans || solve(nums,start+1,target-nums[start]);
-        if(ans) {
-            dp[start][target] = ans? 0 : 1;
-            return true;
-        }
-        
-        ans = ans || solve(nums,start+1,target);
-        
-        dp[start][target] = ans? 0 : 1;
-        
-        return ans;
+        boolean result = false;
+        result = result || solve(nums,currIdx+1, sum + nums[currIdx]);
+        result = result || solve(nums,currIdx+1, sum - nums[currIdx]);
+
+        dp[currIdx][sum + totalSum] =  result ? 1 : 0;
+        return result;
     }
 }
