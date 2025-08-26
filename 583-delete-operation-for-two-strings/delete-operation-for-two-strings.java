fe6f629492bc31d742f1e955a167547d27ce3788
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-26 08:59:01 +0530

    Time: 9 ms (8.52%) | Memory: 45.4 MB (5.55%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -1,44 +1,35 @@
 class Solution {
-    int n1;
-    int n2;
-    int dp[][];
-    public int minDistance(String s1, String s2) {
-        n1 = s1.length();
-        n2 = s2.length();
+    int n1, n2;
+    int[][] dp;
+    public int minDistance(String word1, String word2) {
+        n1 = word1.length();
+        n2 = word2.length();
 
-        return  n1 + n2 - 2 *(MaxCommonSubsequence(s1,s2));
-
-
-    }
-    
-    public int MaxCommonSubsequence(String text1, String text2) {
         dp = new int[n1][n2];
-        
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
-        }
+        for(int[] row : dp ) Arrays.fill(row, -1);
 
-        return solve(text1, text2, 0,0);
+        return solve(word1, word2, 0,0);
     }
 
-    public int solve(String text1, String text2, int i, int j){
-        if(i>=n1 || j>=n2){
-            return 0;
-        } 
-        
-        if(dp[i][j]!= -1){
-            return dp[i][j];
-        }
-        char ch1 = text1.charAt(i);
-        char ch2 = text2.charAt(j);
+    private int solve(String word1,String word2, int i, int j) {
+        if(i>=n1 && j>= n2) return 0;
+        if(i>=n1) return n2- j;
+        if(j>=n2) return n1 - i;
 
-        if(ch1 == ch2){
-            return dp[i][j] = 1  + solve(text1, text2, i+1, j+1);
+        if(dp[i][j] != -1) return dp[i][j];
+
+
+        char ch1 = word1.charAt(i);
+        char ch2 = word2.charAt(j);
+        int result = 0;
+        if(ch1 == ch2) {
+            result = solve(word1,word2,i+1,j+1);
+        }else {
+            int opt1 = 1 + solve(word1,word2,i+1,j);
+            int opt2 = 1+ solve(word1,word2,i,j+1);
+
+            result = Math.min(opt1,opt2);
         }
-        int opt1 = solve(text1, text2, i+1, j);
-        int opt2 = solve(text1, text2, i, j+1);
-            
-        return dp[i][j] = Math.max(opt1,opt2);
-        
+        return dp[i][j] = result;
     }
 }
