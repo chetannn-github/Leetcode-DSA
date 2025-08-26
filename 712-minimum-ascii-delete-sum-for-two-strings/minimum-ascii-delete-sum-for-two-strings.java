b2622524101a21d884984bea6f549daa248e21a6
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-26 18:46:35 +0530

    Time: 23 ms (39.60%) | Memory: 45 MB (37.62%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,52 +1,45 @@
 class Solution {
-    int n1;
-    int n2;
-    int dp[][];
+    int n1, n2;
+    int[][] dp;int[] pre1, pre2;
     public int minimumDeleteSum(String s1, String s2) {
         n1 = s1.length();
         n2 = s2.length();
+        pre1 = new int[n1+1];
+        pre2 = new int[n2+1];
 
-        int totalASCII = 0;
-        for(int i=0; i<n1; i++){
-            totalASCII += (int) s1.charAt(i);
-        }
-        for(int i=0; i<n2; i++){
-            totalASCII += (int) s2.charAt(i);
+        for(int i=1; i < n1 + 1; i++) {
+            pre1[i] = pre1[i-1] + s1.charAt(i-1);
         }
 
-        return totalASCII - 2 *(MaxCommonSubsequence(s1,s2));
+        for(int i=1; i < n2 + 1; i++) {
+            pre2[i] = pre2[i-1] + s2.charAt(i-1);
+        }
 
-
-    }
-    
-    public int MaxCommonSubsequence(String text1, String text2) {
         dp = new int[n1][n2];
-        
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
-        }
+        for(int[] row : dp ) Arrays.fill(row, -1);
 
-        return solve(text1, text2, 0,0);
+        return solve(s1, s2, 0,0);
     }
 
-    public int solve(String text1, String text2, int i, int j){
-        if(i>=n1 || j>=n2){
-            return 0;
-        } 
-        
-        if(dp[i][j]!= -1){
-            return dp[i][j];
-        }
-        char ch1 = text1.charAt(i);
-        char ch2 = text2.charAt(j);
+    private int solve(String s1,String s2, int i, int j) {
+        if(i>=n1 && j>= n2) return 0;
+        if(i>=n1) return pre2[n2] - pre2[j];
+        if(j>=n2) return pre1[n1] - pre1[i];
 
-        if(ch1 == ch2){
-            return dp[i][j] = (int) ch1  + solve(text1, text2, i+1, j+1);
+        if(dp[i][j] != -1) return dp[i][j];
+
+
+        char ch1 = s1.charAt(i);
+        char ch2 = s2.charAt(j);
+        int result = 0;
+        if(ch1 == ch2) {
+            result = solve(s1,s2,i+1,j+1);
+        }else {
+            int opt1 = ch1 + solve(s1,s2,i+1,j);
+            int opt2 = ch2 + solve(s1,s2,i,j+1);
+
+            result = Math.min(opt1,opt2);
         }
-        int opt1 = solve(text1, text2, i+1, j);
-        int opt2 = solve(text1, text2, i, j+1);
-            
-        return dp[i][j] = Math.max(opt1,opt2);
-        
+        return dp[i][j] = result;
     }
 }
