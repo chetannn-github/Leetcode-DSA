60a7a81401f3a8533da0c980336e2b996a64c9cf
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-30 14:23:20 +0530

    Time: 1 ms (29.90%) | Memory: 45 MB (29.81%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -1,3 +1,26 @@
+// class Solution {
+//     int maximumDepth;
+//     public int diameterOfBinaryTree(TreeNode root) {
+//         maximumDepth = 0;
+//         solve(root, 1);
+//         return maximumDepth - 1;
+//     }
+
+//     public int solve(TreeNode root, int currDepth) {
+//         if(root == null) return currDepth;
+
+//         int leftDepth = solve(root.left, currDepth + 1);
+//         int rightDepth = solve(root.right, currDepth + 1);
+
+    
+//         maximumDepth = Math.max(leftDepth + rightDepth + 1 - 2 *(currDepth + 1 ) , maximumDepth);
+//         return Math.max(leftDepth, rightDepth);
+    
+//     }
+// }
+
+
+
 class Solution {
     int max ;
     public int diameterOfBinaryTree(TreeNode root) {
@@ -7,15 +30,14 @@ class Solution {
     }
 
     public int dfs(TreeNode root){
-        if(root == null){
-            return 0;
-        }
+        if(root == null) return -1;
+        
     
         int leftDepth = 1 + dfs(root.left);
         int rightDepth = 1 +  dfs(root.right);  
 
-        if(leftDepth + rightDepth -2 > max){
-            max = leftDepth + rightDepth -2 ;
+        if(leftDepth + rightDepth > max){
+            max = leftDepth + rightDepth;
         }
         
         return Math.max(leftDepth,rightDepth);
