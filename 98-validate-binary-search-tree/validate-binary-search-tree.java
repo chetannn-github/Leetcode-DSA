aa603b424f86614da7937e54de2527dc5b39c578
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-30 18:39:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.9 MB (7.85%) - LeetSync

--- 98-validate-binary-search-tree/validate-binary-search-tree.java
+++ 98-validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,21 +1,45 @@
 class Solution {
+    Integer prev = null;
     public boolean isValidBST(TreeNode root) {
-        // method 1 is simple find inorder traversal check is it sorted 
-        // m2 hum range bata degee kii buddy iss range ke alawa iss subtree me nhii
-        // hona chaiyee
-        return solve(root,-2512,-2512);
+        return inorder(root);
     }
 
-    public boolean solve(TreeNode root, int min, int max){
+    public boolean inorder(TreeNode root){
         if(root == null) return true;
+        
+        boolean result = inorder(root.left); 
 
-        if(min != -2512 && root.val <= min){
+        if(prev != null && prev >= root.val) {  
             return false;
         }
-        if(max != -2512 && root.val >= max){
-            return false;
-        }
 
-        return solve(root.left,min,root.val) && solve(root.right,root.val,max);
+        prev = root.val; 
+
+        result = result && inorder(root.right); 
+        return result;
     }
 }
+
+
+
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         // method 1 is simple find inorder traversal check is it sorted 
+//         // m2 hum range bata degee kii buddy iss range ke alawa iss subtree me nhii
+//         // hona chaiyee
+//         return solve(root,-2512,-2512);
+//     }
+
+//     public boolean solve(TreeNode root, int min, int max){
+//         if(root == null) return true;
+
+//         if(min != -2512 && root.val <= min){
+//             return false;
+//         }
+//         if(max != -2512 && root.val >= max){
+//             return false;
+//         }
+
+//         return solve(root.left,min,root.val) && solve(root.right,root.val,max);
+//     }
+// }
