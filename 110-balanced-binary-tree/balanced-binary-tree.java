c4507fd0985caa3bc6ecaa1eebecde4a332da582
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-08-30 12:12:36 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (33.30%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.java
+++ 110-balanced-binary-tree/balanced-binary-tree.java
@@ -1,25 +1,42 @@
+// class Solution {
+//     boolean isBalanced = true;
+//     public boolean isBalanced(TreeNode root) {
+//         solve(root, 0);
+//         return isBalanced;
+//     }
+
+//     public int solve(TreeNode root, int depth ) {
+//         if(root == null) return depth;
+//         if(!isBalanced) return depth;
+
+//         int depthLeft = solve(root.left, depth + 1);
+//         int depthRight = solve(root.right, depth + 1);
+//         if(isBalanced) isBalanced = Math.abs(depthLeft - depthRight) <= 1;
+
+//         return Math.max(depthLeft, depthRight);
+//     }
+// }
+
+
+
 class Solution {
-    boolean ans = true;
+    
     public boolean isBalanced(TreeNode root) {
-        dfs(root,0);
-        return ans;
+        return solve(root) != Integer.MAX_VALUE;
     }
 
-    public int dfs(TreeNode root,int currDepth){
-        if(!ans) return currDepth;
-        if(root == null) return currDepth;
+    public int solve(TreeNode root) {
+        if(root == null) return 0;
+
+        int depthLeft = solve(root.left);
+        int depthRight = solve(root.right);
         
-        int leftDepth = currDepth;
-        int rightDepth = currDepth;
-        
-        if(root.left != null){leftDepth = dfs(root.left,currDepth+1);}
-        if(root.right != null){rightDepth = dfs(root.right,currDepth+1);}
+        if(depthLeft == Integer.MAX_VALUE) return depthLeft;
+        if(depthRight == Integer.MAX_VALUE) return depthRight;
 
-        if(Math.abs(leftDepth- rightDepth) >1){
-            ans = false;
-        }
+        if(Math.abs(depthLeft - depthRight) > 1) return Integer.MAX_VALUE;
 
-        return Math.max(leftDepth,rightDepth);
 
+        return 1 + Math.max(depthLeft, depthRight);
     }
 }
