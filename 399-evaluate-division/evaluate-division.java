6f621942349e39bc20a6fff188a9d9cb45bb3fd9
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-09-07 14:06:49 +0530

    Time: 1 ms (99.19%) | Memory: 42.6 MB (23.36%) - LeetSync

--- 399-evaluate-division/evaluate-division.java
+++ 399-evaluate-division/evaluate-division.java
@@ -11,7 +11,45 @@ class Solution {
     HashMap<String,List<Pair>> adj;
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
         adj = new HashMap<>();
+        constructGraph(equations,values);
 
+        double[] result = new double[queries.size()];
+        int idx = 0;
+        for(List<String> q : queries){
+            String u = q.get(0);
+            String v = q.get(1);
+            
+            if(!adj.containsKey(u) || !adj.containsKey(v) ){
+                result[idx++] = -1.0;
+                continue;
+            }
+            
+            result[idx++] = dfs(u,v,1.0, new HashSet<>());
+
+        }
+        return result;
+    }
+
+    public double dfs(String u, String v, double curr, HashSet<String> visited){
+        if(u.equals(v)) return curr;
+        visited.add(u);
+        double result = -1.0;
+
+        for(Pair nb : adj.get(u)){
+            String nbr = nb.to;
+            
+            if(!visited.contains(nbr)){
+                visited.add(nbr);
+                double possibleAns = dfs(nbr,v,(double) curr * nb.wt,visited) ;
+                if(possibleAns != -1.0){
+                    return possibleAns;
+                };
+            }
+        }   
+        return result;
+    }
+
+    public void constructGraph(List<List<String>> equations, double[] values) {
         int idx = 0;
         for(List<String> eq : equations){
             String u = eq.get(0);
@@ -25,39 +63,5 @@ class Solution {
             ls.add(new Pair(u,(double) 1/values[idx++]));
             adj.put(v,ls);
         }
-        double[] ans = new double[queries.size()];
-        idx = 0;
-        for(List<String> q : queries){
-            String u = q.get(0);
-            String v = q.get(1);
-            
-            if(!adj.containsKey(u) || !adj.containsKey(v) ){
-                ans[idx++] = -1.0;
-                continue;
-            }
-            
-            ans[idx++] = dfs(u,v,1.0, new HashSet<>());
-
-        }
-        return ans;
-    }
-
-    public double dfs(String u, String v, double curr, HashSet<String> visited){
-        if(u.equals(v)) return curr;
-        visited.add(u);
-        double ans = -1.0;
-
-        for(Pair nb : adj.get(u)){
-            String nbr = nb.to;
-            
-            if(!visited.contains(nbr)){
-                visited.add(nbr);
-                double possibleAns = dfs(nbr,v,(double) curr * nb.wt,visited) ;
-                if(possibleAns != -1.0){
-                    return possibleAns;
-                };
-            }
-        }   
-        return ans;
     }
 }
