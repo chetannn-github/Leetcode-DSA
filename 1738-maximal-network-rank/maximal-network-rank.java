b1ce3ad02e8aff5a56925af8a5be12707a297b7d
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-09-09 08:13:20 +0530

    Time: 13 ms (55.14%) | Memory: 45.4 MB (89.09%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.java
+++ 1738-maximal-network-rank/maximal-network-rank.java
@@ -1,22 +1,36 @@
 class Solution {
     public int maximalNetworkRank(int n, int[][] roads) {
-        List<HashSet<Integer>> adj = new ArrayList<>();
-        for(int i=0; i<n; i++) adj.add(new HashSet<>());
+        HashSet<Integer>[] graph = new HashSet[n];
+        int[] degree = new int[n];
 
-        for(int[] road : roads){
-            adj.get(road[0]).add(road[1]);
-            adj.get(road[1]).add(road[0]);
+        for(int i=0; i<n; i++) graph[i] = new HashSet<>();
+
+        for(int[] road : roads) {
+            int u = road[0], v = road[1];
+            degree[u]++;
+            degree[v]++;
+
+            HashSet<Integer> nbr = graph[u];
+            nbr.add(v);
+            graph[u] = nbr;
+
+            nbr = graph[v];
+            nbr.add(u);
+            graph[v] = nbr;
+
         }
-        int max = 0;
 
-        for(int i=0; i<n; i++){
-            for(int j = i+1; j<n; j++){
-                int count = adj.get(i).size();
-                if(adj.get(j).contains(i)) count--;
-                count += adj.get(j).size();
-                max = Math.max(count, max);
+
+        int result = 0;
+
+        for(int i=0; i<n; i++) {
+            for(int j=i+1; j<n; j++) {
+                int sub = graph[i].contains(j) ? -1 : 0;
+                int currRes = degree[i] + degree[j] +  sub;
+                result = result > currRes ? result : currRes;
             }
         }
-        return max;
+        
+        return result;
     }
 }
