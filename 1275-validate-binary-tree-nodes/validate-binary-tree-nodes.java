cfaeda6d70bb660a4eea898a04996fe70cf87a71
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-09-09 20:24:09 +0530

    Time: 5 ms (72.22%) | Memory: 45.2 MB (96.42%) - LeetSync

--- 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.java
+++ 1275-validate-binary-tree-nodes/validate-binary-tree-nodes.java
@@ -1,34 +1,84 @@
+// class Solution {
+//     public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
+//         HashMap<Integer,Integer> hm = new HashMap<>();
+        
+//         for(int i=0; i<n; i++){
+//             if(leftChild[i]== -1 && rightChild[i] == -1) continue;
+//             if(hm.containsKey(leftChild[i]) || hm.containsKey(rightChild[i])){
+//                 return false; // ek child ke two parents
+//             }
+//             if(hm.containsKey(i) && (hm.get(i) == leftChild[i] ||hm.get(i) == rightChild[i] )){
+//                 return false; // cycle detection
+//             }
+            
+//             if(leftChild[i] != -1) hm.put(leftChild[i],i);
+//             if(rightChild[i] != -1) hm.put(rightChild[i],i);
+        
+//         }
+//         int root = -1;
+
+//         for(int i=0; i<n; i++){
+//             if(!hm.containsKey(i)){
+//                 if(root == -1 ) root = i;
+//                 else return false; // two roots
+//             }
+//         }
+
+//         if(root == -1) return false;
+
+//         Queue<Integer> queue = new LinkedList<>();
+//         queue.add(root);
+//         int count = 0;
+
+//         while(!queue.isEmpty()){
+//             int curr = queue.remove();
+//             count++;
+
+//             if(leftChild[curr] != -1) queue.add(leftChild[curr]);
+//             if(rightChild[curr] != -1) queue.add(rightChild[curr]);
+//         }
+        
+//         return count ==n;
+
+        
+//     }
+// }
+
+
 class Solution {
     public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
-        HashMap<Integer,Integer> hm = new HashMap<>();
-        
-        for(int i=0; i<n; i++){
-            if(leftChild[i]== -1 && rightChild[i] == -1) continue;
-            if(hm.containsKey(leftChild[i]) || hm.containsKey(rightChild[i])){
-                return false; // ek child ke two parents
+        int[] indegree = new int[n];
+        for(int i=0; i<n; i++) {
+            if(leftChild[i] != -1) {
+                indegree[leftChild[i]]++;
+                if(indegree[leftChild[i]] > 1) return false;
             }
-            if(hm.containsKey(i) && (hm.get(i) == leftChild[i] ||hm.get(i) == rightChild[i] )){
-                return false; // cycle detection
-            }
-            
-            if(leftChild[i] != -1) hm.put(leftChild[i],i);
-            if(rightChild[i] != -1) hm.put(rightChild[i],i);
-        
-        }
-        int root = -1;
 
-        for(int i=0; i<n; i++){
-            if(!hm.containsKey(i)){
-                if(root == -1 ) root = i;
-                else return false; // two roots
+            if(rightChild[i] != -1) {
+                indegree[rightChild[i]]++;
+                if(indegree[rightChild[i]] > 1) return false;
+            }
+        }
+
+        // sirf ek root hona Chaiye 
+        boolean rootFound = false;
+        int root = -1;
+        for(int i=0; i<n; i++) {
+            int in = indegree[i];
+            if(in == 0) {
+                if(rootFound) return false;
+                rootFound = true;
+                root = i;
             }
         }
 
         if(root == -1) return false;
+        
 
         Queue<Integer> queue = new LinkedList<>();
         queue.add(root);
         int count = 0;
+    
 
         while(!queue.isEmpty()){
             int curr = queue.remove();
@@ -36,9 +86,12 @@ class Solution {
 
             if(leftChild[curr] != -1) queue.add(leftChild[curr]);
             if(rightChild[curr] != -1) queue.add(rightChild[curr]);
+
+
+            if(count > n) return false;
         }
         
-        return count ==n;
+        return count == n;
 
         
     }
