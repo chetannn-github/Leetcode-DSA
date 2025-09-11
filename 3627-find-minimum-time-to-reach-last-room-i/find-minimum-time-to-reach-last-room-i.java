b7ec291734677abfc46c747332736384ee4b753e
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-09-11 07:43:09 +0530

    Time: 9 ms (39.26%) | Memory: 44.5 MB (91.92%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.java
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.java
@@ -1,54 +1,74 @@
 class Solution {
-    int[][] dirns = {{1,0}, {-1,0}, {0,1}, {0,-1}};
-
     public int minTimeToReach(int[][] moveTime) {
-        int m = moveTime.length, n = moveTime[0].length;
-        int[][] distance = new int[m][n];
-        for(int i=0; i<m; i++) {
-            Arrays.fill(distance[i],Integer.MAX_VALUE);
-        }
+        return dijkstra(moveTime);
+    }
 
-        PriorityQueue<Triplet> pq = new PriorityQueue<>((a,b)->(a.wt - b.wt));
-        pq.add(new Triplet(0,0,0));
-        distance[0][0] = 0;
 
-        while(!pq.isEmpty()) {
-            Triplet curr = pq.remove();
-            int x = curr.x, y = curr.y , wt = curr.wt;
+    public int dijkstra(int[][] arr) {
+        int rows = arr.length, cols = arr[0].length;
+        int[][] dist = new int[rows][cols];
 
-            if(x == m-1 && y == n-1) return distance[m-1][n-1];
-
-            for(int[] dirn : dirns) {
-                int nx = x + dirn[0], ny = y + dirn[1];
-
-                boolean isNonOutOfBound = (nx >= 0 && nx < m && ny >= 0 && ny < n);
-
-                if(isNonOutOfBound) {
-                   
-                    if(wt < moveTime[nx][ny] && distance[nx][ny] > moveTime[nx][ny] + 1) { 
-                        pq.add(new Triplet(nx,ny,moveTime[nx][ny] + 1));
-                        distance[nx][ny] = moveTime[nx][ny] + 1;
-                    }else if(wt >= moveTime[nx][ny] && distance[nx][ny] > wt+1) {
-                        pq.add(new Triplet(nx,ny,wt+1));
-                        distance[nx][ny] = wt+1;
-                    }
-                }
+        for(int i=0; i<rows; i++) {
+            for(int j=0; j<cols; j++) {
+                dist[i][j] = Integer.MAX_VALUE;
             }
         }
 
-        return distance[m-1][n-1];
+        PriorityQueue<Triplet> pq = new PriorityQueue<>((a,b)->(a.cost - b.cost));
+        pq.add(new Triplet(0,0,0));
+        dist[0][0] = 0;
+
+        while(!pq.isEmpty()) {
+            Triplet curr = pq.remove();
+            int x = curr.x, y = curr.y, cost = curr.cost;
+
+            if(dist[x][y] < cost) continue;
+            if(x + 1 < rows) {
+                int nbrCost = Math.max(cost,arr[x+1][y]) + 1;
+                if (dist[x+1][y] > nbrCost) {
+                    dist[x+1][y] = nbrCost;
+                    pq.add(new Triplet(x+1,y,nbrCost));
+                }
+            }
 
 
+            if(x - 1 >= 0) {
+                int nbrCost = Math.max(cost,arr[x-1][y]) + 1;
+                if (dist[x-1][y] > nbrCost) {
+                    dist[x-1][y] = nbrCost;
+                    pq.add(new Triplet(x-1,y,nbrCost));
+                }
+            }
+
+            if(y+1 < cols) {
+                int nbrCost = Math.max(cost,arr[x][y+1]) + 1;
+                if (dist[x][y+1] > nbrCost) {
+                    dist[x][y+1] = nbrCost;
+                    pq.add(new Triplet(x,y+1,nbrCost));
+                }
+            }
+
+            if(y-1 >=0) {
+                int nbrCost = Math.max(cost,arr[x][y-1]) + 1;
+                if (dist[x][y-1] > nbrCost) {
+                    dist[x][y-1] = nbrCost;
+                    pq.add(new Triplet(x,y-1,nbrCost));
+                }
+            }
+
+            
+        }
+
+        return dist[rows-1][cols-1];
     }
 }
+
 
 class Triplet {
-    int x, y, wt;
-    Triplet(int x, int y, int wt) {
+    int x, y, cost;
+    Triplet(int x, int y, int cost) {
         this.x = x;
         this.y = y;
-        this.wt = wt;
+        this.cost = cost;
     }
 }
-
-
