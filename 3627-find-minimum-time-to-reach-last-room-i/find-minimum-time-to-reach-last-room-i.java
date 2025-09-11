cabb37b424860fe2fb6bfcc600cdff637df5aaf4
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-09-11 07:46:23 +0530

    Time: 8 ms (61.20%) | Memory: 43.9 MB (100.00%) - LeetSync

--- 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.java
+++ 3627-find-minimum-time-to-reach-last-room-i/find-minimum-time-to-reach-last-room-i.java
@@ -5,6 +5,7 @@ class Solution {
 
 
     public int dijkstra(int[][] arr) {
+        int[][] dirns = {{1,0}, {-1,0}, {0,1}, {0,-1}};
         int rows = arr.length, cols = arr[0].length;
         int[][] dist = new int[rows][cols];
 
@@ -21,42 +22,22 @@ class Solution {
         while(!pq.isEmpty()) {
             Triplet curr = pq.remove();
             int x = curr.x, y = curr.y, cost = curr.cost;
-
             if(dist[x][y] < cost) continue;
-            if(x + 1 < rows) {
-                int nbrCost = Math.max(cost,arr[x+1][y]) + 1;
-                if (dist[x+1][y] > nbrCost) {
-                    dist[x+1][y] = nbrCost;
-                    pq.add(new Triplet(x+1,y,nbrCost));
+
+            for(int[] dirn : dirns) {
+                int nx = x + dirn[0], ny = y + dirn[1];
+                boolean isNonOutOfBound = (nx >= 0 && nx < rows && ny >= 0 && ny < cols);
+
+                if(isNonOutOfBound) {
+                    int nbrCost = Math.max(cost,arr[nx][ny]) + 1;
+
+                    if (dist[nx][ny] > nbrCost) {
+                    dist[nx][ny] = nbrCost;
+                    pq.add(new Triplet(nx,ny,nbrCost));
+                }
+
                 }
             }
-
-
-            if(x - 1 >= 0) {
-                int nbrCost = Math.max(cost,arr[x-1][y]) + 1;
-                if (dist[x-1][y] > nbrCost) {
-                    dist[x-1][y] = nbrCost;
-                    pq.add(new Triplet(x-1,y,nbrCost));
-                }
-            }
-
-            if(y+1 < cols) {
-                int nbrCost = Math.max(cost,arr[x][y+1]) + 1;
-                if (dist[x][y+1] > nbrCost) {
-                    dist[x][y+1] = nbrCost;
-                    pq.add(new Triplet(x,y+1,nbrCost));
-                }
-            }
-
-            if(y-1 >=0) {
-                int nbrCost = Math.max(cost,arr[x][y-1]) + 1;
-                if (dist[x][y-1] > nbrCost) {
-                    dist[x][y-1] = nbrCost;
-                    pq.add(new Triplet(x,y-1,nbrCost));
-                }
-            }
-
-            
         }
 
         return dist[rows-1][cols-1];
