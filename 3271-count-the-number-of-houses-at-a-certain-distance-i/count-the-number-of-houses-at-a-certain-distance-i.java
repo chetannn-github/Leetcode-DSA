a3428d904dc00f4ad4d0897506d5f199896b4215
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-09-11 17:34:40 +0530

    Time: 77 ms (53.93%) | Memory: 45.8 MB (6.74%) - LeetSync

--- 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.java
+++ 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.java
@@ -39,23 +39,12 @@
 // dimag me aata hain a ke nbr uske bade wale hii banae aurr double krr lenge value koo but wo glt hain kukii wo jldi aage jaakrr peeche aasktaa hain assume 1 --> 100 ek connection hain toh wo peeche jaakr help kregaa bro
 class Solution {
     int result[];
-    List<List<Integer>> adj;
+    List<Integer>[] graph;
     public int[] countOfPairs(int n, int x, int y) {
+        constructGraph(n,x,y);
         result = new int[n];
-        adj = new ArrayList<>();
-
-        for(int i=0; i<n; i++) {
-            List<Integer> nbr = new ArrayList<>();
-            if(x != y ) {
-                if(i == x-1) nbr.add(y-1);
-                if(i == y-1) nbr.add(x-1);
-            }
-            if(i<n-1) nbr.add(i+1);
-            if(i>0)  nbr.add(i-1);
-            adj.add(nbr);
-        }
         
-        // System.out.println(adj.toString());
+        // System.out.println(graph.toString());
         for(int i=0; i<n; i++) {
             bfs(i);
         }
@@ -77,7 +66,7 @@ class Solution {
             while(n-->0) {
                 int curr = queue.remove();
                 
-                for(int nbr : adj.get(curr)) {
+                for(int nbr : graph[curr]) {
                     if(!visited.contains(nbr)) {
                         queue.add(nbr);
                         visited.add(nbr);
@@ -87,6 +76,22 @@ class Solution {
             }
             
             result[count++] += queue.size();
+        }
+    }
+
+
+    public void constructGraph(int n, int x, int y) {
+        graph = new List[n];
+
+        for(int i=0; i<n; i++) graph[i] = new ArrayList<>();
+
+        for(int i=0; i<n; i++) {
+            if(x != y) {
+                if(i == x-1) graph[i].add(y-1);
+                if(i == y-1) graph[i].add(x-1);
+            }
+            if(i<n-1) graph[i].add(i+1);
+            if(i>0)  graph[i].add(i-1);
         }
     }
 }
