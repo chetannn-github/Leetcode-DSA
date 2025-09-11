d8115409c43815569ba4c9f4e3d05d5b3c476285
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-09-11 08:07:25 +0530

    Time: 15 ms (18.85%) | Memory: 49.1 MB (59.86%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.java
@@ -1,43 +1,79 @@
 class Solution {
     int MOD = 1_000_000_007;
+    
     public int countPaths(int n, int[][] roads) {
-        List<List<List<Integer>>> adj = new ArrayList<>();
+        List<Node>[] graph = new List[n];
+        constructGraph(graph,roads,n);
+        return dijkstra(graph,n);
 
-        for(int i=0; i<n; i++) adj.add(new ArrayList<>());
+        
+    }
 
-        for(int[] road : roads){
-            adj.get(road[0]).add(Arrays.asList(road[1],road[2]));
-            adj.get(road[1]).add(Arrays.asList(road[0],road[2]));
+    private void constructGraph( List<Node>[] graph, int[][] edges, int n) {
+        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
+
+        for(int[] edge : edges) {
+            int u = edge[0], v = edge[1], wt = edge[2];
+            graph[u].add(new Node(v, wt));
+            graph[v].add(new Node(u, wt));
         }
+    }
 
-        PriorityQueue<long[]> pq = new PriorityQueue<>((a,b)->(a[0] - b[0] > 0 ? 1 : -1));
-        pq.add(new long[]{0,0}); // wt , u
-        long[] count = new long[n];
+    private int dijkstra(List<Node>[] graph, int n) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> Long.compare(a.dist, b.dist));
+        pq.add(new Pair(0, 0));  //  node,dist
+
         long[] dist = new long[n];
-        Arrays.fill(dist,Long.MAX_VALUE);
+        long[] count = new long[n];
+        Arrays.fill(dist, Long.MAX_VALUE);
+
         dist[0] = 0;
         count[0] = 1;
 
-        while(!pq.isEmpty()){
-            long curr[] = pq.remove();
-            long u = curr[1];
-            long wt = curr[0];
+        while (!pq.isEmpty()) {
+            Pair curr = pq.poll();
+            int u = curr.node;
+            long wt = curr.dist;
 
-            for(List<Integer> nbr : adj.get((int)u)){
-                int v = nbr.get(0);
-                int cost = nbr.get(1);
+            if (wt > dist[u]) continue;
 
-                if((long)cost + wt < dist[v]){
-                    dist[v] = (long)cost + wt;
-                    pq.add(new long[]{dist[v],v});
-                    count[v] = count[(int) u];
-                }else if(cost + wt == dist[v]){
-                    count[v] += count[(int) u];
-                    count[v] %=MOD;
+            for (Node neighbor : graph[u]) {
+                int v = neighbor.v;
+                int nbrCost = neighbor.wt;
+
+                if (wt + nbrCost < dist[v]) {
+                    dist[v] = wt + nbrCost;
+                    count[v] = count[u];
+                    pq.add(new Pair(dist[v], v));
+                } else if (wt + nbrCost == dist[v]) {
+                    count[v] = (count[v] + count[u]) % MOD;
                 }
             }
         }
 
-        return (int) count[n-1] % MOD;
+        return (int) (count[n - 1] % MOD);
+    }
+}
+
+
+
+
+class Node {
+    int v;
+    int wt;
+
+    Node(int v, int wt) {
+        this.v = v;
+        this.wt = wt;
+    }
+}
+
+class Pair {
+    long dist;
+    int node;
+
+    Pair(long dist, int node) {
+        this.dist = dist;
+        this.node = node;
     }
 }
