f488eaf3c0a42551c4813d59264a242632c86fd2
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-09-13 17:22:39 +0530

    Time: 35 ms (20.12%) | Memory: 59.5 MB (5.31%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.java
+++ 310-minimum-height-trees/minimum-height-trees.java
@@ -1,37 +1,26 @@
 class Solution {
     int diameterEnd = -1, maxDepth = 0;
-    List<List<Integer>> adj;List<Integer> result;
+    List<Integer>[] graph;List<Integer> result;
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        result = new ArrayList<>();
-        if(n==1) {
-            result.add(0);
-            return result;
-        }
-        adj = new ArrayList<>();
-        for(int i=0; i<n; i++) adj.add(new ArrayList<>());
+        if (n==1) return new ArrayList<>(List.of(0));
 
-        for(int[] edge : edges) {
-            int u = edge[0], v = edge[1];
-            adj.get(u).add(v);
-            adj.get(v).add(u);
-        }
-
+        constructGraph(edges,n);result = new ArrayList<>();
         dfs(0, new HashSet<>(), 0, false);
-        int oneEnd = diameterEnd;
+        int A = diameterEnd;
+
         maxDepth = 0;
-        diameterEnd = -1;
-        dfs(oneEnd,new HashSet<>(), 0, false);
-        int secondEnd = diameterEnd;
+        dfs(A,new HashSet<>(), 0, false);
+        int B = diameterEnd;
 
         
         
-        dfs(oneEnd, new HashSet<>(), 0, true);
-        // System.out.println(oneEnd+"->"+secondEnd+  "->"+maxDepth);
+        dfs(A, new HashSet<>(), 0, true);
+        // System.out.println(A+"->"+B+  "->"+maxDepth);
         return result;
     }
 
 
-    public boolean dfs(int currNode, HashSet<Integer> visited, int currDepth, boolean isAddedToList) {
+    public boolean dfs(int currNode, HashSet<Integer> visited, int currDepth, boolean isToAddInList) {
         visited.add(currNode);
     
         if(currDepth > maxDepth) {
@@ -39,17 +28,19 @@ class Solution {
             diameterEnd = currNode;
         }
 
-        boolean ans = currNode == diameterEnd;
-        for(int nbr : adj.get(currNode)) {
+        // bhai jo path B tak le jaae whii add kr skta hain jb depth suitable aaye
+
+        boolean isPathToEndOfDiameter = currNode == diameterEnd;
+        for(int nbr : graph[currNode]) {
             if(!visited.contains(nbr)) {
-                if(dfs(nbr,visited, currDepth + 1,isAddedToList)) {
-                    ans = true;
+                if(dfs(nbr,visited, currDepth + 1,isToAddInList)) {
+                    isPathToEndOfDiameter = true;
                 }
             }
         }
         
         
-        if(ans && isAddedToList) {
+        if(isPathToEndOfDiameter && isToAddInList) {
             if((maxDepth & 1) == 1) {
                 if(currDepth == maxDepth/2 || currDepth == maxDepth / 2 + 1) {
                     result.add(currNode);
@@ -59,6 +50,19 @@ class Solution {
             }
         }
 
-        return ans;
+        return isPathToEndOfDiameter;
+        
+    }
+
+
+    public void constructGraph(int[][] edges, int n) {
+        graph = new List[n];
+        for(int i=0; i<n; i++) graph[i] = new ArrayList<>();
+
+        for(int[] edge : edges) {
+            int u = edge[0], v = edge[1];
+            graph[u].add(v);
+            graph[v].add(u);
+        }
     }
 }
