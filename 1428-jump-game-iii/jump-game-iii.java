ada36811528799547511cfbcfb871e7ca9a4d01f
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-09-13 13:40:24 +0530

    Time: 30 ms (6.95%) | Memory: 67.8 MB (5.06%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.java
+++ 1428-jump-game-iii/jump-game-iii.java
@@ -1,32 +1,33 @@
 class Solution {
     HashMap<Integer,List<Integer>> adj;
-    HashSet<Integer> dest;
+    HashSet<Integer> destinations;
     public boolean canReach(int[] arr, int start) {
         if(arr[start] == 0) return true;
 
         this.adj = new HashMap<>();
         int length = arr.length;
-        dest = new HashSet<>();
+        destinations = new HashSet<>();
 
         for(int i=0; i<length; i++) {
-            if(arr[i] == 0) dest.add(i);
-            else {List<Integer> nbr = adj.getOrDefault(i,new ArrayList<>());
+            if(arr[i] == 0) destinations.add(i);
+            else {
+                List<Integer> nbr = adj.getOrDefault(i,new ArrayList<>());
                 if (i + arr[i] < length)  nbr.add(i + arr[i]);
                 if (i - arr[i] >= 0) nbr.add( i - arr[i]);
                 adj.put(i,nbr);
             }
         }
         
-        return dfs(start,dest, new HashSet<>());
+        return dfs(start,destinations, new HashSet<>());
     }
 
-    public boolean dfs(int start, HashSet<Integer> dest, HashSet<Integer> visited) {
-        if(dest.contains(start)) return true;
+    public boolean dfs(int start, HashSet<Integer> destinations, HashSet<Integer> visited) {
+        if(destinations.contains(start)) return true;
         visited.add(start);
 
         for(int nbr : adj.getOrDefault(start, new ArrayList<>())) {
             if(!visited.contains(nbr)) {
-                if(dfs(nbr,dest,visited)) return true;
+                if(dfs(nbr,destinations,visited)) return true;
             }
         }
 
