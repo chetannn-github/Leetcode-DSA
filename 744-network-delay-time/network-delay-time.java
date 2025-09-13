7388af9084b792f92cb02c7fa2edfb16e1858114
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-09-13 15:29:22 +0530

    Time: 12 ms (56.06%) | Memory: 48.9 MB (47.14%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,19 +1,31 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int source) {
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
+        List<Node>[] graph = constructGraph(times,n+1);
+        int[] minDelayFromSource = dijkstra(graph, source, n+1);
+        
+        int maxDelay = findMaxOfArray(minDelayFromSource);
+        return  maxDelay == Integer.MAX_VALUE ? -1 : maxDelay;
+    }
 
-        List<List<Node>> adj = new ArrayList<>();
-        for(int i = 0; i <= n; i++) {
-            adj.add(new ArrayList<>());
+    public List<Node>[] constructGraph(int[][] edges, int n) {
+        List<Node>[] graph = new List[n];
+
+        for(int i = 0; i < n; i++) {
+            graph[i] = new ArrayList<>();
         }
 
-        for(int[] time : times) {
-            int u = time[0], v = time[1], wt = time[2];
-            adj.get(u).add(new Node(v, wt));
+        for(int[] edge : edges) {
+            int u = edge[0], v = edge[1], wt = edge[2];
+            graph[u].add(new Node(v, wt));
         }
+        return graph;
+    }
 
-        dist[source] = 0;
+    public int[] dijkstra(List<Node>[] graph, int source, int n) {
+        int[] minDistanceFromSrc = new int[n];
+        Arrays.fill(minDistanceFromSrc, Integer.MAX_VALUE);
+       
+        minDistanceFromSrc[source] = 0;
         PriorityQueue<Pair> pq = new PriorityQueue<>((a, b) -> a.dist - b.dist);
         pq.add(new Pair(0, source));
         int max = 0;
@@ -23,42 +35,46 @@ class Solution {
             int d = curr.dist;
             int u = curr.node;
 
-            if (d > dist[u]) continue;
+            if (d > minDistanceFromSrc[u]) continue;
 
-            for (Node neighbor : adj.get(u)) {
+            for (Node neighbor : graph[u]) {
                 int v = neighbor.v;
                 int wt = neighbor.wt;
 
-                if (d + wt < dist[v]) {
-                    dist[v] = d + wt;
-                    pq.add(new Pair(dist[v], v));
+                if (d + wt < minDistanceFromSrc[v]) {
+                    minDistanceFromSrc[v] = d + wt;
+                    pq.add(new Pair(minDistanceFromSrc[v], v));
                 }
             }
         }
 
-        for (int i = 1; i <= n; i++) {
-            if(dist[i] == Integer.MAX_VALUE) return -1;
-            max = Math.max(max, dist[i]);
-        }
+        return minDistanceFromSrc;
+    }
 
-        return max;
+    public int findMaxOfArray(int[] arr) {
+        int maxVal = Integer.MIN_VALUE;
+
+        for(int i = 1; i<arr.length; i++) {
+            maxVal = Math.max(maxVal, arr[i]);
+        }
+        return maxVal;
     }
 }
 
 
 
 class Node {
-        int v, wt;
-        Node(int v, int wt) {
-            this.v = v;
-            this.wt = wt;
-        }
+    int v, wt;
+    Node(int v, int wt) {
+        this.v = v;
+        this.wt = wt;
     }
+}
 
-    class Pair {
-        int dist, node;
-        Pair(int dist, int node) {
-            this.dist = dist;
-            this.node = node;
-        }
+class Pair {
+    int dist, node;
+    Pair(int dist, int node) {
+        this.dist = dist;
+        this.node = node;
     }
+}
