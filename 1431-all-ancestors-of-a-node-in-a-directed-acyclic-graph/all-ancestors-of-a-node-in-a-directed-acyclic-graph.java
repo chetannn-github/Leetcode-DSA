f73d2a347629c41f8242ee7846d018332d994fe7
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-09-14 11:52:30 +0530

    Time: 107 ms (35.23%) | Memory: 101 MB (10.64%) - LeetSync

--- 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
+++ 1431-all-ancestors-of-a-node-in-a-directed-acyclic-graph/all-ancestors-of-a-node-in-a-directed-acyclic-graph.java
@@ -1,103 +1,118 @@
+class Solution {
+    public List<List<Integer>> getAncestors(int n, int[][] edges) {
+        int[] indegree = new int[n];
+        List<Integer>[] graph = new List[n];
+        constructGraphAndCalcIndegree(n, edges, indegree, graph);
+        
+
+        List<Set<Integer>> ancestors = new ArrayList<>();
+        for(int i=0; i<n; i++) ancestors.add(new HashSet<>());
+        
+
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i=0; i<n; i++){
+            if(indegree[i] == 0) queue.add(i);
+        }
+
+        while(!queue.isEmpty()){
+            int curr = queue.remove();
+
+            for(int nbr : graph[curr]){
+                ancestors.get(nbr).add(curr);
+                for(int parentAncestor : ancestors.get(curr)){
+                    ancestors.get(nbr).add(parentAncestor);
+                }
+                
+                if(--indegree[nbr] == 0){
+                    queue.add(nbr);   
+                }
+            }
+        }
+
+        
+        return convertListOfSetToSortedList(ancestors,n);
+    }
+
+
+    private void constructGraphAndCalcIndegree(int n,int[][] edges,int[] indegree ,List<Integer>[] graph) {
+        for(int i=0; i<n; i++) graph[i] = new ArrayList<>();
+
+        for(int[] edge : edges){
+            int u = edge[0], v = edge[1];
+            graph[u].add(v);
+            indegree[v]++;
+        }
+    }
+
+
+    private List<List<Integer>> convertListOfSetToSortedList(List<Set<Integer>> ancestors, int n){
+        List<List<Integer>> result = new ArrayList<>();
+
+        for(int i=0; i<n; i++){
+            List<Integer> list = new ArrayList<>();
+            for(int ancestor : ancestors.get(i)){
+                list.add(ancestor);
+            }
+            Collections.sort(list);
+            result.add(list);
+        }
+
+        return result;
+    }
+}
+
+
+
+// jab indegree zero ho chukii hogii tb uske saree parents nipat chuke hogeee
 // class Solution {
 //     public List<List<Integer>> getAncestors(int n, int[][] edges) {
-//         int[] indegree = new int[n];
-//         List<List<Integer>> adj = new ArrayList<>();
+//         List<List<Integer>> graph = new ArrayList<>();        
+//         List<List<Integer>> parents = new ArrayList<>();    
 //         List<Set<Integer>> ancestors = new ArrayList<>();
+//         int[] indegree = new int[n];
 
-//         for(int i=0; i<n; i++) {
-//             adj.add(new ArrayList<>());
+//         for (int i = 0; i < n; i++) {
+//             graph.add(new ArrayList<>());
+//             parents.add(new ArrayList<>());
 //             ancestors.add(new HashSet<>());
 //         }
-//         for(int[] edge : edges){
-//             adj.get(edge[0]).add(edge[1]);
-//             indegree[edge[1]]++;
+
+//         for (int[] edge : edges) {
+//             int u = edge[0], v = edge[1];
+//             graph.get(u).add(v);      
+//             parents.get(v).add(u);  
+//             indegree[v]++;
 //         }
 
 //         Queue<Integer> queue = new LinkedList<>();
-//         for(int i=0; i<n; i++){
-//             if(indegree[i] == 0) queue.add(i);
+//         for (int i = 0; i < n; i++) {
+//             if (indegree[i] == 0) queue.add(i);
 //         }
 
-//         while(!queue.isEmpty()){
-//             int curr = queue.remove();
+//         while (!queue.isEmpty()) {
+//             int curr = queue.poll();
 
-//             for(int nbr : adj.get(curr)){
-//                 ancestors.get(nbr).add(curr);
-//                 for(int parentAncestor : ancestors.get(curr)){
-//                     ancestors.get(nbr).add(parentAncestor);
-//                 }
-                
-//                 if(--indegree[nbr] == 0){
-//                     queue.add(nbr);   
+            
+//             for (int child : graph.get(curr)) {
+//                 indegree[child]--;
+
+//                 if (indegree[child] == 0) {
+//                     for (int parent : parents.get(child)) {
+//                         ancestors.get(child).add(parent);
+//                         ancestors.get(child).addAll(ancestors.get(parent));
+//                     }
+//                     queue.add(child);
 //                 }
 //             }
 //         }
 
 //         List<List<Integer>> ans = new ArrayList<>();
-//         for(int i=0; i<n; i++){
-//             List<Integer> list = new ArrayList<>();
-//             for(int ancestor : ancestors.get(i)){
-//                 list.add(ancestor);
-//             }
-//             Collections.sort(list);
-//             ans.add(list);
+//         for (int i = 0; i < n; i++) {
+//             List<Integer> sortedList = new ArrayList<>(ancestors.get(i));
+//             Collections.sort(sortedList);
+//             ans.add(sortedList);
 //         }
+
 //         return ans;
 //     }
 // }
-
-
-
-// jab indegree zero ho chukii hogii tb uske saree parents nipat chuke hogeee
-class Solution {
-    public List<List<Integer>> getAncestors(int n, int[][] edges) {
-        List<List<Integer>> adj = new ArrayList<>();        
-        List<List<Integer>> parents = new ArrayList<>();    
-        List<Set<Integer>> ancestors = new ArrayList<>();
-        int[] indegree = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            adj.add(new ArrayList<>());
-            parents.add(new ArrayList<>());
-            ancestors.add(new HashSet<>());
-        }
-
-        for (int[] edge : edges) {
-            int u = edge[0], v = edge[1];
-            adj.get(u).add(v);      
-            parents.get(v).add(u);  
-            indegree[v]++;
-        }
-
-        Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) queue.add(i);
-        }
-
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
-
-            
-            for (int child : adj.get(curr)) {
-                indegree[child]--;
-
-                if (indegree[child] == 0) {
-                    for (int parent : parents.get(child)) {
-                        ancestors.get(child).add(parent);
-                        ancestors.get(child).addAll(ancestors.get(parent));
-                    }
-                    queue.add(child);
-                }
-            }
-        }
-
-        List<List<Integer>> ans = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            List<Integer> sortedList = new ArrayList<>(ancestors.get(i));
-            Collections.sort(sortedList);
-            ans.add(sortedList);
-        }
-
-        return ans;
-    }
-}
