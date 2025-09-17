9f193ab65e3da066c4f9eeff374a8a30a91c4636
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-09-17 18:36:04 +0530

    Time: 360 ms (96.69%) | Memory: 106.5 MB (80.84%) - LeetSync

--- 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
+++ 421-maximum-xor-of-two-numbers-in-an-array/maximum-xor-of-two-numbers-in-an-array.java
@@ -1,16 +1,20 @@
 class Solution {
     public int findMaximumXOR(int[] nums) {
         Trie prefixTree = new Trie();
+
         for(int num : nums) {
             prefixTree.insert(num);
         }
+
         int maxXOR = 0;
-        for(int num : nums){
-            maxXOR = Math.max(maxXOR,prefixTree.findMaxXOR(num));
+        for(int num : nums) {
+            maxXOR = Math.max(maxXOR, prefixTree.findMaxXOR(num));
         }
         return maxXOR;
     }
 }
+
+
 
 class Trie {
     TrieNode root;
@@ -18,56 +22,50 @@ class Trie {
         root = new TrieNode();
     }
 
-    void insert (int num) {
+    void insert(int num) {
         TrieNode curr = root;
-        for(int i=31 ; i>=0; i--){
-            int bit = (num>>i) & 1;
 
-            if(bit == 0 ){
-                if(curr.zero == null ) curr.zero = new TrieNode();
-                curr = curr.zero;
-            }else{
-                if(curr.one == null ) curr.one = new TrieNode();
-                curr = curr.one;
+        for(int i=31; i>=0; i--) {
+            int bit = num & (1<<i);
+            if(bit == 0) {
+                if(curr.left == null) curr.left = new TrieNode();
+                curr = curr.left;
+            }else {
+                if(curr.right == null) curr.right = new TrieNode();
+                curr = curr.right;
             }
-            
-        } 
+        }
     }
 
-    int findMaxXOR (int num) {
+    int findMaxXOR(int num) {
         TrieNode curr = root;
-        int maxVal = 0;
+        int maxXORPossible = 0;
 
-        for(int i=31; i>=0; i--){
-            int bit = (num>>i) & 1;
-            int optimumVal = 1 - bit;
+        for(int i=31; i>=0; i--) {
+            int bit = num & (1<<i);
 
-            if((optimumVal == 0 && curr.zero == null) || (optimumVal == 1 && curr.one == null) ){
-                if(optimumVal == 0){
-                    curr = curr.one;
-                }else{
-                    curr = curr.zero;
+            if(bit == 0) {
+                if(curr.right != null) {
+                    curr = curr.right;
+                    maxXORPossible = (maxXORPossible | (1 << i));
+                } else {
+                    curr = curr.left;
                 }
-            }else{
-                if(optimumVal == 0){
-                    curr = curr.zero;
-                }else{
-                    curr = curr.one;
+            } else {
+                if(curr.left != null) {
+                    curr = curr.left;
+                    maxXORPossible = (maxXORPossible | (1 << i));
+                } else {
+                    curr = curr.right;
                 }
-                maxVal |= (1<<i);
             }
-            
         }
 
-        return maxVal;
+        return maxXORPossible;
     }
 }
 
+
 class TrieNode {
-    TrieNode zero;
-    TrieNode one;
-    TrieNode () {
-        zero = null;
-        one = null;
-    }
+    TrieNode left, right;
 }
