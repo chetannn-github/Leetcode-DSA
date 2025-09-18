f5fe88c3f76f4f66519433553379531afa4f59e4
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-09-18 16:03:34 +0530

    Time: 42 ms (30.53%) | Memory: 48.5 MB (37.63%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.java
+++ 1397-search-suggestions-system/search-suggestions-system.java
@@ -1,65 +1,3 @@
-// class Solution {
-//     public List<List<String>> suggestedProducts(String[] products, String searchWord) {
-//         Trie prefixTree = new Trie(); 
-//         for(String product : products) {
-//             prefixTree.insert(product);
-//         }
-//         List<List<String>> results = new ArrayList<>();
-
-//         for(int i=1; i<=searchWord.length(); i++) {
-//             String prefix = searchWord.substring(0,i);
-//             results.add(prefixTree.findThreeSuggestions(prefix));
-//         }
-//         return results;
-//     }
-// }
-
-// class Trie {
-//     TrieNode root;
-//     Trie() {
-//         root = new TrieNode();
-//     }
-//     void insert(String word) {
-//         TrieNode curr = root;
-        
-//         for(char ch : word.toCharArray()) {
-//             if(curr.children.get(ch) == null) {
-//                 curr.children.put(ch, new TrieNode());
-//             }
-//             curr = curr.children.get(ch);
-            
-//             curr.words.add(word);
-//         }
-//         curr.isEndOfWord = true;
-//     }
-
-//     List<String> findThreeSuggestions(String prefix) {
-//         TrieNode curr = root;
-        
-//         for(char ch : prefix.toCharArray()) {
-//             if(curr.children.get(ch) == null) return new ArrayList<>();
-//             curr = curr.children.get(ch);
-//             // System.out.println(curr.words.toString());
-//         }
-//         List<String> suggestions = curr.words;
-//         Collections.sort(suggestions); 
-//         return suggestions.subList(0,Math.min(3,suggestions.size()));
-//     }
-// }
-
-// class TrieNode {
-//     HashMap<Character,TrieNode> children;
-//     boolean isEndOfWord;
-//     List<String> words;
-
-//     TrieNode() {
-//         isEndOfWord = false;
-//         words = new ArrayList<>();
-//         children = new HashMap<>();
-//     }
-// }
-
-
 // ek idea yhh bhii ho sktaa hh shuru me hii bataa do kitne suggest krogee 
 // aur input ko sort krrr detee hhh 
 // aur har node ko hum atmax utne hii words rkhegee
@@ -76,7 +14,7 @@ class Solution {
 
         for(int i=1; i<=searchWord.length(); i++) {
             String prefix = searchWord.substring(0,i);
-            results.add(prefixTree.findThreeSuggestions(prefix));
+            results.add(prefixTree.findKSuggestions(prefix));
         }
         return results;
     }
@@ -103,7 +41,7 @@ class Trie {
         }
     }
 
-    List<String> findThreeSuggestions(String prefix) {
+    List<String> findKSuggestions(String prefix) {
         TrieNode curr = root;
         
         for(char ch : prefix.toCharArray()) {
