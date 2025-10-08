30c682a3291a1eb30172a9de73fa4cbdeda7594a
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-10-08 07:06:26 +0530

    Time: 26 ms (48.94%) | Memory: 42.8 MB (75.15%) - LeetSync

--- 133-clone-graph/clone-graph.java
+++ 133-clone-graph/clone-graph.java
@@ -1,32 +1,27 @@
 class Solution {
     public Node cloneGraph(Node node) {
-        if(node == null) return null;
+        if(node == null) return node;
 
-        HashMap<Integer,Node> copy = new HashMap<>();
-        Queue<Node> queue = new LinkedList<>();
-    
-    
         Node mainNode = new Node(node.val);
+        Queue<Node> queue = new LinkedList<>();
+        HashMap<Integer,Node>  map = new HashMap<>();
+
         queue.add(node);
-        copy.put(node.val,mainNode);
-
-        while(!queue.isEmpty()){
+        map.put(mainNode.val,mainNode);
+        
+        while(!queue.isEmpty()) {
             Node curr = queue.remove();
-            Node copied = copy.get(curr.val);
+            Node copied = map.get(curr.val);
 
-            for(Node nb : curr.neighbors){
-                int val = nb.val;
-
-                if(!copy.containsKey(val)){
-                    copy.put(val,new Node(val));
-                    queue.add(nb);
+            for (Node nbr : curr.neighbors) {
+                if(!map.containsKey(nbr.val)) {
+                    map.put(nbr.val, new Node(nbr.val));
+                    queue.add(nbr);
                 }
-                copied.neighbors.add(copy.get(val));
-
+                copied.neighbors.add(map.get(nbr.val));
             }
         }
 
         return mainNode;
-    
     }    
 }
