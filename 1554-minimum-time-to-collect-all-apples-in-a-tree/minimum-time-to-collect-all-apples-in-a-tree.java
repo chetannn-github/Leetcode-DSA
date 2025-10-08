2d4f9f3a7a6c4bc65178e793a0b4897512ac4fa1
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-10-08 06:41:41 +0530

    Time: 23 ms (54.70%) | Memory: 76.1 MB (84.12%) - LeetSync

--- 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.java
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.java
@@ -1,46 +1,5 @@
-// class Solution {
-//     HashMap<Integer,List<Integer>> adj;
-//     HashSet<Integer> visited;
-//     public int minTime(int n, int[][] edges, List<Boolean> hasApple) {
-//         adj = new HashMap<>();
-//         visited = new HashSet<>();
-
-//         for(int[] edge : edges){
-//             List<Integer> nbr = adj.getOrDefault(edge[0],new ArrayList<>());
-//             nbr.add(edge[1]);
-//             adj.put(edge[0],nbr);
-
-//             nbr = adj.getOrDefault(edge[1],new ArrayList<>());
-//             nbr.add(edge[0]);
-//             adj.put(edge[1],nbr);
-//         }
-//         int ans = dfs(0,hasApple);
-
-//         return ans > 0 ? ans-2 : 0 ;
-            
-//     }
-
-
-//     public int dfs(int curr,List<Boolean> hasApple){
-//         visited.add(curr);
-//         int time = 0;
-
-//         for(Integer nbr : adj.getOrDefault(curr,new ArrayList<>())){
-//             if(!visited.contains(nbr)){
-//                 time += dfs(nbr,hasApple);
-//             }
-//         }
-
-//         if(hasApple.get(curr)){
-//             time +=2;
-//         }else if(time!=0){
-//             time +=2;
-//         }
-
-//         return time;
-//     }
-// }
-
+// child apne parent ko signal degaa if neg nhii hain bro mere lineage me 
+// aur time > 0 then hain broo mere paas phrr tum +2 krogee kukii you need to go there
 
 class Solution {
     HashSet<Integer> visited;
@@ -67,8 +26,6 @@ class Solution {
         if(time == 0 && !hasApple.get(curr)) {
             return -1;
         }
-    
-
         return time;
     }
 
