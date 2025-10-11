98051426fedbb29990c205b3923d8bcdeddb522f
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-10-11 15:23:59 +0530

    Time: 146 ms (37.76%) | Memory: 44.9 MB (70.43%) - LeetSync

--- 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
+++ 2206-detonate-the-maximum-bombs/detonate-the-maximum-bombs.java
@@ -1,47 +1,49 @@
+// dsu nhii lagegaa broo kukii it is not like a b ko kregaa toh b a ko kregaa
 class Solution {
-    List<List<Integer>> adj;
+    List<Integer>[] adj;
     public int maximumDetonation(int[][] bombs) {
         int n = bombs.length;
-        adj = new ArrayList<>();
-        for(int i=0; i<n; i++) adj.add(new ArrayList<>());
+        constructGraph(bombs, n);
 
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                long dSquare =(long) Math.pow( bombs[i][0] - bombs[j][0] , 2) +(long) Math.pow(bombs[i][1] - bombs[j][1],2); 
-                
-                int r1 = bombs[i][2];
-                int r2 = bombs[j][2];
-                if((long) r1*r1 >= dSquare){
-                    adj.get(i).add(j);
-                }
-                if((long) r2*r2 >= dSquare){
-                    adj.get(j).add(i);
-                }
-                
-            }
-        }
-        // System.out.println(adj.toString());
-        int max = 1;
+        int maxVisited = 1;
         HashSet<Integer> visited = new HashSet<>();
         for(int i=0; i<n;i++){
-            
             dfs(i,visited);
-            max = Math.max(visited.size(), max);
+            maxVisited = Math.max(visited.size(), maxVisited);
             visited.clear();
         }
-        return max;
+        return maxVisited;
     }
 
 
     public void dfs(int curr, HashSet<Integer> visited){
         visited.add(curr);
 
-        for(int nbr : adj.get(curr)){
+        for(int nbr : adj[curr]){
             if(!visited.contains(nbr)){
                 dfs(nbr,visited);
             }
         }
-
-
     }
+
+    public void constructGraph(int[][] bombs, int n) {
+        adj = new List[n];
+        for(int i=0; i<n; i++) adj[i] = new ArrayList<>();
+
+        for(int i=0; i<n; i++){
+            for(int j=i+1; j<n; j++){
+                long dSquare = (long) Math.pow(bombs[i][0] - bombs[j][0],2) 
+                + (long) Math.pow(bombs[i][1] - bombs[j][1],2); 
+                
+                int r1 = bombs[i][2];
+                int r2 = bombs[j][2];
+                if((long) r1*r1 >= dSquare){
+                    adj[i].add(j);
+                }
+                if((long) r2*r2 >= dSquare){
+                    adj[j].add(i);
+                }
+            }
+        }
+    } 
 }
