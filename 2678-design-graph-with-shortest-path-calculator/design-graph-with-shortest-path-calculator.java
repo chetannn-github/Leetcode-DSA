4cef9906daf42f9544b049e35ae9b7dcad5caa95
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-10-25 09:57:04 +0530

    Time: 1576 ms (5.06%) | Memory: 54.7 MB (98.88%) - LeetSync

---
+++ 2678-design-graph-with-shortest-path-calculator/design-graph-with-shortest-path-calculator.java
@@ -0,0 +1,44 @@
+class Graph {
+    int[][] dist;
+    int n;
+    public Graph(int n, int[][] edges) {
+        this.n = n;
+        dist = new int[n][n];
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                dist[i][j] = i == j ? 0 : Integer.MAX_VALUE;
+            }
+        }
+
+        for(int[] edge : edges) {
+            int u = edge[0], v = edge[1], wt = edge[2];
+            dist[u][v] = wt;
+        }
+        floydWarshall();
+    }
+    
+    public void addEdge(int[] edge) {
+        int u = edge[0], v = edge[1], wt = edge[2];
+        dist[u][v] = wt;
+        floydWarshall();
+    }
+    
+    public int shortestPath(int u, int v) {
+        return dist[u][v] == Integer.MAX_VALUE ? -1 : dist[u][v];
+    }
+
+    public void floydWarshall() {
+        for(int via = 0; via < n; via++) {
+            for(int i=0; i<n;i++) {
+                for(int j=0; j<n; j++) {
+                    boolean isConnected = (
+                        dist[i][via] != Integer.MAX_VALUE && dist[via][j] != Integer.MAX_VALUE
+                    );
+                    boolean isOptimal = isConnected && dist[i][j] > dist[i][via] + dist[via][j];
+                    if(isOptimal) dist[i][j] = dist[i][via] + dist[via][j];
+                }
+            } 
+        }
+    }
+}
+
