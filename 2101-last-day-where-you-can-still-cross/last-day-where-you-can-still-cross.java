21b9dd39449207f166d4c73ba7fe03c41d39b3af
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-10-27 15:00:04 +0530

    Time: 42 ms (74.73%) | Memory: 59.1 MB (5.34%) - LeetSync

--- 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
+++ 2101-last-day-where-you-can-still-cross/last-day-where-you-can-still-cross.java
@@ -1,125 +1,124 @@
+// class Solution {
+//     int[][] dirns = {{0,1}, {1,0}, {-1,0},{0,-1}};
+//     int[][] grid;
+//     int row,col;
+//     public int latestDayToCross(int row, int col, int[][] cells) {
+//         int n = cells.length;
+//         this.row = row;
+//         this.col = col;
+//         grid = new int[row][col];
+        
+//         for(int i=0; i<n; i++) {
+//             int x = cells[i][0] - 1, y = cells[i][1]-1;
+//             grid[x][y] = i + 1;
+//         }
+
+//         int start = 0, end = n - 1;
+//         int result = 0;
+
+//         while(start <= end) {
+//             int mid = start + ((end - start) >> 1);
+            
+//             if(canCross(mid)) {
+//                 result = mid;
+//                 start = mid + 1;
+//             }else end = mid - 1;
+            
+//         }
+//         return result;
+//     }
+
+//     public boolean canCross(int currDay) {
+//         boolean result = false;
+//         for(int j=0; j<col; j++) {
+//             if(grid[0][j] > currDay) {
+//                 if(dfs(0,j,currDay,new boolean[row][col])) return true;
+//             }
+//         }
+//         return false;
+//     }
+
+//     private boolean dfs(int x,int y, int day, boolean[][] visited) {
+//         if(x == row-1 ) return true;
+//         visited[x][y] = true;
+
+//         boolean result = false;
+//         for(int[] dirn : dirns) {
+//             int nx = x + dirn[0], ny = y + dirn[1];
+//             if(isOutOfBound(nx,ny,row,col)) continue;
+
+//             if(grid[nx][ny] > day && !visited[nx][ny]) {
+//                 if(dfs(nx,ny,day,visited)) return true;
+//             }
+//         }
+
+//         return result;
+//     }
+
+
+//     private boolean isOutOfBound(int x, int y,int rows, int cols) {
+//         return x<0 || y<0 || x>=rows || y>=cols;
+//     }
+// }
+
 class Solution {
     int[][] dirns = {{0,1}, {1,0}, {-1,0},{0,-1}};
-    int[][] grid;
-    int row,col;
+    int row, col;
+
     public int latestDayToCross(int row, int col, int[][] cells) {
-        int n = cells.length;
         this.row = row;
         this.col = col;
-        grid = new int[row][col];
-        
-        for(int i=0; i<n; i++) {
-            int x = cells[i][0] - 1, y = cells[i][1]-1;
-            grid[x][y] = i + 1;
-        }
+        int n = cells.length;
 
         int start = 0, end = n - 1;
         int result = 0;
 
-        while(start <= end) {
+        while (start <= end) {
             int mid = start + ((end - start) >> 1);
-            
-            if(canCross(mid)) {
+
+            if (canCross(mid, cells)) {
                 result = mid;
                 start = mid + 1;
-            }else end = mid - 1;
-            
+            } else {
+                end = mid - 1;
+            }
         }
-        return result;
+        return result + 1;
     }
 
-    public boolean canCross(int currDay) {
-        boolean result = false;
+    private boolean canCross(int currDay, int[][] cells) {
+        int[][] grid = new int[row][col];
+
+        for(int i=0; i<=currDay; i++) {
+            int x = cells[i][0] - 1,y = cells[i][1] - 1;
+            grid[x][y] = 1;
+        }
+
         for(int j=0; j<col; j++) {
-            if(grid[0][j] > currDay) {
-                if(dfs(0,j,currDay,new boolean[row][col])) return true;
+            if(grid[0][j] == 0 && dfs(0, j, grid)) {
+                return true;
             }
         }
         return false;
     }
 
-    private boolean dfs(int x,int y, int day, boolean[][] visited) {
-        if(x == row-1 ) return true;
-        visited[x][y] = true;
+    private boolean dfs(int x, int y, int[][] grid) {
+        if(x == row - 1) return true;
 
-        boolean result = false;
-        for(int[] dirn : dirns) {
-            int nx = x + dirn[0], ny = y + dirn[1];
-            if(isOutOfBound(nx,ny,row,col)) continue;
+        grid[x][y] = 1;
 
-            if(grid[nx][ny] > day && !visited[nx][ny]) {
-                if(dfs(nx,ny,day,visited)) return true;
+        for (int[] dirn : dirns) {
+            int nx = x + dirn[0];
+            int ny = y + dirn[1];
+
+            if(!isOutOfBound(nx, ny, row, col) && grid[nx][ny] == 0) {
+                if (dfs(nx, ny, grid)) return true;
             }
         }
-
-        return result;
+        return false;
     }
 
-
-    private boolean isOutOfBound(int x, int y,int rows, int cols) {
-        return x<0 || y<0 || x>=rows || y>=cols;
+    private boolean isOutOfBound(int x, int y, int rows, int cols) {
+        return x < 0 || y < 0 || x >= rows || y >= cols;
     }
 }
-
-
-// class Solution {
-//     int ROW, COL;
-//     int[][] dirns = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-    
-//     private boolean dfs(int[][] grid, int x, int y) {
-//         if (x < 0 || x >= ROW || y < 0 || y >= COL || grid[x][y] == 1) {
-//             return false;
-//         }
-
-//         if (x == ROW - 1)
-//             return true;
-
-//         grid[x][y] = 1;
-//         for (int[] dirn : dirns) {
-//             int nx = x + dirn[0];
-//             int ny = y + dirn[1];
-
-//             if(dfs(grid, new_i, new_j)) return true;
-//         }
-
-//         return false;
-//     }
-
-//     private boolean canCross(int[][] cells, int day) {
-//         int[][] grid = new int[ROW][COL];
-
-//         for (int i = 0; i <= day; ++i) {
-//             int r = cells[i][0] - 1;
-//             int c = cells[i][1] - 1;
-//             grid[r][c] = 1;
-//         }
-
-//         for (int j = 0; j < COL; j++) {
-//             if (grid[0][j] == 0 && dfs(grid, 0, j))
-//                 return true;
-//         }
-//         return false;
-//     }
-
-//     public int latestDayToCross(int row, int col, int[][] cells) {
-//         this.ROW = row;
-//         this.COL = col;
-
-//         int n = cells.length;
-//         int l = 0, r = n - 1;
-//         int lastDay = 0;
-
-//         while (l <= r) {
-//             int mid = l + (r - l) / 2;
-
-//             if (canCross(cells, mid)) {
-//                 lastDay = mid + 1;
-//                 l = mid + 1;
-//             } else {
-//                 r = mid - 1;
-//             }
-//         }
-
-//         return lastDay;
-//     }
-// }
