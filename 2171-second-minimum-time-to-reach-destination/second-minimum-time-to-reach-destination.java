c5b4066604f3b3a63a4e443c7cc39a1906cbd358
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-10-28 07:00:26 +0530

    Time: 108 ms (38.83%) | Memory: 55.9 MB (84.57%) - LeetSync

---
+++ 2171-second-minimum-time-to-reach-destination/second-minimum-time-to-reach-destination.java
@@ -0,0 +1,60 @@
+class Solution {
+    List<Integer>[] graph;
+    public int secondMinimum(int n, int[][] edges, int time, int change) {
+        graph = new List[n];
+
+        for(int i=0; i<n; i++) graph[i] = new ArrayList<>();
+
+        for(int[] edge : edges) {
+            int u = edge[0]-1, v = edge[1]-1;
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        int[] secondMin = dijkstra(0,time,change,n);
+        return secondMin[n-1];
+    }
+
+    private int[] dijkstra(int src, int time, int change, int n) {
+        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->(a.wt - b.wt));
+        int[] dist1 = new int[n];
+        int[] dist2 = new int[n];
+        pq.add(new Pair(src,0));
+        Arrays.fill(dist1, Integer.MAX_VALUE);
+        Arrays.fill(dist2, Integer.MAX_VALUE);
+        dist1[src] = 0;
+
+
+        while(!pq.isEmpty()) {
+            Pair curr = pq.remove();
+            int u = curr.node, wt = curr.wt;
+            
+            for(int nbr : graph[u]) {
+                int nbrWt = wt + time;
+                boolean oddDiv = ((wt / change) & 1) == 1;
+                if(oddDiv) nbrWt = time + change * ((wt/change) + 1);
+                
+                if(dist1[nbr] > nbrWt) {
+                    dist1[nbr] = nbrWt;
+                    pq.add(new Pair(nbr,dist1[nbr]));
+                }
+
+                else if(nbrWt != dist1[nbr] && dist2[nbr] > nbrWt) {
+                    dist2[nbr] = nbrWt;
+                    pq.add(new Pair(nbr,dist2[nbr]));
+                }
+            }
+        }
+
+        return dist2;
+    }
+}
+
+class Pair {
+    int node, wt;
+    Pair(int node, int wt) {
+        this.node = node;
+        this.wt = wt;
+    }
+}
+
