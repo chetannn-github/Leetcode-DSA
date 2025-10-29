2478a233135556ad2ed5b366c210f3508694c70a
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-10-29 13:17:28 +0530

    Time: 150 ms (8.61%) | Memory: 122 MB (28.75%) - LeetSync

--- 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.java
+++ 1986-largest-color-value-in-a-directed-graph/largest-color-value-in-a-directed-graph.java
@@ -1,76 +1,91 @@
 class Solution {
     List<Integer>[] graph;
     int result = 1;
-    HashMap<Integer, int[]> dp;
-
+    HashMap<Integer,int[]> dp;
     public int largestPathValue(String colors, int[][] edges) {
         int n = colors.length();
-        constructGraph(edges, n);
-        List<Integer> topoSort = kahnAlgo(edges, n);
-        if(topoSort.size() != n) return -1; // cycle detected
+        constructGraph(edges,n);
+        List<Integer> topoSort = kahnAlgo(edges,n);
+        if(topoSort.size() != n) return -1;
 
         dp = new HashMap<>();
+       
+        for(int node=0; node<n; node++) {
+            if(!dp.containsKey(node)) { 
+                dfs(node,colors);
+            }
+        }
 
-        for (int node = 0; node < n; node++) {
-            if (!dp.containsKey(node)) {
-                dfs(node, colors);
+        System.out.println(dp.toString());
+        for(int i=0; i<n; i++) {
+            for(int maxColor : dp.get(i)) {
+                result = Math.max(maxColor, result);
             }
         }
 
         return result;
+       
     }
 
-    private void constructGraph(int[][] edges, int n) {
-        graph = new ArrayList[n];
-        for (int i = 0; i < n; i++) graph[i] = new ArrayList<>();
-        for (int[] edge : edges) {
-            int u = edge[0], v = edge[1];
-            graph[u].add(v);
-        }
-    }
-
-    private List<Integer> kahnAlgo(int[][] edges, int n) {
+    public List<Integer> kahnAlgo(int[][] edges, int n) {
         List<Integer> topoSort = new ArrayList<>();
         int[] indegree = new int[n];
 
-        for (int[] edge : edges) {
+        for(int[] edge : edges) {
             int u = edge[0], v = edge[1];
             indegree[v]++;
         }
 
         Queue<Integer> queue = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            if (indegree[i] == 0) queue.add(i);
+        
+        for(int i=0; i<n; i++) {
+            if(indegree[i] == 0) queue.add(i);
         }
 
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()) {
             int curr = queue.remove();
             topoSort.add(curr);
-            for (int nbr : graph[curr]) {
-                if (--indegree[nbr] == 0) queue.add(nbr);
+
+            for(int nbr : graph[curr]) {
+                if(--indegree[nbr] == 0) queue.add(nbr);
             }
         }
 
         return topoSort;
     }
 
-    private int[] dfs(int curr, String colors) {
-        if (dp.containsKey(curr)) return dp.get(curr);
+    private void constructGraph(int[][] edges, int n) {
+        graph = new ArrayList[n];
+        for(int i=0; i<n; i++) graph[i] = new ArrayList<>();
 
+        for(int[] edge : edges) {
+            int u = edge[0], v = edge[1];
+            graph[u].add(v);
+        }
+    }
+
+    private int[] dfs(int curr, String colors) {
+        if(dp.containsKey(curr)) { 
+            return dp.get(curr);
+        }
+
+        
         int currColor = colors.charAt(curr) - 'a';
         int[] maxColors = new int[26];
         maxColors[currColor]++;
 
-        for (int nbr : graph[curr]) {
-            int[] nbrColors = dfs(nbr, colors);
-            for (int i = 0; i < 26; i++) {
-                maxColors[i] = Math.max(maxColors[i], nbrColors[i] + (i == currColor ? 1 : 0));
+        for(int nbr : graph[curr]) {
+            int[] myColors = dfs(nbr,colors);
+
+            for(int i=0; i<26; i++) {
+                maxColors[i] = Math.max(maxColors[i], myColors[i] + (i == currColor ? 1 : 0));
             }
         }
 
-        dp.put(curr, maxColors);
-
+        dp.put(curr,maxColors);
         for (int val : maxColors) result = Math.max(result, val);
+
         return maxColors;
-    }
+        
+    } 
 }
