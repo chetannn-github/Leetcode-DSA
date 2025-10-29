ecd76e7d31dd6610217c8013302bd9d31ed4654d
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-10-29 08:12:37 +0530

    Time: 44 ms (35.60%) | Memory: 69.8 MB (6.28%) - LeetSync

--- 1309-sort-items-by-groups-respecting-dependencies/sort-items-by-groups-respecting-dependencies.java
+++ 1309-sort-items-by-groups-respecting-dependencies/sort-items-by-groups-respecting-dependencies.java
@@ -1,52 +1,30 @@
 class Solution {
-    public int[] sortItems(int n, int m, int[] group, List<List<Integer>> beforeItems) {
+        Map<Integer, List<Integer>> itemGraph,groupGraph;
+        int[] itemIndegree,groupIndegree;
+        public int[] sortItems(int n, int m, int[] group, List<List<Integer>> beforeItems) {
         for(int i=0; i<n; i++) {
             if(group[i] == -1) group[i] = m++;
         }
-        Map<Integer, List<Integer>> itemGraph = new HashMap<>();
-        int[] itemIndegree = new int[n];
-        Map<Integer, List<Integer>> groupGraph = new HashMap<>();
-        int[] groupIndegree = new int[m];
-        for(int i = 0; i < n; ++i) {
-            itemGraph.put(i, new ArrayList<>());
-        }
         
-        for(int i = 0; i < m; ++i) {
-            groupGraph.put(i, new ArrayList<>());
-        }
-        
-       
-        for(int i=0; i<n; i++) {
-            for(int prev : beforeItems.get(i)) {
-                itemGraph.get(prev).add(i);
-                itemIndegree[i]++;
-                
-                if(group[i] != group[prev]) {
-                    int prevItemGroup = group[prev];
-                    int currItemGroup = group[i];
-                    groupGraph.get(prevItemGroup).add(currItemGroup);
-                    groupIndegree[currItemGroup]++;
-                }
-                
-            }
-        }
-        
+        buildItemAndGroupGraph(n,m,group,beforeItems);
         List<Integer> itemOrder  = topoSort(itemGraph, itemIndegree);
         List<Integer> groupOrder = topoSort(groupGraph, groupIndegree);
         
 
-        Map<Integer, List<Integer>> groupToItemsInOrder = new HashMap<>();
+        Map<Integer, List<Integer>> groupToItems = new HashMap<>();
         for(Integer item : itemOrder) {
             int itemGroup = group[item];
-            groupToItemsInOrder.computeIfAbsent(itemGroup, k -> new ArrayList<>()).add(item);
+            List<Integer> groupElements = groupToItems.getOrDefault(itemGroup, new ArrayList<>()); 
+            groupElements.add(item);
+            groupToItems.put(itemGroup, groupElements);
         }
         
-        List<Integer> answerList = new ArrayList<>();
+        List<Integer> resultList = new ArrayList<>();
         for(int groupIndex : groupOrder) {
-            answerList.addAll(groupToItemsInOrder.getOrDefault(groupIndex, new ArrayList<>()));
+            resultList.addAll(groupToItems.getOrDefault(groupIndex, new ArrayList<>()));
         }
         
-        return answerList.stream().mapToInt(Integer::intValue).toArray();
+        return listToArray(resultList);
     }
     
     private List<Integer> topoSort(Map<Integer, List<Integer>> adj, int[] indegree) {
@@ -72,5 +50,36 @@ class Solution {
         }
 
         return result.size() == adj.size() ? result : new ArrayList<>();
+    }
+
+    private void buildItemAndGroupGraph(int n,int m, int[] group, List<List<Integer>> beforeItems) {
+        itemGraph = new HashMap<>();
+        groupGraph = new HashMap<>();
+        itemIndegree = new int[n];
+        groupIndegree = new int[m];
+
+        for(int i=0; i<n; ++i) itemGraph.put(i, new ArrayList<>());
+        for(int i=0; i<m; ++i) groupGraph.put(i, new ArrayList<>());
+        
+        for(int i=0; i<n; i++) {
+            for(int prev : beforeItems.get(i)) {
+                itemGraph.get(prev).add(i);
+                itemIndegree[i]++;
+                
+                if(group[i] != group[prev]) {
+                    int prevGroup = group[prev],currGroup = group[i];
+                    groupGraph.get(prevGroup).add(currGroup);
+                    groupIndegree[currGroup]++;
+                }
+                
+            }
+        }
+    }
+
+    private int[] listToArray(List<Integer> list) {
+        int n = list.size();
+        int[] arr = new int[n];
+        for(int i=0; i<n; i++) arr[i] = list.get(i);
+        return arr;
     }
 }
