075b0dfe2eff1ae42a8cbd27764b6c0922e0d7c0
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-11-03 07:38:04 +0530

    Time: 33 ms (53.18%) | Memory: 90.6 MB (5.24%) - LeetSync

---
+++ 2409-number-of-increasing-paths-in-a-grid/number-of-increasing-paths-in-a-grid.java
@@ -0,0 +1,40 @@
+class Solution { 
+    int m,n;
+    long NOT_VISITED_FLAG = -1;
+    int MOD = 1_000_000_007;
+    private static int[][] dirns = {{0,1},{1,0},{-1,0},{0,-1}};
+    long[][] dp;
+
+    public int countPaths(int[][] grid) {
+        this.m = grid.length;
+        this.n = grid[0].length;
+        dp = new long[m][n];
+
+        for(int i=0; i<m; i++) Arrays.fill(dp[i],NOT_VISITED_FLAG);
+
+        long result = 0;
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                result += (dfs(grid,i,j) % MOD);
+            }
+        }
+
+        return (int) (result % 1_000_000_007);
+    }
+
+    private long dfs(int[][] grid, int x, int y) {
+        if(dp[x][y] != NOT_VISITED_FLAG) return dp[x][y];
+
+        long result = 1;
+        for(int[] dirn : dirns) {
+            int nx = x + dirn[0], ny = y + dirn[1];
+            if(nx<0 || ny<0 || nx>=m || ny>=n) continue;
+
+            if(grid[nx][ny] < grid[x][y]) {
+                result += dfs(grid,nx,ny);
+            }
+        }
+
+        return dp[x][y] = (result % MOD);
+    }
+}
