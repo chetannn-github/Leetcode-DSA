6259a4289669c8313db0b5742da83280a8750237
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-11-18 17:47:30 +0530

    Time: 9 ms (92.19%) | Memory: 46.2 MB (33.59%) - LeetSync

--- 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
+++ 1569-max-dot-product-of-two-subsequences/max-dot-product-of-two-subsequences.java
@@ -2,31 +2,29 @@ class Solution {
     int[] nums1, nums2;
     int N,M;
     int INVALID = Integer.MIN_VALUE;
-    int[][][] dp;
+    int[][] dp;
     int NOT_CACHED = Integer.MIN_VALUE;
     public int maxDotProduct(int[] nums1, int[] nums2) {
         this.N = nums1.length;
         this.M = nums2.length;
         this.nums1 = nums1;
         this.nums2 = nums2;
-        this.dp = new int[N][M][2];
+        this.dp = new int[N][M];
 
-        for(int[][] grid : dp) {
-            for(int[] row : grid) Arrays.fill(row,NOT_CACHED);
-        }
-
-        return solve(0,0,0);
+        for(int[] row : dp) Arrays.fill(row,NOT_CACHED);
+        return solve(0,0);
     }
 
-    private int solve(int x, int y, int taken) {
-        if(x >= N || y >= M) return taken == 1 ? 0 : INVALID;
-        if(dp[x][y][taken] != NOT_CACHED) return dp[x][y][taken];
+    private int solve(int x, int y) {
+        if(x >= N || y >= M) return INVALID;
+        if(dp[x][y] != NOT_CACHED) return dp[x][y];
 
-        int take = nums1[x] * nums2[y] + solve(x+1, y+1,1);
-        int opt1 = solve(x+1,y,taken);
-        int opt2 = solve(x,y+1, taken);
-        int opt3 = solve(x+1,y+1, taken);
+        int val = nums1[x] * nums2[y];
 
-        return dp[x][y][taken] = Math.max(Math.max(take,opt1), Math.max(opt2, opt3));
+        int takeBoth = val + Math.max(0,solve(x+1, y+1));
+        int takeX = solve(x+1,y);
+        int takeY = solve(x,y+1);
+        
+        return dp[x][y] = Math.max(Math.max(takeBoth,takeX), Math.max(val,takeY));
     }
 }
