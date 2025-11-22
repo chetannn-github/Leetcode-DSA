50bb127641d72fa35497bbc8670ed055f8bdbdfb
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-11-22 16:36:28 +0530

    Time: 38 ms (10.92%) | Memory: 46.4 MB (6.00%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.java
@@ -1,28 +1,62 @@
+// class Solution {
+//     HashMap<String,Integer> dp;
+//     int[] cuts;
+//     public int minCost(int n, int[] cuts) {
+//         Arrays.sort(cuts);
+//         this.cuts = cuts;
+//         this.dp = new HashMap<>();
+//         return solve(0,n);
+//     }
+
+//     private int solve(int start, int end) {
+//         if(end - start == 1) return 0;
+//         String key = start + " " + end;
+//         if(dp.containsKey(key)) return dp.get(key);
+
+//         int result = Integer.MAX_VALUE;
+//         for(int i=0; i<cuts.length; i++) {
+//             int cutPos = cuts[i];
+
+//             if(cutPos >= end || cutPos <= start) continue;
+//             int currResult = end - start + solve(start, cutPos) + solve(cutPos, end);
+//             result = Math.min(currResult,result);
+//         }
+//         result = result == Integer.MAX_VALUE ? 0 : result;
+//         dp.put(key,result);
+//         return result;
+//     }
+// }
+
 class Solution {
-    HashMap<String,Integer> dp;
-    int[] cuts;
+    List<Integer> modCuts;
+    Integer[][] dp;
+    int k;
     public int minCost(int n, int[] cuts) {
         Arrays.sort(cuts);
-        this.cuts = cuts;
-        this.dp = new HashMap<>();
-        return solve(0,n);
+        this.k = cuts.length + 2;
+        this.dp = new Integer[k][k];
+        this.modCuts = new ArrayList<>();
+
+        modCuts.add(0);
+        for(int cut : cuts) modCuts.add(cut);
+        modCuts.add(n);
+        return solve(0,k-1);
     }
 
-    private int solve(int start, int end) {
-        if(end - start == 1) return 0;
-        String key = start + " " + end;
-        if(dp.containsKey(key)) return dp.get(key);
+    private int solve(int startIdx, int endIdx) {
+        if(endIdx - startIdx == 1) return 0;
+        if(dp[startIdx][endIdx] != null) return dp[startIdx][endIdx];
 
         int result = Integer.MAX_VALUE;
-        for(int i=0; i<cuts.length; i++) {
-            int currPos = cuts[i];
-
-            if(currPos >= end || currPos <= start) continue;
-            int currResult = end - start + solve(start, currPos) + solve(currPos, end);
+        for(int i=startIdx+1; i<endIdx; i++) {
+            int start = modCuts.get(startIdx), end = modCuts.get(endIdx);
+            int currResult = end - start + solve(startIdx, i) + solve(i, endIdx);
             result = Math.min(currResult,result);
         }
         result = result == Integer.MAX_VALUE ? 0 : result;
-        dp.put(key,result);
-        return result;
+        return dp[startIdx][endIdx] = result;
     }
 }
+
+            
+            
