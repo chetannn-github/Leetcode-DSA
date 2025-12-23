b289289e192c5b5ab854a640ed89ca1988b39ce8
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-12-23 07:49:23 +0530

    Time: 126 ms (74.45%) | Memory: 161.2 MB (15.86%) - LeetSync

---
+++ 2389-design-a-text-editor/design-a-text-editor.java
@@ -0,0 +1,85 @@
+class TextEditor {
+    Node head,curr;
+    
+    public TextEditor() {
+        head = new Node('A');
+        curr = head;
+    }
+    
+    public void addText(String text) {
+        for(char ch : text.toCharArray()) {
+            addNewNode(ch);
+        }
+        
+    }
+    
+    public int deleteText(int k) {
+        int deletionCount = 0;
+        while(k-->0 && curr != head) {
+            removeNode();
+            deletionCount++;
+        }   
+
+        return deletionCount;
+    }
+    
+    public String cursorLeft(int k) {
+        while(k-->0 && curr != head) curr = curr.prev;
+        return getPreviousString();
+
+    }
+    
+    public String cursorRight(int k) {
+        while(k--> 0 && curr.next != null) curr = curr.next;
+        return getPreviousString();
+    }
+
+    private void addNewNode(char ch) {
+        Node node = new Node(ch);
+        node.next = curr.next;
+        if(curr.next != null) curr.next.prev = node;
+        node.prev = curr;
+        curr.next = node;
+        
+        curr = node;
+    }
+
+    private void removeNode() {
+        Node toDelete = curr;
+
+        curr = curr.prev;      
+
+        curr.next = toDelete.next;
+        if (toDelete.next != null) {
+            toDelete.next.prev = curr;
+        }
+        
+        toDelete.prev = null;
+        toDelete.next = null;
+    }
+
+    private String getPreviousString() {
+        Node node = curr;
+        int length = 0;
+        StringBuilder result = new StringBuilder();
+
+        while(node != head && length < 10) {
+            result.append(node.value);
+            node = node.prev;
+            length++;
+        }
+
+        return result.reverse().toString();
+    }
+}
+
+
+class Node{
+    Node prev, next;
+    char value;
+    Node(char value) {
+        this.prev = null;
+        this.next = null;
+        this.value = value;
+    }
+}
