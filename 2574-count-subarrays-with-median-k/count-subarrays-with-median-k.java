770b9f9f40de8822eb15e78f76ba292d1ec62fa2
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-12-23 17:16:48 +0530

    Time: 20 ms (21.13%) | Memory: 72.1 MB (35.21%) - LeetSync

--- 2574-count-subarrays-with-median-k/count-subarrays-with-median-k.java
+++ 2574-count-subarrays-with-median-k/count-subarrays-with-median-k.java
@@ -2,22 +2,14 @@ class Solution {
     public int countSubarrays(int[] nums, int k) {
         // big - small == 0 Or big - small == 1
         int n = nums.length;
-        int idx = 0;
-
-        for(int i = 0; i < n; i++) {
-            if(nums[i] == k) {
-                idx = i;
-                break;
-            }
-        }
+        int idx = getIndex(nums,k);
 
         Map<Integer, Integer> count = new HashMap<>();
         count.put(0, 1);
 
         int diff = 0;
         for (int i = idx - 1; i>=0; i--) {
-            if (nums[i] > k) diff++;
-            else diff--;
+            diff += nums[i] > k ? 1 : -1;
             count.put(diff, count.getOrDefault(diff, 0) + 1);
         }
 
@@ -33,5 +25,17 @@ class Solution {
         }
 
         return result;
+    }
+
+    private int getIndex(int[] arr, int target) {
+        int n = arr.length;
+
+        for(int i = 0; i < n; i++) {
+            if(arr[i] == target) {
+                return i;
+            }
+        }
+
+        return -1;
     }
 }
