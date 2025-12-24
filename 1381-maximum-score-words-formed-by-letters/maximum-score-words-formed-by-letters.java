f3e55f1227f6d6eb04386a2bfd69c8c7efbac92f
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-12-24 22:08:10 +0530

    Time: 1 ms (91.86%) | Memory: 43.5 MB (57.65%) - LeetSync

--- 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.java
+++ 1381-maximum-score-words-formed-by-letters/maximum-score-words-formed-by-letters.java
@@ -3,29 +3,29 @@ class Solution {
     int n;
     int[] score;
     String[] words;
-    HashMap<String,Integer>[] dp;
+    // HashMap<String,Integer>[] dp;
 
     public int maxScoreWords(String[] words, char[] letters, int[] score) {
         this.n = words.length;
         this.score = score;
         this.words = words;
-        this.dp = new HashMap[n];
+        // this.dp = new HashMap[n];
         
         for(char ch : letters) {
             freq[ch-'a']++;
         }
 
-        for(int i=0; i<n; i++) dp[i] = new HashMap<>();
+        // for(int i=0; i<n; i++) dp[i] = new HashMap<>();
 
         return solve(0);
     }
 
     private int solve(int currIdx) {
         if(currIdx >= n) return 0;
-        String key = Arrays.toString(freq);
-        if(dp[currIdx].containsKey(key)) {
-            return dp[currIdx].get(key);
-        }
+        // String key = Arrays.toString(freq);
+        // if(dp[currIdx].containsKey(key)) {
+        //     return dp[currIdx].get(key);
+        // }
 
         int take = 0;
         int takeScore = 0;
@@ -44,7 +44,7 @@ class Solution {
         }  
 
         int skip = solve(currIdx+1);
-        dp[currIdx].put(key,Math.max(take,skip));
+        // dp[currIdx].put(key,Math.max(take,skip));
         return Math.max(take,skip);
     }
 
