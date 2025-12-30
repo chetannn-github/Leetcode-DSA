a4348330e3674542049f187978252edce26db269
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-12-30 15:41:57 +0530

    Time: 17 ms (81.98%) | Memory: 117.9 MB (56.18%) - LeetSync

--- 1622-max-value-of-equation/max-value-of-equation.java
+++ 1622-max-value-of-equation/max-value-of-equation.java
@@ -1,19 +1,55 @@
+// class Solution {
+//     public int findMaxValueOfEquation(int[][] points, int k) {
+//         PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->(b.diff-a.diff));
+//         int result = Integer.MIN_VALUE;
+
+//         for(int[] point : points) {
+//             int x = point[0], y = point[1];
+
+//             while(!pq.isEmpty() && (x - pq.peek().x) > k) pq.remove();
+
+//             if(!pq.isEmpty()) {
+//                 result = Math.max(result,pq.peek().diff + x + y);
+//             }
+
+//             pq.add(new Pair(x,y-x));
+//         }
+
+//         return result;
+//     }
+
+// }
+
+// class Pair{
+//     int x;
+//     int diff;
+//     Pair(int x, int diff) {
+//         this.x = x;
+//         this.diff = diff;
+//     }
+// }
+
+
 class Solution {
     public int findMaxValueOfEquation(int[][] points, int k) {
-        PriorityQueue<Pair> pq = new PriorityQueue<>((a,b)->(b.diff-a.diff));
+        Deque<Pair> dq = new ArrayDeque<>();
         int result = Integer.MIN_VALUE;
 
         for(int[] point : points) {
             int x = point[0], y = point[1];
+            int currDiff = y-x;
 
-            while(!pq.isEmpty() && (x - pq.peek().x) > k) pq.remove();
-
-            if(!pq.isEmpty()) {
-                result = Math.max(result,pq.peek().diff + x + y);
+            while(!dq.isEmpty() && x - dq.peekFirst().x > k) {
+                dq.removeFirst();
             }
 
-            pq.add(new Pair(x,y-x));
+            if(!dq.isEmpty()) result = Math.max(result, dq.peek().diff + x+y);
+            
+            while(!dq.isEmpty() && dq.peekLast().diff <= currDiff) {
+                dq.removeLast();
+            }
 
+            dq.addLast(new Pair(x,currDiff));
         }
 
         return result;
