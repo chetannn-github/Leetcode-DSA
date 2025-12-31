6f8ff444c0b6a3b605638e0ba40af79f535161f1
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2025-12-31 17:08:04 +0530

    Time: 112 ms (15.09%) | Memory: 47.4 MB (14.15%) - LeetSync

--- 1668-find-longest-awesome-substring/find-longest-awesome-substring.java
+++ 1668-find-longest-awesome-substring/find-longest-awesome-substring.java
@@ -3,21 +3,19 @@ public class Solution {
         int result = 0;
         int mask = 0;
         HashMap<Integer, Integer> seen = new HashMap<>();
-        seen.put(0, -1);  // base case
+        seen.put(0, -1);
 
         for (int i = 0; i < s.length(); i++) {
             int digit = s.charAt(i) - '0';
             mask ^= (1 << digit);
 
-            // Case 1: all even frequency
-            if (seen.containsKey(mask)) {
+            if(seen.containsKey(mask)) {
                 result = Math.max(result, i - seen.get(mask));
-            } else {
+            }else {
                 seen.put(mask, i);
             }
 
-            // Case 2: one odd frequency allowed
-            for (int j = 0; j < 10; j++) {
+            for(int j = 0; j < 10; j++) {
                 int maskWithOneBitFlipped = mask ^ (1 << j);
                 if (seen.containsKey(maskWithOneBitFlipped)) {
                     result = Math.max(result, i - seen.get(maskWithOneBitFlipped));
