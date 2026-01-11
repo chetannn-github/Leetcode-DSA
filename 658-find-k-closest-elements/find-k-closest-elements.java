be131a7621c5d74149cd8ed4d342354b3a5e6555
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2026-01-11 16:56:01 +0530

    Time: 4 ms (98.22%) | Memory: 47.8 MB (51.07%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.java
+++ 658-find-k-closest-elements/find-k-closest-elements.java
@@ -1,26 +1,54 @@
+// class Solution {
+//     public List<Integer> findClosestElements(int[] arr, int k, int x) {
+//         PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
+//             int dis1 = Math.abs(x-a);
+//             int dis2 = Math.abs(x-b);
+//             if(dis1 == dis2){
+//                 return b - a;
+//             }else{
+//                 return dis2 - dis1;
+//             }
+//         });
+
+//         for(int point : arr){
+//             pq.add(point);
+//             if(pq.size() > k) pq.remove();
+//         }
+//         List<Integer> result = new ArrayList<>();
+//         while(!pq.isEmpty()){
+//             result.add(pq.remove());
+//         }
+//         Collections.sort(result);
+
+//         return result;
+
+//     }
+// }
+
+
 class Solution {
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)->{
-            int dis1 = Math.abs(x-a);
-            int dis2 = Math.abs(x-b);
-            if(dis1 == dis2){
-                return b - a;
-            }else{
-                return dis2 - dis1;
+        int n = arr.length;
+        int start = 0, end = n - k;
+        int windowStart = 0;
+
+        while(start <= end) {
+            int mid = start + ((end - start)>>1);
+
+            if(mid+k < n && x - arr[mid] > arr[mid + k] - x) {
+                start = mid + 1;
+            }else {
+                windowStart = mid;
+                end = mid-1;
             }
-        });
-
-        for(int point : arr){
-            pq.add(point);
-            if(pq.size() > k) pq.remove();
         }
+
         List<Integer> result = new ArrayList<>();
-        while(!pq.isEmpty()){
-            result.add(pq.remove());
+        for (int i = windowStart; i < start + k; i++) {
+            result.add(arr[i]);
         }
-        Collections.sort(result);
-
         return result;
-
     }
 }
+
+
