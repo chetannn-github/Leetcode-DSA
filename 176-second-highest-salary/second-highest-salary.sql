6ffbb01149dc45ed80e51c0b1987cf66245cb718
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2026-01-19 16:58:13 +0530

    Time: 217 ms (28.66%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,34 +1,26 @@
--- SELECT DISTINCT salary as SecondHighestSalary
--- FROM Employee 
--- ORDER BY salary DESC
--- OFFSET 1
--- LIMIT 1;
-
-SELECT MAX(salary) AS SecondHighestSalary
-FROM (
-    SELECT DISTINCT salary
-    FROM Employee
-    ORDER BY salary DESC
-    OFFSET 1
-    LIMIT 1
-);
-
--- SELECT MAX(salary) AS SecondHighestSalary FROM (
---     SELECT salary
---     FROM (
---         SELECT salary,
---             ROW_NUMBER() OVER (ORDER BY salary DESC) AS rn
---         FROM employee
---     ) 
---     WHERE rn = 2
+-- SELECT MAX(salary) AS SecondHighestSalary
+-- FROM (
+--     SELECT DISTINCT salary
+--     FROM Employee
+--     ORDER BY salary DESC
+--     OFFSET 1
+--     LIMIT 1
 -- );
 
+SELECT MAX(salary) AS SecondHighestSalary FROM (
+    SELECT salary
+    FROM (
+        SELECT salary,
+            DENSE_RANK() OVER (ORDER BY salary DESC) AS rn
+        FROM employee
+    ) 
+    WHERE rn = 2
+);
 
 
 
-        -- SELECT salary,
-        --     ROW_NUMBER() OVER (ORDER BY salary DESC) AS rn
-        -- FROM employee
+
+
     
    
 
