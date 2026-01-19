5478908dfb01e713f0bdd725f922007a37e525a5
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2026-01-19 09:16:05 +0530

    Time: 236 ms (81.60%) | Memory: 0B (100.00%) - LeetSync

--- 175-combine-two-tables/combine-two-tables.sql
+++ 175-combine-two-tables/combine-two-tables.sql
@@ -1,4 +1,5 @@
-SELECT firstName,lastName,city,state
-FROM Person
-LEFT JOIN Address
-USING (personId)
+
+SELECT p.firstName AS firstName, p.lastName AS lastName, a.city as city, a.state as state
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personId;
