0284a4eaa0987575c8632054991ff24f715d8a1a
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2026-01-19 21:10:58 +0530

    Time: 312 ms (56.43%) | Memory: 0B (100.00%) - LeetSync

--- 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
+++ 1625-group-sold-products-by-the-date/group-sold-products-by-the-date.sql
@@ -1,3 +1,10 @@
-SELECT sell_date, COUNT(sell_date) AS num_sold, STRING_AGG(product,',' ORDER BY product) AS products
-FROM (SELECT * FROM Activities GROUP BY sell_date, product) 
-GROUP BY sell_date
+-- SELECT sell_date, COUNT(sell_date) AS num_sold, STRING_AGG(product,',' ORDER BY product) AS products
+-- FROM (SELECT * FROM Activities GROUP BY sell_date, product) 
+-- GROUP BY sell_date 
+
+
+SELECT sell_date, 
+COUNT(DISTINCT product) AS num_sold,
+STRING_AGG(DISTINCT product,',' ORDER BY product) AS products
+FROM Activities 
+GROUP BY sell_date 
