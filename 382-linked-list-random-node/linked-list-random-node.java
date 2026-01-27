66af873b8074d5200d09c274c75a54a163d0ac56
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2026-01-27 19:58:17 +0530

    Time: 12 ms (59.05%) | Memory: 48.4 MB (41.33%) - LeetSync

--- 382-linked-list-random-node/linked-list-random-node.java
+++ 382-linked-list-random-node/linked-list-random-node.java
@@ -1,23 +1,50 @@
+// class Solution {
+//     ListNode head;
+//     int count;
+//     public Solution(ListNode head) {
+//         this.head = head;
+
+//         ListNode curr = head;
+//         this.count = 0;
+//         while(curr != null){
+//             this.count++;
+//             curr = curr.next;
+//         }
+//     }
+    
+//     public int getRandom() {
+//         int rand = (int) (Math.random() * count);
+//         ListNode curr = head;
+//         while(rand-- > 0) {
+//             curr = curr.next;
+//         }
+//         return curr.val;
+//     }
+// }
+
+
 class Solution {
     ListNode head;
-    int count;
     public Solution(ListNode head) {
         this.head = head;
-
-        ListNode curr = head;
-        this.count = 0;
-        while(curr != null){
-            this.count++;
-            curr = curr.next;
-        }
     }
     
     public int getRandom() {
-        int rand = (int) (Math.random() * count);
-        ListNode curr = head;
-        while(rand-- > 0) {
+        int result = head.val;
+
+        int count = 2;
+        ListNode curr = head.next;
+
+        while(curr != null) {
+            int randomIdx = (int) Math.floor(Math.random() * count);
+            count++;
+
+            if(randomIdx == 0) result = curr.val;
+
             curr = curr.next;
         }
-        return curr.val;
+
+        return result;
     }
 }
+
