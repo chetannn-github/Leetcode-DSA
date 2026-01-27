0324cf0949c6089fdc0b70c27fee79b8fabc1d7a
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2026-01-27 15:58:43 +0530

    Time: 234 ms (56.32%) | Memory: 0B (100.00%) - LeetSync

--- 184-department-highest-salary/department-highest-salary.sql
+++ 184-department-highest-salary/department-highest-salary.sql
@@ -1,8 +1,26 @@
-SELECT Employee, Department,Salary 
+-- SELECT Employee, Department,Salary 
+-- FROM (
+--     SELECT e.name as Employee, d.name AS Department, e.salary as Salary,
+--         MAX(salary) OVER (PARTITION BY e.departmentId ORDER BY salary DESC )
+--         FROM Employee e
+--         LEFT JOIN Department d
+--         ON e.departmentId = d.id)
+-- WHERE max = salary
+
+
+SELECT e.dname AS Department, e.name AS Employee,e.Salary 
 FROM (
-    SELECT e.name as Employee, d.name AS Department, e.salary as Salary,
-        MAX(salary) OVER (PARTITION BY e.departmentId ORDER BY salary DESC )
+    SELECT d.name as dname,e.name,e.Salary, e.departmentId
+    FROM Employee e
+    LEFT JOIN Department d
+    ON e.departmentId = d.id
+    ) AS e
+JOIN (
+    SELECT e.departmentId, MAX(e.salary) as maxSalary
         FROM Employee e
-        LEFT JOIN Department d
-        ON e.departmentId = d.id)
-WHERE max = salary
+        GROUP BY e.departmentId
+    ) AS t
+ON t.maxSalary = e.salary AND e.departmentId= t.departmentId
+
+
+
