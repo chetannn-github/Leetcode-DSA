214d90ff815d489f203b8770755c2ee08e3c7c9a
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2026-01-31 19:53:15 +0530

    Time: 255 ms (8.76%) | Memory: 0B (100.00%) - LeetSync

--- 4208-find-emotionally-consistent-users/find-emotionally-consistent-users.sql
+++ 4208-find-emotionally-consistent-users/find-emotionally-consistent-users.sql
@@ -1,7 +1,8 @@
 WITH mod_reactions AS (
     SELECT user_id,reaction,
     COUNT(user_id) OVER(PARTITION BY user_id) AS total_reactions, 
-    COUNT(reaction) OVER(PARTITION BY user_id, reaction) AS reaction_freq
+    COUNT(reaction) OVER(PARTITION BY user_id, reaction) AS reaction_freq,
+    COUNT(*) OVER (PARTITION BY user_id ) AS content_reacted
     FROM reactions
 )
 
@@ -13,11 +14,10 @@ FROM (
     SELECT 
         user_id,reaction,total_reactions,
         reaction_freq,
-        ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY reaction_freq DESC) as rn,
-        COUNT(*) OVER (PARTITION BY user_id ) AS content_reacted
+        ROW_NUMBER() OVER(PARTITION BY user_id ORDER BY reaction_freq DESC) as rn
     FROM mod_reactions
+    WHERE content_reacted >= 5
     
 )
 WHERE rn = 1 AND reaction_freq :: DECIMAL / total_reactions >= 0.6
-AND content_reacted >= 5
 ORDER BY reaction_ratio DESC, user_id ASC;
