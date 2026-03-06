6a7e7fdf5593325aa8f448f567e1153e01685e49
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2026-03-06 20:13:13 +0530

    Time: 0 ms (100.00%) | Memory: 43.3 MB (67.30%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.java
+++ 14-longest-common-prefix/longest-common-prefix.java
@@ -1,58 +1,13 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        Trie prefixTree = new Trie();
-
-        for(String str : strs){
-            if (str.length() == 0) return new String("");
-            prefixTree.insert(str);
+        if (strs.length == 0) return "";
+        String prefix = strs[0];
+        for (int i = 1; i < strs.length; i++) while (
+            strs[i].indexOf(prefix) != 0
+        ) {
+            prefix = prefix.substring(0, prefix.length() - 1);
+            if (prefix.isEmpty()) return "";
         }
-        int LCP = prefixTree.getLongestCommanPrefix();
-        String LCPString = LCP == 0 ? 
-            new String("") :
-            strs[0].substring(0,LCP)
-        ;
-        return LCPString;
-    }
-}
-
-class Trie {
-    TrieNode root;
-    
-    Trie() {
-        root = new TrieNode();
-    }
-    void insert(String val) {
-        TrieNode curr = root;
-        for(char ch : val.toCharArray()){
-            if(curr.children.get(ch) == null){
-                curr.children.put(ch,new TrieNode());
-            }
-            curr = curr.children.get(ch);
-        }
-        curr.isEndOfWord = true;
-    }
-
-    int getLongestCommanPrefix() {
-        TrieNode curr = root;
-        int idx = 0;
-        while(!curr.isEndOfWord && curr.children.size() == 1){
-            for(char key : curr.children.keySet()){
-                curr = curr.children.get(key);
-            }
-            idx++;
-        }
-
-        return idx;
-    }
-
-
-}
-
-class TrieNode {
-    HashMap<Character,TrieNode> children;
-    boolean isEndOfWord;
-    TrieNode() {
-        children = new HashMap<>();
-        isEndOfWord = false;
+        return prefix;
     }
 }
