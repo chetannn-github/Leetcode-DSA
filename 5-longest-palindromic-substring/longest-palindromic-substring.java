88929be4c22db2e84efda91c02619f92a05e5a3c
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2026-03-07 07:38:26 +0530

    Time: 167 ms (20.67%) | Memory: 43.9 MB (47.65%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.java
+++ 5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,51 +1,29 @@
 class Solution {
-    int n;
-    int[][] dp;
-    int maxLength = 0;
-    int resultStart = 0, resultEnd = 0;
-
     public String longestPalindrome(String s) {
-        n = s.length(); 
-        dp = new int[n+1][n+1];
-
-        for(int[] row : dp){
-            Arrays.fill(row,-1);
-        }
-
-        for(int i=0; i<n; i++){
-            solve(s,i,i);
-        }
-        return s.substring(resultStart, resultEnd);
-    }
-
-    public void solve(String s, int start, int end){
-        if(end>=n){
-            return;
-        }
-        
-        if(isPalindrome(s,start,end) == 1) {
-            if(maxLength < end - start + 1) {
-                maxLength = end - start + 1;
-                resultStart = start;
-                resultEnd = end + 1;
+        for (int length = s.length(); length > 0; length--) {
+            for (int start = 0; start <= s.length() - length; start++) {
+                if (check(start, start + length, s)) {
+                    return s.substring(start, start + length);
+                }
             }
         }
-        
-        solve(s,start,end+1);
-        return ;
+
+        return "";
     }
 
-    public int isPalindrome(String s, int left, int right) {
-        if(left>=right){
-            return 1;
+    private boolean check(int i, int j, String s) {
+        int left = i;
+        int right = j - 1;
+
+        while (left < right) {
+            if (s.charAt(left) != s.charAt(right)) {
+                return false;
+            }
+
+            left++;
+            right--;
         }
-        if(dp[left][right]!=-1){
-            return dp[left][right];
-        }
-        if (s.charAt(left) != s.charAt(right)) {
-            return dp[left][right] = 0;
-        }
-        
-        return dp[left][right] = isPalindrome(s,left+1,right-1);
+
+        return true;
     }
 }
