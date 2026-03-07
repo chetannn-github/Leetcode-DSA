e24c80dd7c0a87ca76721ac0c38252c6e411cd99
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2026-03-07 12:55:45 +0530

    Time: 6 ms (69.11%) | Memory: 46.6 MB (37.65%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.java
@@ -1,47 +1,19 @@
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         if(s.length()==0){
-//             return 0;
-//         }
-//         int start = 0;
-//         int length = 1;
-//         HashMap<Character,Integer> hm = new HashMap<>();
-        
-//         for(int end = 0; end<s.length();end++){
-//             char ch = s.charAt(end);
-            
-//             if(hm.isEmpty()  || !hm.containsKey(ch) || hm.get(ch)<start) {
-//                 length = Math.max(length, end - start +1);
-
-//             }else if(hm.containsKey(ch)){
-//                 start = hm.get(ch)+1;
-//             }
-//             hm.put(ch,end);
-//         }
-//         return length ;
-//     }
-// }
-
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        if(s.length()==0){
-            return 0;
-        }
-        int start = 0;
-        int length = 1;
-        HashMap<Character,Integer> hm = new HashMap<>();
-        
-        for(int end = 0; end<s.length();end++){
-            char ch = s.charAt(end);
-            
-            while (hm.getOrDefault(ch,0) > 0) {
-                char st = s.charAt(start);
-                hm.put(st,hm.getOrDefault(st,0)-1);
-                start++;
+        int left = 0;
+        int maxLength = 0;
+        HashSet<Character> charSet = new HashSet<>();
+
+        for (int right = 0; right < s.length(); right++) {
+            while (charSet.contains(s.charAt(right))) {
+                charSet.remove(s.charAt(left));
+                left++;
             }
-            hm.put(ch,hm.getOrDefault(ch,0)+1);
-            length = Math.max(length,end-start+1);
+
+            charSet.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
         }
-        return length ;
+
+        return maxLength;       
     }
 }
