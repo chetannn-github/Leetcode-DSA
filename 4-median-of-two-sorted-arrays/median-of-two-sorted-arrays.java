ee661845d5daf58238af4d96c768205354cc0d34
Chetan Singh Rajawat <chetan.rajawat25@gmail.com>
2026-03-07 07:37:45 +0530

    Time: 1 ms (99.71%) | Memory: 48.9 MB (50.72%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,82 +1,55 @@
-// class Solution {
-//     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-//         int left = 0;
-//         int right = 0;
+class Solution {
+    public double findMedianSortedArrays(int[] A, int[] B) {
+        int na = A.length, nb = B.length;
+        int n = na + nb;
+        if ((na + nb) % 2 == 1) {
+            return solve(A, B, n / 2, 0, na - 1, 0, nb - 1);
+        } else {
+            return (
+                (double) (solve(A, B, n / 2, 0, na - 1, 0, nb - 1) +
+                    solve(A, B, n / 2 - 1, 0, na - 1, 0, nb - 1)) /
+                2
+            );
+        }
+    }
 
-//         int m = nums1.length;
-//         int n = nums2.length;
-
-//         int currCount = 0;
-//         boolean isTotalOddLength = ((m+n) & 1) == 1;
-//         int medianIdx = ((m+n) >> 1);
-
-//         int firstOne = -1;
-//         int secondOne = -1;
-        
-//         while(secondOne == -1) {
-//             while((right >= n || (left < m && nums1[left] <= nums2[right])) ) {
-
-//                 if(currCount == medianIdx) {
-//                     if(isTotalOddLength) return nums1[left];
-//                     else secondOne = nums1[left];
-//                     break;
-//                 }else if(currCount == medianIdx-1) {
-//                     firstOne = nums1[left];
-//                 }
-//                 left++;
-//                 currCount++;
-//             }
-
-//             while(secondOne == -1 && (left >= m || (right < n && nums1[left] > nums2[right]))) {
-//                 if(currCount == medianIdx) {
-//                     if(isTotalOddLength) return nums2[right];
-//                     else secondOne = nums2[right];
-//                 }else if(currCount == medianIdx-1) {
-//                     firstOne = nums2[right];
-//                 }
-//                 right++;
-//                 currCount++;
-//             }
-//         }
-
-
-//         return (double) (firstOne + secondOne) / 2 ;
-        
-//     }
-// }
-
-class Solution { 
-    final static int min = Integer.MIN_VALUE;
-    final static int max = Integer.MAX_VALUE;
-
-    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length, n = nums2.length;
-        if(m > n) return findMedianSortedArrays(nums2, nums1);
-
-        boolean isTotalOddLength = ((m+n) & 1) == 1;
-        int start = 0, end = m;
-       
-        int totalLeft = (m+n+1) >> 1;
-
-        while(start <= end) {
-            int mid = start + ((end-start)>>1);
-
-            int l1 = mid == 0 ? min : nums1[mid-1];
-            int l2 = totalLeft - mid == 0 ? min : nums2[totalLeft - mid-1];
-
-            int r1 = mid == m ? max : nums1[mid];
-            int r2 = totalLeft - mid >= n ? max : nums2[totalLeft - mid];
-
-            boolean isGoodParition = (l1 <= r2 && l2 <= r1);
-
-            if(isGoodParition) {
-                if (isTotalOddLength) return Math.max(l1,l2);
-                else return (double) (Math.max(l1,l2) + Math.min(r1,r2)) / 2;
-                
-            }else if(l1 > r2) end = mid-1;
-            else start = mid+1;
+    public int solve(
+        int[] A,
+        int[] B,
+        int k,
+        int aStart,
+        int aEnd,
+        int bStart,
+        int bEnd
+    ) {
+        // If the segment of on array is empty, it means we have passed all
+        // its element, just return the corresponding element in the other array.
+        if (aEnd < aStart) {
+            return B[k - aStart];
+        }
+        if (bEnd < bStart) {
+            return A[k - bStart];
         }
 
-        return -1.0;
+        // Get the middle indexes and middle values of A and B.
+        int aIndex = (aStart + aEnd) / 2, bIndex = (bStart + bEnd) / 2;
+        int aValue = A[aIndex], bValue = B[bIndex];
+
+        // If k is in the right half of A + B, remove the smaller left half.
+        if (aIndex + bIndex < k) {
+            if (aValue > bValue) {
+                return solve(A, B, k, aStart, aEnd, bIndex + 1, bEnd);
+            } else {
+                return solve(A, B, k, aIndex + 1, aEnd, bStart, bEnd);
+            }
+        }
+        // Otherwise, remove the larger right half.
+        else {
+            if (aValue > bValue) {
+                return solve(A, B, k, aStart, aIndex - 1, bStart, bEnd);
+            } else {
+                return solve(A, B, k, aStart, aEnd, bStart, bIndex - 1);
+            }
+        }
     }
 }
